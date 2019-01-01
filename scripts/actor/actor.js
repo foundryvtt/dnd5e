87a0f13c9fb5b5e7635aeace509c33ecc6a76e1c
Andrew <aaclayton@gmail.com>
2019-01-01 12:05:57 -0800

    Dice rolling improvements and fixes for 0.1.2

---
+++ scripts/actor/actor.js
@@ -0,0 +1,289 @@
+/**
+ * Extend the base Actor class to implement additional logic specialized for D&D5e.
+ */
+class Actor5e extends Actor {
+
+  /**
+   * Augment the basic actor data with additional dynamic data.
+   */
+  prepareData(actorData) {
+    actorData = super.prepareData(actorData);
+    const data = actorData.data;
+
+    // Prepare Character data
+    if ( actorData.type === "character" ) this._prepareCharacterData(data);
+    else if ( actorData.type === "npc" ) this._prepareNPCData(data);
+
+    // Ability modifiers and saves
+    for (let abl of Object.values(data.abilities)) {
+      abl.mod = Math.floor((abl.value - 10) / 2);
+      abl.save = abl.mod + ((abl.proficient || 0) * data.attributes.prof.value);
+    }
+
+    // Skill modifiers
+    for (let skl of Object.values(data.skills)) {
+      skl.value = parseFloat(skl.value || 0);
+      skl.mod = data.abilities[skl.ability].mod + Math.floor(skl.value * data.attributes.prof.value);
+    }
+
+    // Attributes
+    data.attributes.init.mod = data.abilities.dex.mod + (data.attributes.init.value || 0);
+    data.attributes.ac.min = 10 + data.abilities.dex.mod;
+
+    // Spell DC
+    let spellAbl = data.attributes.spellcasting.value || "int";
+    data.attributes.spelldc.value = 8 + data.attributes.prof.value + data.abilities[spellAbl].mod;
+
+    // Return the prepared Actor data
+    return actorData;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Prepare Character type specific data
+   */
+  _prepareCharacterData(data) {
+
+    // Level, experience, and proficiency
+    data.details.level.value = parseInt(data.details.level.value);
+    data.details.xp.max = this.getLevelExp(data.details.level.value || 1);
+    let prior = this.getLevelExp(data.details.level.value - 1 || 0),
+          req = data.details.xp.max - prior;
+    data.details.xp.pct = Math.min(Math.round((data.details.xp.value -prior) * 100 / req), 99.5);
+    data.attributes.prof.value = Math.floor((data.details.level.value + 7) / 4);
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Prepare NPC type specific data
+   */
+  _prepareNPCData(data) {
+
+    // CR, kill exp, and proficiency
+    data.details.cr.value = parseFloat(data.details.cr.value) || 0;
+    data.details.xp.value = this.getCRExp(data.details.cr.value);
+    data.attributes.prof.value = Math.floor((Math.max(data.details.cr.value, 1) + 7) / 4);
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Return the amount of experience required to gain a certain character level.
+   * @param level {Number}  The desired level
+   * @return {Number}       The XP required
+   */
+  getLevelExp(level) {
+    const levels = [0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000,
+      120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000];
+    return levels[Math.min(level, levels.length - 1)];
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Return the amount of experience granted by killing a creature of a certain CR.
+   * @param cr {Number}     The creature's challenge rating
+   * @return {Number}       The amount of experience granted per kill
+   */
+  getCRExp(cr) {
+    if (cr < 1.0) return Math.max(200 * cr, 10);
+    let _ = undefined;
+    const xps = [10, 200, 450, 700, 1100, 1800, 2300, 2900, 3900, 5000, 5900, 18000, 20000, 22000,
+        25000, 27500, 30000, 32500, 36500, _, _, _, _, _, 155000];
+    return xps[cr];
+  }
+
+  /* -------------------------------------------- */
+  /*  Rolls                                       */
+  /* -------------------------------------------- */
+
+  get _skillSaveRollModalHTML() {
+    return `
+    <form>
+        <div class="form-group">
+            <label>Formula</label>
+            <input type="text" name="formula" value="1d20 + @mod + @bonus" disabled/>
+        </div>
+        <div class="form-group">
+            <label>Situational Bonus?</label>
+            <input type="text" name="bonus" value="" placeholder="e.g. +1d4"/>
+        </div>
+    </form>
+    `;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Roll a Skill Check
+   * Prompt the user for input regarding Advantage/Disadvantage and any Situational Bonus
+   * @param skill {String}    The skill id
+   */
+  rollSkill(skillName) {
+    let skl = this.data.data.skills[skillName],
+      abl = this.data.data.abilities[skl.ability],
+      parts = ["1d20", "@mod", "@bonus"],
+      flavor = `${skl.label} Skill Check`;
+
+    // Create a dialog
+    new Dialog({
+      title: `${skl.label} (${abl.label}) Skill Check`,
+      content: this._skillSaveRollModalHTML,
+      buttons: {
+        advantage: {
+          label: "Advantage",
+          callback: () => {
+            parts[0] = "2d20kh";
+            flavor += " (Advantage)"
+          }
+        },
+        normal: {
+          label: "Normal",
+        },
+        disadvantage: {
+          label: "Disadvantage",
+          callback: () => {
+            parts[0] = "2d20kl";
+            flavor += " (Disadvantage)"
+          }
+        }
+      },
+      close: html => {
+        let bonus = html.find('[name="bonus"]').val(),
+            roll = new Roll(parts.join(" + "), {mod: skl.mod, bonus: bonus}).roll();
+        roll.toMessage({
+          alias: this.name,
+          flavor: flavor,
+          highlightSuccess: roll.parts[0].total === 20,
+          highlightFailure: roll.parts[0].total === 1
+        });
+      }
+    }).render(true);
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Roll a generic ability test or saving throw.
+   * Prompt the user for input on which variety of roll they want to do.
+   * @param abilityId {String}    The ability id (e.g. "str")
+   */
+  rollAbility(abilityId) {
+    let abl = this.data.data.abilities[abilityId];
+    new Dialog({
+      title: `${abl.label} Ability Check`,
+      content: `<p>What type of ${abl.label} check?</p>`,
+      buttons: {
+        test: {
+          label: "Ability Test",
+          callback: () => this.rollAbilityTest(abilityId)
+        },
+        save: {
+          label: "Saving Throw",
+          callback: () => this.rollAbilitySave(abilityId)
+        }
+      }
+    }).render(true);
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Roll an Ability Test
+   * Prompt the user for input regarding Advantage/Disadvantage and any Situational Bonus
+   * @param abilityId {String}    The ability ID (e.g. "str")
+   */
+  rollAbilityTest(abilityId) {
+    let abl = this.data.data.abilities[abilityId],
+        parts = ["1d20", "@mod", "@bonus"],
+        flavor = `${abl.label} Ability Test`;
+
+    // Create a dialog
+    new Dialog({
+      title: flavor,
+      content: this._skillSaveRollModalHTML,
+      buttons: {
+        advantage: {
+          label: "Advantage",
+          callback: () => {
+            parts[0] = "2d20kh";
+            flavor += " (Advantage)"
+          }
+        },
+        normal: {
+          label: "Normal",
+        },
+        disadvantage: {
+          label: "Disadvantage",
+          callback: () => {
+            parts[0] = "2d20kl";
+            flavor += " (Disadvantage)"
+          }
+        }
+      },
+      close: html => {
+        let bonus = html.find('[name="bonus"]').val(),
+            roll = new Roll(parts.join(" + "), {mod: abl.mod, bonus: bonus}).roll();
+        roll.toMessage({
+          alias: this.name,
+          flavor: flavor,
+          highlightSuccess: roll.parts[0].total === 20,
+          highlightFailure: roll.parts[0].total === 1
+        });
+      }
+    }).render(true);
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Roll an Ability Saving Throw
+   * Prompt the user for input regarding Advantage/Disadvantage and any Situational Bonus
+   * @param abilityId {String}    The ability ID (e.g. "str")
+   */
+  rollAbilitySave(abilityId) {
+    let abl = this.data.data.abilities[abilityId],
+        parts = ["1d20", "@mod", "@bonus"],
+        flavor = `${abl.label} Saving Throw`;
+
+    // Create a dialog
+    new Dialog({
+      title: flavor,
+      content: this._skillSaveRollModalHTML,
+      buttons: {
+        advantage: {
+          label: "Advantage",
+          callback: () => {
+            parts[0] = "2d20kh";
+            flavor += " (Advantage)"
+          }
+        },
+        normal: {
+          label: "Normal",
+        },
+        disadvantage: {
+          label: "Disadvantage",
+          callback: () => {
+            parts[0] = "2d20kl";
+            flavor += " (Disadvantage)"
+          }
+        }
+      },
+      close: html => {
+        let bonus = html.find('[name="bonus"]').val(),
+            roll = Roll(parts.join(" + "), {mod: abl.save, bonus: bonus}).roll();
+        roll.toMessage({
+          alias: this.name,
+          flavor: flavor,
+          highlightSuccess: roll.parts[0].total === 20,
+          highlightFailure: roll.parts[0].total === 1
+        });
+      }
+    }).render(true);
+  }
+}
+
+// Assign the actor class to the CONFIG
+CONFIG.Actor.entityClass = Actor5e;
--- scripts/item/item.js
+++ scripts/item/item.js
@@ -110,39 +110,45 @@ class Item5e extends Item {
 
   /**
    * Roll a Weapon Attack
-   * Holding SHIFT when the attack is rolled will "fast-forward".
-   * This chooses the default options of a normal attack with no bonus
+   * Holding SHIFT, ALT, or CTRL when the attack is rolled will "fast-forward".
+   * This chooses the default options of a normal attack with no bonus, Advantage, or Disadvantage respectively
    */
   rollWeaponAttack(ev) {
     if ( this.type !== "weapon" ) throw "Wrong item type!";
 
     // Prepare roll data
-    let rollData = duplicate(this.actor.data.data),
-        abl = this.data.data.ability.value || "str",
+    let itemData = this.data.data,
+        rollData = duplicate(this.actor.data.data),
+        abl = itemData.ability.value || "str",
         parts = ["1d20", "@hit", "@mod", "@prof", "@bonus"],
         adv = 0,
-        flavor = `${this.name} - Attack Roll`;
+        title = `${this.name} - Attack Roll`;
     mergeObject(rollData, {
-      hit: this.data.data.bonus.value,
+      hit: itemData.bonus.value,
       mod: rollData.abilities[abl].mod,
-      prof: rollData.attributes.prof.value,
-      bonus: null
+      prof: Number(itemData.proficient.value) * rollData.attributes.prof.value
     });
 
     // Define roll function
     let roll = () => {
+      let flavor;
       if ( adv === 1 ) {
         parts[0] = "2d20kh";
-        flavor += " (Advantage)";
+        flavor = `${title} (Advantage)`;
       }
       else if ( adv === -1 ) {
         parts[0] = "2d20kl";
-        flavor += " (Disadvantage)"
+        flavor = `${title} (Disadvantage)`;
+      } else {
+        parts[0] = "1d20";
+        flavor = title;
       }
-      let formula = parts.join("+");
+
+      // Don't include situational bonus unless it is defined
+      if ( !rollData.bonus && parts.indexOf("@bonus") !== -1 ) parts.pop();
 
       // Execute the roll and send it to chat
-      let roll = new Roll(formula, rollData).roll();
+      let roll = new Roll(parts.join("+"), rollData).roll();
       roll.toMessage({
         alias: this.actor.name,
         flavor: flavor,
@@ -152,12 +158,12 @@ class Item5e extends Item {
     };
 
     // Fast-forward rolls
-    if ( keyboard.isDown(KEYS.SHIFT) ) return roll();
-    else if ( keyboard.isDown(KEYS.ALT ) ) {
+    if ( ev.shiftKey ) return roll();
+    else if ( ev.altKey ) {
       adv = 1;
       return roll();
     }
-    else if ( keyboard.isDown(KEYS.CTRL ) ) {
+    else if ( ev.ctrlKey || ev.metaKey ) {
       adv = -1;
       return roll();
     }
@@ -166,7 +172,7 @@ class Item5e extends Item {
     let template = "public/systems/dnd5e/templates/chat/roll-dialog.html";
     renderTemplate(template, {formula: parts.join(" + ")}).then(dlg => {
       new Dialog({
-        title: flavor,
+        title: title,
         content: dlg,
         buttons: {
           advantage: {
@@ -201,26 +207,35 @@ class Item5e extends Item {
     if ( this.type !== "weapon" ) throw "Wrong item type!";
 
     // Get data
-    let rollData = duplicate(this.actor.data.data),
-        abl = this.data.data.ability.value || "str",
-        parts = [alternate ? this.data.data.damage2.value : this.data.data.damage.value, "@mod", "@bonus"],
+    let itemData = this.data.data,
+        rollData = duplicate(this.actor.data.data),
+        abl = itemData.ability.value || "str",
+        parts = [alternate ? itemData.damage2.value : itemData.damage.value, "@mod", "@bonus"],
         critical = false,
-        flavor = `${this.name} - Damage Roll`;
+        title = `${this.name} - Damage Roll`;
     mergeObject(rollData, { mod: rollData.abilities[abl].mod, bonus: null });
 
     // Define roll function
     let roll = () => {
+      let flavor = title;
+
+      // Don't include situational bonus unless it is defined
+      if ( !rollData.bonus && parts.indexOf("@bonus") !== -1 ) parts.pop();
+
+      // Roll and alter critical hits
       let roll = new Roll(parts.join("+"), rollData);
       if ( critical ) {
         roll.alter(0, 2);
-        flavor += " (Critical)";
+        flavor = `${title} (Critical)`;
       }
+
+      // Dispatch the roll
       roll.toMessage({ alias: this.actor.name, flavor: flavor});
     };
 
     // Fast-forward rolls
-    if ( [KEYS.SHIFT, KEYS.CTRL].some(k => keyboard.isDown(k)) ) return roll();
-    else if ( keyboard.isDown(KEYS.ALT ) ) {
+    if ( ev.shiftKey || ev.ctrlKey || ev.metaKey )  return roll();
+    else if ( ev.altKey ) {
       critical = true;
       return roll();
     }
@@ -229,7 +244,7 @@ class Item5e extends Item {
     let template = "public/systems/dnd5e/templates/chat/roll-dialog.html";
     renderTemplate(template, {formula: parts.join(" + ")}).then(dlg => {
       new Dialog({
-        title: flavor,
+        title: title,
         content: dlg,
         buttons: {
           advantage: {
@@ -253,18 +268,19 @@ class Item5e extends Item {
 
   /**
    * Roll Spell Damage
-   * Holding SHIFT when the attack is rolled will "fast-forward".
-   * This chooses the default options of a normal damage roll with no damage
+   * Holding SHIFT, ALT, or CTRL when the attack is rolled will "fast-forward".
+   * This chooses the default options of a normal attack with no bonus, Advantage, or Disadvantage respectively
    */
   rollSpellAttack(ev) {
     if ( this.type !== "spell" ) throw "Wrong item type!";
 
     // Prepare roll data
-    let rollData = duplicate(this.actor.data.data),
-        abl = this.data.data.ability.value || this.actor.data.data.attributes.spellcasting.value || "int",
+    let itemData = this.data.data,
+        rollData = duplicate(this.actor.data.data),
+        abl = itemData.ability.value || rollData.attributes.spellcasting.value || "int",
         parts = ["1d20", "@mod", "@prof", "@bonus"],
         adv = 0,
-        flavor = `${this.name} - Spell Attack Roll`;
+        title = `${this.name} - Spell Attack Roll`;
     mergeObject(rollData, {
       mod: rollData.abilities[abl].mod,
       prof: rollData.attributes.prof.value,
@@ -273,18 +289,24 @@ class Item5e extends Item {
 
     // Define roll function
     let roll = () => {
+      let flavor;
       if ( adv === 1 ) {
         parts[0] = "2d20kh";
-        flavor += " (Advantage)";
+        flavor = `${title} (Advantage)`;
       }
       else if ( adv === -1 ) {
         parts[0] = "2d20kl";
-        flavor += " (Disadvantage)"
+        flavor = `${title} (Disadvantage)`;
+      } else {
+        parts[0] = "1d20";
+        flavor = title;
       }
-      let formula = parts.join("+");
+
+      // Don't include situational bonus unless it is defined
+      if ( !rollData.bonus && parts.indexOf("@bonus") !== -1 ) parts.pop();
 
       // Execute the roll and send it to chat
-      let roll = new Roll(formula, rollData).roll();
+      let roll = new Roll(parts.join("+"), rollData).roll();
       roll.toMessage({
         alias: this.actor.name,
         flavor: flavor,
@@ -294,12 +316,12 @@ class Item5e extends Item {
     };
 
     // Fast-forward rolls
-    if ( keyboard.isDown(KEYS.SHIFT) ) return roll();
-    else if ( keyboard.isDown(KEYS.ALT ) ) {
+    if ( ev.shiftKey ) return roll();
+    else if ( ev.altKey ) {
       adv = 1;
       return roll();
     }
-    else if ( keyboard.isDown(KEYS.CTRL ) ) {
+    else if ( ev.ctrlKey || ev.metaKey ) {
       adv = -1;
       return roll();
     }
@@ -308,7 +330,7 @@ class Item5e extends Item {
     let template = "public/systems/dnd5e/templates/chat/roll-dialog.html";
     renderTemplate(template, {formula: parts.join(" + ")}).then(dlg => {
       new Dialog({
-        title: flavor,
+        title: title,
         content: dlg,
         buttons: {
           advantage: {
@@ -343,26 +365,35 @@ class Item5e extends Item {
     if ( this.type !== "spell" ) throw "Wrong item type!";
 
     // Get data
-    let rollData = duplicate(this.actor.data.data),
-        abl = this.data.data.ability.value || this.actor.data.data.attributes.spellcasting.value || "int",
-        parts = [this.data.data.damage.value, "@bonus"],
+    let itemData = this.data.data,
+        rollData = duplicate(this.actor.data.data),
+        abl = itemData.ability.value || rollData.attributes.spellcasting.value || "int",
+        parts = [itemData.damage.value],
         critical = false,
-        flavor = `${this.name} - Damage Roll`;
+        title = `${this.name} - Damage Roll`;
     mergeObject(rollData, { mod: rollData.abilities[abl].mod, bonus: null });
 
     // Define roll function
     let roll = () => {
+      let flavor = title;
+
+      // Don't include situational bonus unless it is defined
+      if ( !rollData.bonus && parts.indexOf("@bonus") !== -1 ) parts.pop();
+
+      // Roll and alter critical hits
       let roll = new Roll(parts.join("+"), rollData);
       if ( critical ) {
         roll.alter(0, 2);
-        flavor += " (Critical)";
+        flavor = `${title} (Critical)`;
       }
+
+      // Dispatch the roll
       roll.toMessage({ alias: this.actor.name, flavor: flavor});
     };
 
     // Fast-forward rolls
-    if ( [KEYS.SHIFT, KEYS.CTRL].some(k => keyboard.isDown(k)) ) return roll();
-    else if ( keyboard.isDown(KEYS.ALT ) ) {
+    if ( ev.shiftKey || ev.ctrlKey || ev.metaKey )  return roll();
+    else if ( ev.altKey ) {
       critical = true;
       return roll();
     }
@@ -371,7 +402,7 @@ class Item5e extends Item {
     let template = "public/systems/dnd5e/templates/chat/roll-dialog.html";
     renderTemplate(template, {formula: parts.join(" + ")}).then(dlg => {
       new Dialog({
-        title: flavor,
+        title: title,
         content: dlg,
         buttons: {
           advantage: {
@@ -456,24 +487,29 @@ class Item5e extends Item {
       prof = rollData.attributes.prof.value * (this.data.data.proficient.value || 0),
       parts = ["1d20", "@mod", "@prof", "@bonus"],
       adv = 0,
-      flavor = `${this.name} - Tool Check`;
+      title = `${this.name} - Tool Check`;
     mergeObject(rollData, {mod: ability.mod, prof: prof, bonus: null});
 
     // Define roll function
     let roll = () => {
-      flavor = `${this.name} - ${ability.label}`;
+      let flavor;
       if ( adv === 1 ) {
         parts[0] = "2d20kh";
-        flavor += " (Advantage)";
+        flavor = `${title} (Advantage)`;
       }
       else if ( adv === -1 ) {
         parts[0] = "2d20kl";
-        flavor += " (Disadvantage)"
+        flavor = `${title} (Disadvantage)`;
+      } else {
+        parts[0] = "1d20";
+        flavor = title;
       }
-      let formula = parts.join("+");
+
+      // Don't include situational bonus unless it is defined
+      if ( !rollData.bonus && parts.indexOf("@bonus") !== -1 ) parts.pop();
 
       // Execute the roll and send it to chat
-      let roll = new Roll(formula, rollData).roll();
+      let roll = new Roll(parts.join("+"), rollData).roll();
       roll.toMessage({
         alias: this.actor.name,
         flavor: flavor,
@@ -483,12 +519,12 @@ class Item5e extends Item {
     };
 
     // Fast-forward rolls
-    if ( keyboard.isDown(KEYS.SHIFT) ) return roll();
-    else if ( keyboard.isDown(KEYS.ALT ) ) {
+    if ( ev.shiftKey ) return roll();
+    else if ( ev.altKey ) {
       adv = 1;
       return roll();
     }
-    else if ( keyboard.isDown(KEYS.CTRL ) ) {
+    else if ( ev.ctrlKey || ev.metaKey ) {
       adv = -1;
       return roll();
     }
@@ -497,7 +533,7 @@ class Item5e extends Item {
     let template = "public/systems/dnd5e/templates/chat/tool-roll-dialog.html";
     renderTemplate(template, { formula: parts.join(" + "), ability: abl, abilities: rollData.abilities}).then(dlg => {
       new Dialog({
-        title: flavor,
+        title: title,
         content: dlg,
         buttons: {
           advantage: {
@@ -515,6 +551,7 @@ class Item5e extends Item {
         default: "normal",
         close: html => {
           ability = rollData.abilities[html.find('[name="ability"]').val()];
+          title = `${this.name} - ${ability.label} Check`;
           mergeObject(rollData, {bonus: html.find('[name="bonus"]').val(), mod: ability.mod});
           roll();
         }
@@ -623,259 +660,5 @@ class Item5e extends Item {
   }
 }
 
-
-/* -------------------------------------------- */
-
-
-// Activate global listeners
-Hooks.on('renderChatLog', (log, html, data) => Item5e.chatListeners(html));
-
 // Assign Item5e class to CONFIG
 CONFIG.Item.entityClass = Item5e;
-
-
-/* -------------------------------------------- */
-
-
-/**
- * Override and extend the basic :class:`ItemSheet` implementation
- */
-class Item5eSheet extends ItemSheet {
-  constructor(item, options) {
-    super(item, options);
-    this.mce = null;
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Use a type-specific template for each different item type
-   */
-  get template() {
-    let type = this.item.type;
-    return `public/systems/dnd5e/templates/items/item-${type}-sheet.html`;
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Prepare item sheet data
-   * Start with the base item data and extending with additional properties for rendering.
-   */
-  getData() {
-    const data = super.getData();
-    data['abilities'] = game.system.template.actor.data.abilities;
-    data['damageTypes'] = CONFIG.damageTypes;
-    let types = (this.item.type === "equipment") ? "armorTypes" : this.item.type + "Types";
-    data[types] = CONFIG[types];
-
-    // Spell-specific data
-    if ( this.item.type === "spell" ) {
-      data["spellSchools"] = CONFIG.spellSchools;
-      data["spellLevels"] = CONFIG.spellLevels;
-    }
-
-    // Tool-specific data
-    else if ( this.item.type === "tool" ) {
-      data["proficiencies"] = CONFIG.proficiencyLevels;
-    }
-    return data;
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Activate listeners for interactive item sheet events
-   */
-  activateListeners(html) {
-    super.activateListeners(html);
-
-	  // Activate TinyMCE Editors
-	  html.find(".editor a.editor-edit").click(ev => {
-	    let button = $(ev.currentTarget),
-	        editor = button.siblings(".editor-content");
-	    createEditor({
-        target: editor[0],
-        height: editor.parent().height() - 40,
-        save_enablewhendirty: true,
-        save_onsavecallback: ed => this._onSaveMCE(ed, editor.attr("data-edit"))
-      }).then(ed => {
-        this.mce = ed[0];
-        button.hide();
-        this.mce.focus();
-      });
-    });
-
-    // Activate tabs
-    html.find('.tabs').each((_, el) => new Tabs(el));
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Customize sheet closing behavior to ensure we clean up the MCE editor
-   */
-  close() {
-    super.close();
-    if ( this.mce ) this.mce.destroy();
-  }
-
-  /* -------------------------------------------- */
-  /*  Saving and Submission                       */
-  /* -------------------------------------------- */
-
-  /**
-   * Extend the default Item Sheet submission logic to save the content of any active MCE editor
-   * @private
-   */
-  _onSubmit(ev) {
-    if ( this.mce ) {
-      const content = this.mce.getContent();
-      this.element.find('[data-edit="data.description.value"]').html(content);
-    }
-    super._onSubmit(ev);
-  };
-
-  /* -------------------------------------------- */
-
-  /**
-   * Handle using the Save button on the MCE editor
-   * @private
-   */
-  _onSaveMCE(ed, target) {
-    const form = this.element.find('.item-sheet')[0];
-    const itemData = validateForm(form);
-    itemData[target] = ed.getContent();
-
-    // Update owned items
-    if (this.item.isOwned) {
-      itemData.id = this.item.data.id;
-      this.item.actor.updateOwnedItem(itemData, true).then(item => {
-        this.item = item;
-        this.render(false);
-      });
-    }
-
-    // Update unowned items
-    else {
-      this.item.update(itemData, true);
-      this.render(false);
-    }
-
-    // Destroy the editor
-    this.mce = null;
-    ed.remove();
-    ed.destroy();
-  }
-}
-
-
-/* -------------------------------------------- */
-
-
-// Override CONFIG
-CONFIG.Item.sheetClass = Item5eSheet;
-
-// Standard D&D Damage Types
-CONFIG.damageTypes = {
-  "acid": "Acid",
-  "bludgeoning": "Bludgeoning",
-  "cold": "Cold",
-  "fire": "Fire",
-  "force": "Force",
-  "lightning": "Lightning",
-  "necrotic": "Necrotic",
-  "piercing": "Piercing",
-  "poison": "Poison",
-  "psychic": "Psychic",
-  "radiant": "Radiant",
-  "slashing": "Slashing",
-  "thunder": "Thunder",
-  "healing": "Healing"
-};
-
-// Weapon Types
-CONFIG.weaponTypes = {
-  "simpleM": "Simple Melee",
-  "simpleR": "Simple Ranged",
-  "martialM": "Martial Melee",
-  "martialR": "Martial Ranged",
-  "natural": "Natural",
-  "improv": "Improvised",
-  "ammo": "Ammunition"
-};
-
-// Weapon Properties
-CONFIG.weaponProperties = {
-  "thr": "Thrown",
-  "amm": "Ammunition",
-  "fir": "Firearm",
-  "rel": "Reload",
-  "two": "Two-Handed",
-  "fin": "Finesse",
-  "lgt": "Light",
-  "ver": "Versatile",
-  "hvy": "Heavy",
-  "rch": "Reach"
-};
-
-// Equipment Types
-CONFIG.armorTypes = {
-  "clothing": "Clothing",
-  "light": "Light Armor",
-  "medium": "Medium Armor",
-  "heavy": "Heavy Armor",
-  "bonus": "Magical Bonus",
-  "natural": "Natural Armor",
-  "shield": "Shield"
-};
-
-// Consumable Types
-CONFIG.consumableTypes = {
-  "potion": "Potion",
-  "scroll": "Scroll",
-  "wand": "Wand",
-  "rod": "Rod",
-  "trinket": "Trinket"
-};
-
-// Spell Types
-CONFIG.spellTypes = {
-  "attack": "Spell Attack",
-  "save": "Saving Throw",
-  "heal": "Healing",
-  "utility": "Utility"
-};
-
-// Spell Schools
-CONFIG.spellSchools = {
-  "abj": "Abjuration",
-  "con": "Conjuration",
-  "div": "Divination",
-  "enc": "Enchantment",
-  "evo": "Evocation",
-  "ill": "Illusion",
-  "nec": "Necromancy",
-  "trs": "Transmutation",
-};
-
-// Spell Levels
-CONFIG.spellLevels = {
-  0: "Cantrip",
-  1: "1st Level",
-  2: "2nd Level",
-  3: "3rd Level",
-  4: "4th Level",
-  5: "5th Level",
-  6: "6th Level",
-  7: "7th Level",
-  8: "8th Level",
-  9: "9th Level"
-};
-
-// Feat Types
-CONFIG.featTypes = {
-  "passive": "Passive Ability",
-  "attack": "Special Attack",
-  "ability": "Active Ability"
-};
