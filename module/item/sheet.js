fb5d130c426117a7e03b480154052b9eb9d2eda3
Jeff Hitchcock <mail@command-center.org>
2021-03-29 16:53:42 -0700

    Remove custom option for class skill proficiencies

--- module/item/sheet.js
+++ module/item/sheet.js
@@ -345,6 +345,7 @@ export default class ItemSheet5e extends ItemSheet {
         options.choices = Object.fromEntries(Object.entries(CONFIG.DND5E.skills).filter(skill => choiceSet.includes(skill[0])));
         options.minimum = skills.number;
         options.maximum = skills.number;
+        options.allowCustom = false;
         break;
     }
 
