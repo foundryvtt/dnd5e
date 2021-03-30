e6fd30ccf5661fa9050811b1883a4b998158f660
Jeff Hitchcock <mail@command-center.org>
2021-03-29 20:26:03 -0700

    Add eslint for checking jsdocs to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,32 @@
 const gulp = require('gulp');
+const eslint = require('gulp-eslint');
 
 const css = require('./utils/css.js');
 const packs = require('./utils/packs.js');
 
 
+
+
+/* ----------------------------------------- */
+/*  Lint Javascript
+/* ----------------------------------------- */
+
+function lintJavascript() {
+  return gulp
+    .src("module/**/*.js")
+    .pipe(eslint({}))
+    .pipe(eslint.format());
+}
+const jsLint = gulp.series(lintJavascript);
+
+
+/* ----------------------------------------- */
+/*  Export Tasks
+/* ----------------------------------------- */
+
 exports.default = gulp.series(
   gulp.parallel(css.compile),
+  gulp.parallel(jsLint),
   css.watchUpdates
 );
 exports.css = css.compile;
