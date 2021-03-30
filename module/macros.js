27c98e636bd6b1bb2a1794f0cc26938939bd1c5c
Jeff Hitchcock <mail@command-center.org>
2021-03-30 10:43:13 -0700

    Fix core JS types capitalization

--- module/macros.js
+++ module/macros.js
@@ -8,7 +8,7 @@
  * Get an existing item macro if one exists, otherwise create a new one.
  * @param {Object} data     The dropped data
  * @param {number} slot     The hotbar slot to use
- * @return {Promise}
+ * @returns {Promise}
  */
 export async function create5eMacro(data, slot) {
   if ( data.type !== "Item" ) return;
