f72562d2e0b95c7ab12d5c31ffc85236a22b0e5f
Jeff Hitchcock <mail@command-center.org>
2021-03-30 12:06:21 -0700

    Add missing return in JSDocs

--- module/actor/sheets/vehicle.js
+++ module/actor/sheets/vehicle.js
@@ -27,6 +27,8 @@ export default class ActorSheet5eVehicle extends ActorSheet5e {
 
   /**
    * Creates a new cargo entry for a vehicle Actor.
+   *
+   * @return {object}
    */
   static get newCargo() {
     return {
