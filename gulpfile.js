eb01f47c7d4dc76f1780a11f6582812b64c7e068
Jeff Hitchcock <mail@command-center.org>
2021-04-05 10:49:59 -0700

    Remove linting from default gulp action

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,12 @@
 const gulp = require('gulp');
-const gulpIf = require('gulp-if');
-const eslint = require('gulp-eslint');
 
 const css = require('./utils/css.js');
 const linting = require("./utils/lint.js");
 const packs = require('./utils/packs.js');
 
 
-
-
-/* ----------------------------------------- */
-/*  Lint Javascript
-/* ----------------------------------------- */
-
-const applyFixes = process.argv.slice(2).includes('--fix');
-const DND5E_JS = [".eslintrc.json", "dnd5e.js", "module/**/*.js"];
-function lintJavascript() {
-  return gulp
-    .src("module/**/*.js")
-    .pipe(eslint({"fix": applyFixes}))
-    .pipe(eslint.format())
-    .pipe(gulpIf((file) => file.eslint != null && file.eslint.fixed, gulp.dest("module/")));
-}
-const jsLint = gulp.series(lintJavascript);
-
-
-/* ----------------------------------------- */
-/*  Export Tasks
-/* ----------------------------------------- */
-
 exports.default = gulp.series(
   gulp.parallel(css.compile),
-  gulp.parallel(jsLint),
   css.watchUpdates
 );
 exports.css = css.compile;
