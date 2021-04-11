f8a3c0d5834267882b33ce5f022a520a6112c457
Jeff Hitchcock <mail@command-center.org>
2021-04-11 09:57:19 -0700

    Modify gulpfile to use minimist for argument parsing

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
 const gulpIf = require('gulp-if');
 const less = require('gulp-less');
+
+const parsedArgs = require('minimist')(process.argv.slice(2));
+
+// Code Linting
 const eslint = require('gulp-eslint');
 require('eslint-plugin-jsdoc');
+
 
 /* ----------------------------------------- */
 /*  Compile LESS
@@ -21,9 +26,9 @@ const css = gulp.series(compileLESS);
 /*  Lint Javascript
 /* ----------------------------------------- */
 
-const applyFixes = process.argv.slice(2).includes('--fix');
 const DND5E_JS = [".eslintrc.json", "dnd5e.js", "module/**/*.js"];
 function lintJavascript() {
+  const applyFixes = parsedArgs.hasOwnProperty("fix");
   return gulp
     .src("module/**/*.js")
     .pipe(eslint({"fix": applyFixes}))
@@ -39,7 +44,6 @@ const jsLint = gulp.series(lintJavascript);
 
 function watchUpdates() {
   gulp.watch(DND5E_LESS, css);
-  gulp.watch(DND5E_JS, jsLint);
 }
 
 /* ----------------------------------------- */
