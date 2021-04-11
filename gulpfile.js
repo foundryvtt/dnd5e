3168f360481ce60ab21f2112673333e055278bc1
Jeff Hitchcock <mail@command-center.org>
2021-04-11 11:15:02 -0700

    Modify gulpfile to allow linting of dnd5e.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const gulpIf = require('gulp-if');
 const less = require('gulp-less');
+const mergeStream = require("merge-stream");
 
 const parsedArgs = require('minimist')(process.argv.slice(2));
 
 // Code Linting
 const eslint = require('gulp-eslint');
+require('babel-eslint');
 require('eslint-plugin-jsdoc');
 
 
@@ -26,14 +28,19 @@ const css = gulp.series(compileLESS);
 /*  Lint Javascript
 /* ----------------------------------------- */
 
-const DND5E_JS = [".eslintrc.json", "dnd5e.js", "module/**/*.js"];
+const LINTING_PATHS = ["./dnd5e.js", "./module/"];
 function lintJavascript() {
   const applyFixes = parsedArgs.hasOwnProperty("fix");
-  return gulp
-    .src("module/**/*.js")
-    .pipe(eslint({"fix": applyFixes}))
-    .pipe(eslint.format())
-    .pipe(gulpIf((file) => file.eslint != null && file.eslint.fixed, gulp.dest("module/")));
+  const tasks = LINTING_PATHS.map((path) => {
+    const src = path.endsWith("/") ? `${path}**/*.js` : path;
+    const dest = path.endsWith("/") ? path : `${path.split("/").slice(0, -1).join("/")}/`;
+    return gulp
+      .src(src)
+      .pipe(eslint({"fix": applyFixes}))
+      .pipe(eslint.format())
+      .pipe(gulpIf((file) => file.eslint != null && file.eslint.fixed, gulp.dest(dest)));
+  });
+  return mergeStream.call(null, tasks);
 }
 const jsLint = gulp.series(lintJavascript);
 
