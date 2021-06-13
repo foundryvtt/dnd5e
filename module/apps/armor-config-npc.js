357aad73149cb5a3794f3a0f5ac7c65cd97349fd
Jeff Hitchcock <mail@command-center.org>
2021-06-13 12:27:14 -0700

    Implement initial NPC armor config UI

--- module/apps/armor-config-npc.js
+++ module/apps/armor-config-npc.js
@@ -18,22 +18,57 @@ export default class ArmorConfigNPC extends DocumentSheet {
 
   /* -------------------------------------------- */
 
-  /** @override */
+  /** @inheritdoc */
   get title() {
     return `${game.i18n.localize("DND5E.ArmorConfig")}: ${this.document.name}`;
   }
 
   /* -------------------------------------------- */
 
-  /** @override */
-  getData() {
-    
+  /** @inheritdoc */
+  async getData() {
+    // let armorTypes = Object.entries(CONFIG.DND5E.armorProficiencies).reduce((obj, [key, label]) => {
+    //   if (key === "shl") return obj;
+    //   obj[key] = {
+    //     label: label,
+    //     types: []
+    //   }
+    //   return obj;
+    // }, {});
+
+    let data = {
+      armor: foundry.utils.getProperty(this.object.data.data, "details.armor"),
+      types: {}
+    };
+
+    const pack = game.packs.get(CONFIG.DND5E.sourcePacks.ITEMS);
+    for ( const [key, id] of Object.entries(CONFIG.DND5E.armorIds) ) {
+      const item = await pack.getDocument(id);
+      const type = foundry.utils.getProperty(item.data.data, "armor.type");
+      const entry = {
+        label: item.name,
+        chosen: key === data.armor.type
+      };
+      if ( data.types[type] === undefined ) {
+        data.types[type] = {
+          label: CONFIG.DND5E.armorProficiencies[CONFIG.DND5E.armorProficienciesMap[type]],
+          children: {}
+        }
+      }
+      data.types[type].children[key] = entry;
+    }
+
+    return data;
   }
 
   /* -------------------------------------------- */
 
-  /** @override */
+  /** @inheritdoc */
   async _updateObject(event, formData) {
-    
+    const armorObject = foundry.utils.expandObject(formData);
+    const updates = {
+      "data.details.armor": armorObject.armor
+    };
+    return this.object.update(updates);
   }
 }
