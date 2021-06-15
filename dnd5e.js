e0495dae51b30b86e8e99d98f255687b7a3710e7
fyorl <kim.mantas@gmail.com>
2021-06-15 16:54:49 +0100

    [#1156] Force a migration in 1.3.4 even though there's nothing to migrate, in order to catch worlds that were accidentally not migrated earlier when they should have been.

--- dnd5e.js
+++ dnd5e.js
@@ -180,10 +180,9 @@ Hooks.once("ready", function() {
   // Determine whether a system migration is required and feasible
   if ( !game.user.isGM ) return;
   const currentVersion = game.settings.get("dnd5e", "systemMigrationVersion");
-  if ( !currentVersion ) return game.settings.set('dnd5e', 'systemMigrationVersion', game.system.data.version);
-  const NEEDS_MIGRATION_VERSION = "1.3.0";
+  const NEEDS_MIGRATION_VERSION = "1.3.4";
   const COMPATIBLE_MIGRATION_VERSION = 0.80;
-  const needsMigration = currentVersion && isNewerVersion(NEEDS_MIGRATION_VERSION, currentVersion);
+  const needsMigration = !currentVersion || isNewerVersion(NEEDS_MIGRATION_VERSION, currentVersion);
   if ( !needsMigration ) return;
 
   // Perform the migration
