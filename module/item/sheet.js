f74e19de292138fd6505bb156e565845a9023806
Jeff Hitchcock <mail@command-center.org>
2021-08-08 11:02:40 -0700

    Add feature section to Background item and grant it to Actor when dropped

--- module/item/sheet.js
+++ module/item/sheet.js
@@ -28,7 +28,8 @@ export default class ItemSheet5e extends ItemSheet {
       classes: ["dnd5e", "sheet", "item"],
       resizable: true,
       scrollY: [".tab.details"],
-      tabs: [{navSelector: ".tabs", contentSelector: ".sheet-body", initial: "description"}]
+      tabs: [{navSelector: ".tabs", contentSelector: ".sheet-body", initial: "description"}],
+      dragDrop: [{ dropSelector: ".drop-area" }]
     });
   }
 
@@ -87,6 +88,7 @@ export default class ItemSheet5e extends ItemSheet {
     if ( itemData.type === "background" ) {
       this._prepareTraits(itemData);
       await this._prepareGrantedTraits(data);
+      if ( itemData.data.feature ) data.feature = await fromUuid(itemData.data.feature);
     }
 
     // Re-define the template data references (backwards compatible)
@@ -552,6 +554,7 @@ export default class ItemSheet5e extends ItemSheet {
     super.activateListeners(html);
     if ( this.isEditable ) {
       html.find(".damage-control").click(this._onDamageControl.bind(this));
+      html.find(".delete-item").click(this._onDeleteLinkedItem.bind(this));
       html.find(".delete-tag").click(this._onDeleteTag.bind(this));
       html.find(".trait-configuration").click(this._onConfigureTraits.bind(this));
       html.find(".trait-selector").click(this._onSelectTraits.bind(this));
@@ -649,6 +652,24 @@ export default class ItemSheet5e extends ItemSheet {
   /* -------------------------------------------- */
 
   /**
+   * Handle the deletion of a linked item.
+   * @param {Event} event  The click event that triggered the deletion.
+   * @private
+   */
+  _onDeleteLinkedItem(event) {
+    event.preventDefault();
+    const dropArea = event.target.closest(".linked-item");
+    const type = dropArea.dataset.type;
+
+    if ( type === "feature" ) {
+      this.position.height = "auto";
+      return this.object.update({ "data.feature": null });
+    }
+  }
+
+  /* -------------------------------------------- */
+
+  /**
    * Handle the deletion of a tag when the delete tag link is clicked.
    * @param {Event} event         The click event that triggered the deletion.
    * @returns {Item5e|undefined}  Item with the updates applied (if they were applied).
@@ -693,4 +714,35 @@ export default class ItemSheet5e extends ItemSheet {
     }
     return this.object.update(updateData);
   }
+
+  /* -------------------------------------------- */
+  /*  Drag and Drop                               */
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  async _onDrop(event) {
+    // Try to extract the data
+    let data;
+    try {
+      data = JSON.parse(event.dataTransfer.getData("text/plain"));
+    } catch (err) {
+      return false;
+    }
+
+    const allowed = Hooks.call("dropItemSheetData", this.object, this, data);
+    if ( allowed === false ) return false;
+
+    if ( data.type !== "Item" ) return false;
+    const item = await Item.implementation.fromDropData(data);
+
+    // Get information from the drop area
+    const linkedItemArea = event.target.closest(".linked-item");
+    const type = linkedItemArea?.dataset.type;
+
+    if ( type === "feature" ) {
+      this.position.height = "auto";
+      return this.object.update({"data.feature": item.uuid});
+    }
+  }
+
 }
