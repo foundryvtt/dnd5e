9046410eacb9fa1fd9a32b60629928205b7dee56
Kandashi <jacobmpalmer@live.co.uk>
2021-08-27 03:58:30 +0000

    Update entity.js

--- module/actor/entity.js
+++ module/actor/entity.js
@@ -755,7 +755,7 @@ export default class Actor5e extends Actor {
     // Compose roll parts and data
     const parts = ["@mod"];
     const data = {mod: skl.mod + skl.prof};
-    
+
     // Ability test bonus
     if ( bonuses.check ) {
       data["checkBonus"] = bonuses.check;
