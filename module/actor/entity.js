3db174d432e33b86d261c339f9565ff9b51b975c
Kandashi <jacobmpalmer@live.co.uk>
2021-08-27 02:15:07 +0000

    Update entity.js

--- module/actor/entity.js
+++ module/actor/entity.js
@@ -749,12 +749,13 @@ export default class Actor5e extends Actor {
    * @return {Promise<Roll>}      A Promise which resolves to the created Roll instance
    */
   rollSkill(skillId, options={}) {
+    options.chooseModifier = true
     const skl = this.data.data.skills[skillId];
     const bonuses = getProperty(this.data.data, "bonuses.abilities") || {};
 
     // Compose roll parts and data
     const parts = ["@mod"];
-    const data = {mod: skl.mod + skl.prof};
+    const data = {mod: skl.mod + skl.prof, abilities: this.data.data.abilities, default: {ability: skl.ability}};
 
     // Ability test bonus
     if ( bonuses.check ) {
