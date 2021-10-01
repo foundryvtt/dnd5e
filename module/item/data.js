8b6b7119981e353c208beb0e7f1be7c10ea5b9f6
Jeff Hitchcock <mail@command-center.org>
2021-10-01 12:27:26 -0700

    Fix bug with GrantedTraitData

--- module/item/data.js
+++ module/item/data.js
@@ -3,15 +3,17 @@ import * as fields from "/common/data/fields.mjs";
 
 class GrantedTraitData extends foundry.abstract.DocumentData {
   static defineSchema() {
-    choices: {
-      type: [String],
-      required: false,
-      default: []
-    },
-    count: fields.field(fields.POSITIVE_INTEGER_FIELD, {
-      required: true,
-      nullable: false,
-      default: 1
-    })
+    return {
+      choices: {
+        type: [String],
+        required: false,
+        default: []
+      },
+      count: fields.field(fields.POSITIVE_INTEGER_FIELD, {
+        required: true,
+        nullable: false,
+        default: 1
+      })
+    };
   }
 }
