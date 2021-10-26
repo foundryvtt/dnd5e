7f6ce4b00b44ee5106cb7ceb4e29bc31aea96844
Jeff Hitchcock <mail@command-center.org>
2021-10-26 11:58:51 -0700

    Add ability for modules to customize skills

--- module/applications/actor/base-sheet.mjs
+++ module/applications/actor/base-sheet.mjs
@@ -139,8 +139,8 @@ export default class ActorSheet5e extends ActorSheet {
       skl.ability = CONFIG.DND5E.abilityAbbreviations[skl.ability];
       skl.icon = this._getProficiencyIcon(skl.value);
       skl.hover = CONFIG.DND5E.proficiencyLevels[skl.value];
-      skl.label = CONFIG.DND5E.skills[s];
-      skl.baseValue = source.system.skills[s].value;
+      skl.label = CONFIG.DND5E.skills[s]?.label;
+      skl.baseValue = source.system.skills[s]?.value ?? 0;
     }
 
     // Update traits
