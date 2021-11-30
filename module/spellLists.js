4160c1cf74d0735a3ed35fb6745ebafaa2a792b4
Jeff Hitchcock <mail@command-center.org>
2021-11-30 14:54:52 -0800

    Start building spell list

---
+++ module/spellLists.js
@@ -0,0 +1,128 @@
+export const DefaultSpellList = {
+  cleric: {
+    /** Cantrips **/
+    "guidance":                         "dnd5e.spells.P7mF2MxSuVJwHRRY",
+    "light":                            "dnd5e.spells.Bnn9Nzajixvow9xi",
+    "mending":                          "dnd5e.spells.kjmjY0zlE6IEiQVL",
+    "resistance":                       "dnd5e.spells.dl8YwvMboBqX2OC4",
+    "scared-flame":                     "dnd5e.spells.n9pJzTDsAwQxJVRl",
+    "spare-the-dying":                  "dnd5e.spells.8zT7njvqbpXs4Cel",
+    "thaumaturgy":                      "dnd5e.spells.MUO1uYN7JR1hm4dR",
+
+    /** 1st Level **/
+    "bane":                             "dnd5e.spells.",
+    "bless":                            "dnd5e.spells.",
+    "command":                          "dnd5e.spells.",
+    "create-or-destroy-water":          "dnd5e.spells.",
+    "cure-wounds":                      "dnd5e.spells.",
+    "detect-evil-and-good":             "dnd5e.spells.",
+    "detect-magic":                     "dnd5e.spells.",
+    "detect-poison-and-disease":        "dnd5e.spells.",
+    "guiding-bold":                     "dnd5e.spells.",
+    "healing-word":                     "dnd5e.spells.",
+    "inflict-wounds":                   "dnd5e.spells.",
+    "protection-from-evil-and-good":    "dnd5e.spells.",
+    "purify-food-and-drink":            "dnd5e.spells.",
+    "sanctuary":                        "dnd5e.spells.",
+    "shield-of-faith":                  "dnd5e.spells.",
+
+    /** 2nd Level **/
+    "dnd5e.spells.", // Aid
+    "dnd5e.spells.", // Augury
+    "dnd5e.spells.", // Blindness/Deafness
+    "dnd5e.spells.", // Calm Emotions
+    "dnd5e.spells.", // Continual Flame
+    "dnd5e.spells.", // Enhance Ability
+    "dnd5e.spells.", // Find Traps
+    "dnd5e.spells.", // Gentle Repose
+    "dnd5e.spells.", // Hold Person
+    "dnd5e.spells.", // Lesser Restoration
+    "dnd5e.spells.", // Locate Object
+    "dnd5e.spells.", // Prayer of Healing
+    "dnd5e.spells.", // Protection from Poison
+    "dnd5e.spells.", // Silence
+    "dnd5e.spells.", // Spiritual Weapon
+    "dnd5e.spells.", // Warding Bond
+    "dnd5e.spells.", // Zone of Truth
+
+    /** 3rd Level **/
+    "dnd5e.spells.", // Animate Dead
+    "dnd5e.spells.", // Beacon of Hope
+    "dnd5e.spells.", // Bestow Curse
+    "dnd5e.spells.", // Clairvoyance
+    "dnd5e.spells.", // Create Food and Water
+    "dnd5e.spells.", // Daylight
+    "dnd5e.spells.", // Dispel Magic
+    "dnd5e.spells.", // Glyph of Warding
+    "dnd5e.spells.", // Magic Circle
+    "dnd5e.spells.", // Mass Healing Word
+    "dnd5e.spells.", // Meld into Stone
+    "dnd5e.spells.", // Protection from Energy
+    "dnd5e.spells.", // Remove Cruse
+    "dnd5e.spells.", // Revivify
+    "dnd5e.spells.", // Sending
+    "dnd5e.spells.", // Speak with Dead
+    "dnd5e.spells.", // Spirit Guardians
+    "dnd5e.spells.", // Tongues
+    "dnd5e.spells.", // Water Walk
+
+    /** 4th Level **/
+    "dnd5e.spells.", // Banishment
+    "dnd5e.spells.", // Control Water
+    "dnd5e.spells.", // Death Ward
+    "dnd5e.spells.", // Divination
+    "dnd5e.spells.", // Freedom of Movement
+    "dnd5e.spells.", // Guardian of Faith
+    "dnd5e.spells.", // Locate Creature
+    "dnd5e.spells.", // Stone Shape
+    
+    /** 5th Level **/
+    "dnd5e.spells.", // Commune
+    "dnd5e.spells.", // Contagion
+    "dnd5e.spells.", // Dispel Evil and Good
+    "dnd5e.spells.", // Flame Strike
+    "dnd5e.spells.", // Geas
+    "dnd5e.spells.", // Greater Restoration
+    "dnd5e.spells.", // Hallow
+    "dnd5e.spells.", // Insect Plague
+    "dnd5e.spells.", // Legend Lore
+    "dnd5e.spells.", // Mass Cure Wounds
+    "dnd5e.spells.", // Planar Binding
+    "dnd5e.spells.", // Raise Dead
+    "dnd5e.spells.", // Scrying
+    
+    /** 6th Level **/
+    "dnd5e.spells.", // Blade Barrier
+    "dnd5e.spells.", // Create Undead
+    "dnd5e.spells.", // Find the Path
+    "dnd5e.spells.", // Forbiddance
+    "dnd5e.spells.", // Harm
+    "dnd5e.spells.", // Heal
+    "dnd5e.spells.", // Heroes' Feast
+    "dnd5e.spells.", // Planar Ally
+    "dnd5e.spells.", // True Seeing
+    "dnd5e.spells.", // Word of Recall
+    
+    /** 7th Level **/
+    "dnd5e.spells.", // Conjure Celestial
+    "dnd5e.spells.", // Divine Word
+    "dnd5e.spells.", // Etherealness
+    "dnd5e.spells.", // Fire Storm
+    "dnd5e.spells.", // Plane Shift
+    "dnd5e.spells.", // Regenerate
+    "dnd5e.spells.", // Resurrection
+    "dnd5e.spells.", // Symbol
+    
+    /** 8th Level **/
+    "dnd5e.spells.", // Antimagic Field
+    "dnd5e.spells.", // Control Weather
+    "dnd5e.spells.", // Earthquake
+    "dnd5e.spells.", // Holy Aura
+    
+    /** 9th Level **/
+    "dnd5e.spells.", // Astral Projection
+    "dnd5e.spells.", // Gate
+    "dnd5e.spells.", // Mass Heal
+    "dnd5e.spells.", // True Resurrection
+  }
+};
