664aca617ad843474e36d2bd5dbc3a61a1496796
Jeff Hitchcock <mail@command-center.org>
2021-12-16 14:17:54 -0800

    Remove other advancement types, remove rollup

--- module/advancement.js
+++ module/advancement.js
@@ -1,6 +1,2 @@
-export { AbilityScoreImprovementAdvancement } from "./advancement/abilityScoreImprovement.js";
-export { HitPointsAdvancement } from "./advancement/hitPoints.js";
-export { ItemChoiceAdvancement } from "./advancement/itemChoice.js";
+export { Advancement } from "./advancement/advancement.js";
 export { ItemGrantAdvancement } from "./advancement/itemGrant.js";
-export { ScaleValueAdvancement } from "./advancement/scaleValue.js";
-export { TraitAdvancement } from "./advancement/trait.js";
