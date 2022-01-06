b918425ce9422ab4528d81a1e24cb0fc92d44ca6
fyorl <kim.mantas@gmail.com>
2022-01-06 14:16:55 +0000

    [#1386] Allow Healing actions to also consume ammunition.

--- module/item/entity.js
+++ module/item/entity.js
@@ -505,7 +505,7 @@ export default class Item5e extends Item {
     // Define follow-up actions resulting from the item usage
     let createMeasuredTemplate = hasArea;       // Trigger a template creation
     let consumeRecharge = !!recharge.value;     // Consume recharge
-    let consumeResource = !!resource.target && (resource.type !== "ammo"); // Consume a linked (non-ammo) resource
+    let consumeResource = !!resource.target && (!item.hasAttack || (resource.type !== "ammo")); // Consume a linked (non-ammo) resource
     let consumeSpellSlot = requireSpellSlot;    // Consume a spell slot
     let consumeUsage = !!uses.per;              // Consume limited uses
     let consumeQuantity = uses.autoDestroy;     // Consume quantity of the item in lieu of uses
@@ -1020,10 +1020,10 @@ export default class Item5e extends Item {
 
     // Invoke the d20 roll helper
     const roll = await d20Roll(rollConfig);
-    if ( roll === false ) return null;
+    if ( roll === null ) return null;
 
     // Commit ammunition consumption on attack rolls resource consumption if the attack roll was made
-    if ( ammo && !isObjectEmpty(ammoUpdate) ) await ammo.update(ammoUpdate);
+    if ( ammo && !foundry.utils.isObjectEmpty(ammoUpdate) ) await ammo.update(ammoUpdate);
     return roll;
   }
 
@@ -1038,7 +1038,8 @@ export default class Item5e extends Item {
    * @param {number} [config.spellLevel]   If the item is a spell, override the level for damage scaling
    * @param {boolean} [config.versatile]   If the item is a weapon, roll damage using the versatile formula
    * @param {object} [config.options]      Additional options passed to the damageRoll function
-   * @returns {Promise<Roll>}        A Promise which resolves to the created Roll instance
+   * @returns {Promise<Roll>}              A Promise which resolves to the created Roll instance, or null if the action
+   *                                       cannot be performed.
    */
   rollDamage({critical=false, event=null, spellLevel=null, versatile=false, options={}}={}) {
     if ( !this.hasDamage ) throw new Error("You may not make a Damage Roll with this Item.");
@@ -1123,7 +1124,7 @@ export default class Item5e extends Item {
     }
 
     // Call the roll helper utility
-    return damageRoll(mergeObject(rollConfig, options));
+    return damageRoll(foundry.utils.mergeObject(rollConfig, options));
   }
 
   /* -------------------------------------------- */
