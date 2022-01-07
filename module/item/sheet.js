4f3eb2a451eab4e611dbdeb75317ccfa85634cf4
Jeff 'Arbron' Hitchcock <gitlab779@command-center.org>
2022-01-07 13:41:28 +0000

    Hit Dice as resource for item consumption

--- module/item/sheet.js
+++ module/item/sheet.js
@@ -149,6 +149,15 @@ export default class ItemSheet5e extends ItemSheet {
       }, {});
     }
 
+    // Hit Dice
+    else if ( consume.type === "hitDice" ) {
+      return {
+        smallest: game.i18n.localize("DND5E.ConsumeHitDiceSmallest"),
+        ...CONFIG.DND5E.hitDieTypes.reduce((obj, hd) => { obj[hd] = hd; return obj; }, {}),
+        largest: game.i18n.localize("DND5E.ConsumeHitDiceLargest")
+      };
+    }
+
     // Materials
     else if ( consume.type === "material" ) {
       return actor.items.reduce((obj, i) => {
@@ -178,6 +187,7 @@ export default class ItemSheet5e extends ItemSheet {
         return obj;
       }, {});
     }
+
     else return {};
   }
 
