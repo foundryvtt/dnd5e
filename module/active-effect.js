e6044ef870bf29b3d569495e68d26e8f00b038cd
Jeff Hitchcock <mail@command-center.org>
2022-01-11 10:56:00 -0800

    Remove number type coercion

--- module/active-effect.js
+++ module/active-effect.js
@@ -47,8 +47,6 @@ export default class ActiveEffect5e extends ActiveEffect {
     if ( data.type === Boolean ) {
       if ( value === "false" ) change.value = false;
       else change.value = Boolean(value);
-    } else if ( data.type === Number ) {
-      change.value = Number(value);
     }
 
     return change;
