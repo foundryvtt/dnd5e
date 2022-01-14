133088924af10b264e5c5adec509abccdf64076e
Jeff Hitchcock <mail@command-center.org>
2022-01-14 11:26:11 -0800

    Remove other advancement types

--- module/advancement.js
+++ module/advancement.js
@@ -1,24 +1,16 @@
-import { AbilityScoreImprovementAdvancement } from "./advancement/abilityScoreImprovement.js";
 import { AdvancementSelection } from "./advancement/advancementSelection.js";
 import { BaseAdvancement } from "./advancement/baseAdvancement.js";
 import { BaseConfig } from "./advancement/baseConfig.js";
 import { HitPointsAdvancement } from "./advancement/hitPoints.js";
-import { ItemChoiceAdvancement } from "./advancement/itemChoice.js";
 import { ItemGrantAdvancement } from "./advancement/itemGrant.js";
-import { ScaleValueAdvancement } from "./advancement/scaleValue.js";
-import { TraitAdvancement } from "./advancement/trait.js";
 
 const advancement = {
   AdvancementSelection,
   BaseAdvancement,
   BaseConfig,
   types: {
-    AbilityScoreImprovementAdvancement,
     HitPointsAdvancement,
-    ItemChoiceAdvancement,
-    ItemGrantAdvancement,
-    ScaleValueAdvancement,
-    TraitAdvancement
+    ItemGrantAdvancement
   }
 };
 
