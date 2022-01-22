fcc2a76ed8a78ac95ce0e52db1762f21c326ee8f
Jeff Hitchcock <mail@command-center.org>
2022-01-22 12:06:09 -0800

    Display scale value data in configuration app

--- module/advancement/scaleValue.js
+++ module/advancement/scaleValue.js
@@ -1,4 +1,47 @@
 import { Advancement } from "./advancement.js";
+import { AdvancementConfig } from "./advancementConfig.js";
+
+
+/**
+ * Configuration application for scale values.
+ *
+ * @extends {AdvancementConfig}
+ */
+export class ScaleValueConfig extends AdvancementConfig {
+
+  /** @inheritdoc */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["dnd5e", "advancement", "scale-value", "two-column"],
+      template: "systems/dnd5e/templates/advancement/scale-value-config.html",
+      title: "DND5E.AdvancementTitle",
+      width: 540
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  getData() {
+    const data = super.getData();
+    data.classIdentifier = this.parent.identifier;
+
+    let lastValue = "";
+    data.levels = this.advancement.constructor.allLevels.reduce((obj, level) => {
+      obj[level] = { placeholder: lastValue, value: "" };
+      const value = data.data.configuration.scale[level];
+      if ( value && (value !== lastValue) ) {
+        obj[level].value = value;
+        lastValue = value;
+      }
+      return obj;
+    }, {});
+
+    return data;
+  }
+
+}
+
 
 /**
  * Advancement that represents a value that scales with class level. **Can only be added to classes or subclasses.**
@@ -39,6 +82,10 @@ export class ScaleValueAdvancement extends Advancement {
 
   /* -------------------------------------------- */
 
+  static configApp = ScaleValueConfig;
+
+  /* -------------------------------------------- */
+
   /** @inheritdoc */
   static multiLevel = true;
 
@@ -68,7 +115,7 @@ export class ScaleValueAdvancement extends Advancement {
    * @returns {string}      Scale value at the given level or an empty string.
    */
   valueForLevel(level) {
-    const key = Object.keys(this.data.configuration.scale).reverse().find(l => l <= level);
+    const key = Object.keys(this.data.configuration.scale).reverse().find(l => Number(l) <= level);
     return this.data.configuration.scale[key] ?? "";
   }
 
