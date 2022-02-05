5a3d0e69e7219cc89fe1084e8d43960521d8dff2
Jeff Hitchcock <mail@command-center.org>
2022-02-05 10:55:34 -0800

    Add custom advancement icons

--- module/advancement/hit-points.js
+++ module/advancement/hit-points.js
@@ -15,7 +15,7 @@ export class HitPointsAdvancement extends Advancement {
   static get metadata() {
     return foundry.utils.mergeObject(super.metadata, {
       order: 10,
-      icon: "icons/svg/regen.svg",
+      icon: "systems/dnd5e/icons/advancement/hit-points.svg",
       title: game.i18n.localize("DND5E.AdvancementHitPointsTitle"),
       hint: game.i18n.localize("DND5E.AdvancementHitPointsHint"),
       multiLevel: true,
