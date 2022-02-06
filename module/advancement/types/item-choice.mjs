35a33493cfac792a53a0064ba44fc72a29aaf513
Jeff Hitchcock <mail@command-center.org>
2022-02-06 11:51:09 -0800

    Add item choice advancement

---
+++ module/advancement/types/item-choice.mjs
@@ -0,0 +1,63 @@
+import Advancement from "../advancement.mjs";
+
+/**
+ * Advancement that presents the player with a choice of multiple items that they can take. Keeps track of which
+ * items were selected at which levels.
+ *
+ * @extends {Advancement}
+ */
+export class ItemChoiceAdvancement extends Advancement {
+
+  /** @inheritdoc */
+  static get metadata() {
+    return foundry.utils.mergeObject(super.metadata, {
+      defaults: {
+        configuration: {
+          choices: {},
+          allowDrops: true,
+          pool: null
+        },
+        order: 50,
+        icon: "icons/svg/stone-path.svg",
+        title: game.i18n.localize("DND5E.AdvancementItemChoiceTitle"),
+        hint: game.i18n.localize("DND5E.AdvancementItemChoiceHint"),
+        multiLevel: true
+      }
+    });
+  }
+
+  /* -------------------------------------------- */
+  /*  Instance Properties                         */
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  get levels() {
+    return Array.from(Object.keys(this.data.configuration.choices));
+  }
+
+  /* -------------------------------------------- */
+  /*  Display Methods                             */
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  configuredForLevel(level) {
+    return this.data.value[level] !== undefined;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  titleForLevel(level) {
+    return `${this.title} <em>(${game.i18n.localize("DND5E.AdvancementChoices")})</em>`;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  summaryForLevel(level) {
+    const items = this.data.value[level];
+    if ( !items ) return "";
+    return Object.values(items).reduce((html, uuid) => html + game.dnd5e.utils._linkForUuid(uuid), "");
+  }
+
+}
