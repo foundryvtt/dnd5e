7b3a10d6adc738233512653d3465d465ee5ed42f
Jeff Hitchcock <mail@command-center.org>
2022-02-10 12:18:00 -0800

    Add localizations for migration messages

--- dnd5e.js
+++ dnd5e.js
@@ -220,8 +220,7 @@ Hooks.once("ready", function() {
 
   // Perform the migration
   if ( currentVersion && isNewerVersion(COMPATIBLE_MIGRATION_VERSION, currentVersion) ) {
-    const warning = "Your DnD5e system data is from too old a Foundry version and cannot be reliably migrated to the latest version. The process will be attempted, but errors may occur.";
-    ui.notifications.error(warning, {permanent: true});
+    ui.notifications.error(game.i18n.localize("MIGRATION.5eVersionTooOldWarning"), {permanent: true});
   }
   migrations.migrateWorld();
 });
