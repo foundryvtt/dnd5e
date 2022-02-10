8d8640fc9b872a0ec875931888875a1eca81aeea
akrigline <a.krigline@gmail.com>
2022-02-10 17:25:35 -0500

    refactor: switch to using subclasses

--- module/advancement/advancementConfig.js
+++ module/advancement/advancementConfig.js
@@ -30,7 +30,6 @@ export class AdvancementConfig extends FormApplication {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["dnd5e", "advancement", "dialog"],
-      dragDrop: [{ dropSelector: ".drop-target" }],
       template: "systems/dnd5e/templates/advancement/advancement-config.html",
       width: 400,
       height: "auto"
@@ -47,25 +46,6 @@ export class AdvancementConfig extends FormApplication {
 
   /* -------------------------------------------- */
 
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    // Remove an item from the list
-    html.on("click", ".item-delete", event => {
-      const parentElement = $(event.currentTarget).parents("[data-item-uuid]");
-      const uuidToDelete = parentElement.data("itemUuid");
-
-      const updates = {
-        configuration: this.prepareConfigurationUpdate({
-          items: this.advancement.data.configuration.items.filter(uuid => uuid !== uuidToDelete)
-        })
-      };
-
-      return this._updateAdvancement(updates);
-    });
-  }
-
-
   /** @inheritdoc */
   getData() {
     return {
@@ -78,64 +58,6 @@ export class AdvancementConfig extends FormApplication {
       showClassRestrictions: this.parent.type === "class",
       showLevelSelector: !this.advancement.constructor.multiLevel
     };
-  }
-
-  /* -------------------------------------------- */
-
-  /** @inheritdoc */
-  _canDragDrop() {
-    return this.isEditable;
-  }
-
-  /* -------------------------------------------- */
-
-  /** @inheritdoc */
-  async _onDrop(event) {
-    // Try to extract the data
-    let data;
-    try {
-      data = JSON.parse(event.dataTransfer.getData("text/plain"));
-    } catch(err) {
-      return false;
-    }
-
-    if (this.advancement.data.type === "ItemGrant") {
-      return this._onDropItemGrantItem(event, data);
-    }
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Handle dropping of an item reference or item data onto the Advancement Config for ItemGrant
-   * @param {DragEvent} event     The concluding DragEvent which contains drop data
-   * @param {object} data         The data transfer extracted from the event
-   * @returns {Promise<object>}    A data object which describes the result of the drop
-   * @private
-   */
-  async _onDropItemGrantItem(event, data) {
-    if (data.type !== "Item") return false;
-    const item = await Item.implementation.fromDropData(data);
-
-    const existingItems = this.advancement.data.configuration.items;
-
-    // Abort if this uuid exists already
-    if (existingItems.includes(item.uuid)) {
-      ui.notifications.warn(game.i18n.localize("DND5E.AdvancementItemGrantDuplicateWarning"));
-
-      return false;
-    }
-
-    const updates = {
-      configuration: this.prepareConfigurationUpdate({
-        items: [
-          ...existingItems,
-          item.uuid
-        ]
-      })
-    };
-
-    return this._updateAdvancement(updates);
   }
 
   /* -------------------------------------------- */
--- module/advancement/itemGrant.js
+++ module/advancement/itemGrant.js
@@ -1,4 +1,117 @@
 import { Advancement } from "./advancement.js";
+import { AdvancementConfig } from "./advancementConfig.js";
+
+/**
+ * Configuration application for item grants.
+ *
+ * @extends {AdvancementConfig}
+ */
+export class ItemGrantConfig extends AdvancementConfig {
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      dragDrop: [{ dropSelector: ".drop-target" }],
+      template: "systems/dnd5e/templates/advancement/item-grant-config.html"
+    });
+  }
+
+
+  /* -------------------------------------------- */
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Remove an item from the list
+    html.on("click", ".item-delete", event => {
+      const parentElement = $(event.currentTarget).parents("[data-item-uuid]");
+      const uuidToDelete = parentElement.data("itemUuid");
+
+      const updates = {
+        configuration: this.prepareConfigurationUpdate({
+          items: this.advancement.data.configuration.items.filter(uuid => uuid !== uuidToDelete)
+        })
+      };
+
+      return this._updateAdvancement(updates);
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  getData() {
+    const data = super.getData();
+    data.classIdentifier = this.parent.identifier;
+
+    let lastValue = "";
+    data.levels = this.advancement.constructor.allLevels.reduce((obj, level) => {
+      obj[level] = { placeholder: lastValue, value: "" };
+      const value = this.data.configuration.scale[level];
+      if ( value && (value !== lastValue) ) {
+        obj[level].value = value;
+        lastValue = value;
+      }
+      return obj;
+    }, {});
+
+    return data;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  _canDragDrop() {
+    return this.isEditable;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  async _onDrop(event) {
+    // Try to extract the data
+    let data;
+    try {
+      data = JSON.parse(event.dataTransfer.getData("text/plain"));
+    } catch(err) {
+      return false;
+    }
+
+    if (data.type !== "Item") return false;
+    const item = await Item.implementation.fromDropData(data);
+
+    const existingItems = this.advancement.data.configuration.items;
+
+    // Abort if this uuid exists already
+    if (existingItems.includes(item.uuid)) {
+      ui.notifications.warn(game.i18n.localize("DND5E.AdvancementItemGrantDuplicateWarning"));
+
+      return false;
+    }
+
+    const updates = {
+      configuration: this.prepareConfigurationUpdate({
+        items: [
+          ...existingItems,
+          item.uuid
+        ]
+      })
+    };
+
+    return this._updateAdvancement(updates);
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  prepareConfigurationUpdate(configuration) {
+    configuration.scale = this.constructor._cleanedObject(configuration.scale);
+    return configuration;
+  }
+
+}
+
 
 /**
  * Advancement that automatically grants one or more items to the player. Presents the player with the option of
@@ -36,6 +149,14 @@ export class ItemGrantAdvancement extends Advancement {
 
   /** @inheritdoc */
   static hint = "DND5E.AdvancementItemGrantHint";
+
+
+  /* -------------------------------------------- */
+
+  /**
+   * Subclass of AdvancementConfig that allows for editing of this advancement type.
+   */
+  static configApp = ItemGrantConfig;
 
   /* -------------------------------------------- */
   /*  Display Methods                             */
