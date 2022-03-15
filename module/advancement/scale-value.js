3b63b7a5d5416f2fb2f90bd60929fff42d8bacb4
Jeff Hitchcock <mail@command-center.org>
2022-03-15 11:35:57 -0700

    Fix ScaleValueFlow to work with new Application setup, adjust CSS font size

--- module/advancement/scale-value.js
+++ module/advancement/scale-value.js
@@ -155,16 +155,20 @@ export class ScaleValueConfig extends AdvancementConfig {
 export class ScaleValueFlow extends AdvancementFlow {
 
   /** @inheritdoc */
-  static template = "systems/dnd5e/templates/advancement/scale-value-flow.html";
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      template: "systems/dnd5e/templates/advancement/scale-value-flow.html"
+    });
+  }
 
   /* -------------------------------------------- */
 
   /** @inheritdoc */
   getData() {
-    return {
+    return foundry.utils.mergeObject(super.getData(), {
       initial: this.advancement.valueForLevel(this.level - 1),
       final: this.advancement.valueForLevel(this.level)
-    };
+    });
   }
 
 }
