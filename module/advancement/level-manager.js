fdb06b8f2731edd94c2d95d60000da7a41f3aab9
Jeff Hitchcock <mail@command-center.org>
2022-04-06 11:20:15 -0700

    Add dynamic buttons to level manager, prepare to call advancement

--- module/advancement/level-manager.js
+++ module/advancement/level-manager.js
@@ -26,16 +26,58 @@ export class LevelManager extends FormApplication {
   /** @inheritdoc */
   async getData() {
     const classes = this.object.itemTypes.class.sort((a, b) => a.name.localeCompare(b.name));
+    const characterLevel = classes.reduce((lvl, cls) => lvl + cls.data.data.levels, 0);
     return foundry.utils.mergeObject(super.getData(), {
-      classes
+      classes,
+      characterLevel,
+      isMaxLevel: characterLevel >= CONFIG.DND5E.maxLevel
     });
   }
 
   /* -------------------------------------------- */
 
   /** @inheritdoc */
+  activateListeners(html) {
+    super.activateListeners(html);
+    html[0].querySelectorAll("button:is(.add, .subtract)").forEach(e => {
+      e.addEventListener("click", this._onClickAddSubtractButton.bind(this));
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Update value of level field on button clicks, capping to min and max levels.
+   * @param {Event} event  Click event for the change.
+   */
+  _onClickAddSubtractButton(event) {
+    event.preventDefault();
+    const form = event.target.closest("form");
+    const characterLevel = Number(form.dataset.characterLevel);
+    const field = event.target.closest("div").querySelector("input.levels");
+
+    if ( event.currentTarget.classList.contains("add") && (characterLevel + 1 <= CONFIG.DND5E.maxLevel) ) {
+      field.value++;
+      form.dataset.characterLevel = characterLevel + 1;
+    } else if ( event.currentTarget.classList.contains("subtract") && (field.value > 0) ) {
+      field.value--;
+      form.dataset.characterLevel = characterLevel - 1;
+    }
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
   async _updateObject(event, formData) {
-    
+    // Figure out level deltas for all classes
+    const levelDeltas = Object.entries(formData).map(([identifier, level]) => {
+      const cls = this.object.classes[identifier];
+      const delta = level - cls.data.data.levels;
+      return [cls, delta];
+    }).filter(([, delta]) => delta !== 0).sort((a, b) => a[1] - b[1]);
+
+    // Create advancement manager and trigger changes
+    console.log(levelDeltas);
   }
 
 }
