bde72fee7f6b91554fd670e3a69b7158c884645c
Jeff Hitchcock <mail@command-center.org>
2022-06-14 15:32:23 -0700

    [#1002] Don't call dnd5e.roll hook until after ability template has been created

--- module/canvas/ability-template.mjs
+++ module/canvas/ability-template.mjs
@@ -53,7 +53,8 @@ export default class AbilityTemplate extends MeasuredTemplate {
   /* -------------------------------------------- */
 
   /**
-   * Creates a preview of the spell template
+   * Creates a preview of the spell template.
+   * @returns {Promise}  A promise that resolves with the final measured template if created.
    */
   drawPreview() {
     const initialLayer = canvas.activeLayer;
@@ -67,7 +68,7 @@ export default class AbilityTemplate extends MeasuredTemplate {
     this.actorSheet?.minimize();
 
     // Activate interactivity
-    this.activatePreviewListeners(initialLayer);
+    return this.activatePreviewListeners(initialLayer);
   }
 
   /* -------------------------------------------- */
@@ -75,57 +76,66 @@ export default class AbilityTemplate extends MeasuredTemplate {
   /**
    * Activate listeners for the template preview
    * @param {CanvasLayer} initialLayer  The initially active CanvasLayer to re-activate after the workflow is complete
+   * @returns {Promise}                 A promise that resolves with the final measured template if created.
    */
   activatePreviewListeners(initialLayer) {
-    const handlers = {};
-    let moveTime = 0;
+    return new Promise((resolve, reject) => {
+      const handlers = {};
+      let moveTime = 0;
 
-    // Update placement (mouse-move)
-    handlers.mm = event => {
-      event.stopPropagation();
-      let now = Date.now(); // Apply a 20ms throttle
-      if ( now - moveTime <= 20 ) return;
-      const center = event.data.getLocalPosition(this.layer);
-      const snapped = canvas.grid.getSnappedPosition(center.x, center.y, 2);
-      this.document.updateSource({x: snapped.x, y: snapped.y});
-      this.refresh();
-      moveTime = now;
-    };
+      // Update placement (mouse-move)
+      handlers.mm = event => {
+        event.stopPropagation();
+        let now = Date.now(); // Apply a 20ms throttle
+        if ( now - moveTime <= 20 ) return;
+        const center = event.data.getLocalPosition(this.layer);
+        const snapped = canvas.grid.getSnappedPosition(center.x, center.y, 2);
+        this.document.updateSource({x: snapped.x, y: snapped.y});
+        this.refresh();
+        moveTime = now;
+      };
 
-    // Cancel the workflow (right-click)
-    handlers.rc = event => {
-      this.layer._onDragLeftCancel(event);
-      canvas.stage.off("mousemove", handlers.mm);
-      canvas.stage.off("mousedown", handlers.lc);
-      canvas.app.view.oncontextmenu = null;
-      canvas.app.view.onwheel = null;
-      initialLayer.activate();
-      this.actorSheet?.maximize();
-    };
+      // Shared cancelation code
+      handlers.cancel = async event => {
+        this.layer._onDragLeftCancel(event);
+        canvas.stage.off("mousemove", handlers.mm);
+        canvas.stage.off("mousedown", handlers.lc);
+        canvas.app.view.oncontextmenu = null;
+        canvas.app.view.onwheel = null;
+        initialLayer.activate();
+        await this.actorSheet?.maximize();
+      };
 
-    // Confirm the workflow (left-click)
-    handlers.lc = event => {
-      handlers.rc(event);
-      const destination = canvas.grid.getSnappedPosition(this.document.x, this.document.y, 2);
-      this.document.updateSource(destination);
-      canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [this.document.toObject()]);
-    };
+      // Cancel the workflow (right-click)
+      handlers.rc = async event => {
+        await handlers.cancel(event);
+        reject();
+      };
 
-    // Rotate the template by 3 degree increments (mouse-wheel)
-    handlers.mw = event => {
-      if ( event.ctrlKey ) event.preventDefault(); // Avoid zooming the browser window
-      event.stopPropagation();
-      let delta = canvas.grid.type > CONST.GRID_TYPES.SQUARE ? 30 : 15;
-      let snap = event.shiftKey ? delta : 5;
-      const update = {direction: this.document.direction + (snap * Math.sign(event.deltaY))};
-      this.document.updateSource(update);
-      this.refresh();
-    };
+      // Confirm the workflow (left-click)
+      handlers.lc = async event => {
+        await handlers.cancel(event);
+        const destination = canvas.grid.getSnappedPosition(this.document.x, this.document.y, 2);
+        this.document.updateSource(destination);
+        resolve(canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [this.document.toObject()]));
+      };
 
-    // Activate listeners
-    canvas.stage.on("mousemove", handlers.mm);
-    canvas.stage.on("mousedown", handlers.lc);
-    canvas.app.view.oncontextmenu = handlers.rc;
-    canvas.app.view.onwheel = handlers.mw;
+      // Rotate the template by 3 degree increments (mouse-wheel)
+      handlers.mw = event => {
+        if ( event.ctrlKey ) event.preventDefault(); // Avoid zooming the browser window
+        event.stopPropagation();
+        let delta = canvas.grid.type > CONST.GRID_TYPES.SQUARE ? 30 : 15;
+        let snap = event.shiftKey ? delta : 5;
+        const update = {direction: this.document.direction + (snap * Math.sign(event.deltaY))};
+        this.document.updateSource(update);
+        this.refresh();
+      };
+
+      // Activate listeners
+      canvas.stage.on("mousemove", handlers.mm);
+      canvas.stage.on("mousedown", handlers.lc);
+      canvas.app.view.oncontextmenu = handlers.rc;
+      canvas.app.view.onwheel = handlers.mw;
+    });
   }
 }
