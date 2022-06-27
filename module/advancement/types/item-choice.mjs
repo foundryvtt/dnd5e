4692d285cab624be2c1239146cd6037c7f05fb3c
Jeff Hitchcock <mail@command-center.org>
2022-06-27 10:24:26 -0700

    Add item choice config

--- module/advancement/types/item-choice.mjs
+++ module/advancement/types/item-choice.mjs
@@ -1,4 +1,5 @@
 import Advancement from "../advancement.mjs";
+import AdvancementConfig from "../advancement-config.mjs";
 
 /**
  * Advancement that presents the player with a choice of multiple items that they can take. Keeps track of which
@@ -15,13 +16,16 @@ export class ItemChoiceAdvancement extends Advancement {
         configuration: {
           choices: {},
           allowDrops: true,
-          pool: null
-        },
-        order: 50,
-        icon: "icons/svg/stone-path.svg",
-        title: game.i18n.localize("DND5E.AdvancementItemChoiceTitle"),
-        hint: game.i18n.localize("DND5E.AdvancementItemChoiceHint"),
-        multiLevel: true
+          pool: []
+        }
+      },
+      order: 50,
+      icon: "systems/dnd5e/icons/svg/item-choice.svg",
+      title: game.i18n.localize("DND5E.AdvancementItemChoiceTitle"),
+      hint: game.i18n.localize("DND5E.AdvancementItemChoiceHint"),
+      multiLevel: true,
+      apps: {
+        config: ItemChoiceConfig
       }
     });
   }
@@ -58,6 +62,99 @@ export class ItemChoiceAdvancement extends Advancement {
     const items = this.data.value[level];
     if ( !items ) return "";
     return Object.values(items).reduce((html, uuid) => html + game.dnd5e.utils._linkForUuid(uuid), "");
+  }
+
+}
+
+
+/**
+ * Configuration application for item choices.
+ *
+ * @extends {AdvancementConfig}
+ */
+export class ItemChoiceConfig extends AdvancementConfig {
+
+  /** @inheritdoc */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["dnd5e", "advancement", "item-choice", "two-column"],
+      dragDrop: [{ dropSelector: ".drop-target" }],
+      template: "systems/dnd5e/templates/advancement/item-choice-config.html",
+      width: 540
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  prepareConfigurationUpdate(configuration) {
+    configuration.choices = this.constructor._cleanedObject(configuration.choices);
+    return configuration;
+  }
+
+  /* -------------------------------------------- */
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Remove an item from the list
+    html.on("click", ".item-delete", this._onItemDelete.bind(this));
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle deleting an existing Item entry from the Advancement.
+   * @param {Event} event        The originating click event.
+   * @returns {Promise<Item5e>}  The promise for the updated parent Item which resolves after the application re-renders
+   * @private
+   */
+  async _onItemDelete(event) {
+    event.preventDefault();
+    const uuidToDelete = event.currentTarget.closest("[data-item-uuid]")?.dataset.itemUuid;
+    if ( !uuidToDelete ) return;
+    const pool = this.advancement.data.configuration.pool.filter(uuid => uuid !== uuidToDelete);
+    const updates = { configuration: this.prepareConfigurationUpdate({ pool }) };
+    await this.advancement.update(updates);
+    this.render();
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  _canDragDrop() {
+    return this.isEditable;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  async _onDrop(event) {
+    // Try to extract the data
+    let data;
+    try {
+      data = JSON.parse(event.dataTransfer.getData("text/plain"));
+    } catch(err) {
+      return false;
+    }
+
+    if ( data.type !== "Item" ) return false;
+    const item = await Item.implementation.fromDropData(data);
+
+    const existingPool = this.advancement.data.configuration.pool;
+
+    // Abort if this uuid is the parent item
+    if ( item.uuid === this.item.uuid ) {
+      return ui.notifications.warn(game.i18n.localize("DND5E.AdvancementItemGrantRecursiveWarning"));
+    }
+
+    // Abort if this uuid exists already
+    if ( existingPool.includes(item.uuid) ) {
+      return ui.notifications.warn(game.i18n.localize("DND5E.AdvancementItemGrantDuplicateWarning"));
+    }
+
+    await this.advancement.update({"configuration.pool": [...existingPool, item.uuid]});
+    this.render();
   }
 
 }
