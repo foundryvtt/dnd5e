bf33ba403cb4d768b182734b73185f230a0c5f56
Jeff Hitchcock <mail@command-center.org>
2022-08-01 10:00:05 -0700

    [#1002] Refactor AbilityTemplate event handlers into seperate methods

--- module/canvas/ability-template.mjs
+++ module/canvas/ability-template.mjs
@@ -4,6 +4,30 @@
 export default class AbilityTemplate extends MeasuredTemplate {
 
   /**
+   * Track the timestamp when the last mouse move event was captured.
+   * @type {number}
+   */
+  #moveTime = 0;
+
+  /* -------------------------------------------- */
+
+  /**
+   * The initially active CanvasLayer to re-activate after the workflow is complete.
+   * @type {CanvasLayer}
+   */
+  #initialLayer;
+
+  /* -------------------------------------------- */
+
+  /**
+   * Track the bound event handlers so they can be properly canceled later.
+   * @type {object}
+   */
+  #events;
+
+  /* -------------------------------------------- */
+
+  /**
    * A factory method to create an AbilityTemplate instance using provided data from an Item5e instance
    * @param {Item5e} item               The Item object for which to construct the template
    * @returns {AbilityTemplate|null}    The template object, or null if the item does not produce a template
@@ -81,62 +105,95 @@ export default class AbilityTemplate extends MeasuredTemplate {
    */
   activatePreviewListeners(initialLayer) {
     return new Promise((resolve, reject) => {
-      const handlers = {};
-      let moveTime = 0;
-
-      // Update placement (mouse-move)
-      handlers.mm = event => {
-        event.stopPropagation();
-        let now = Date.now(); // Apply a 20ms throttle
-        if ( now - moveTime <= 20 ) return;
-        const center = event.data.getLocalPosition(this.layer);
-        const snapped = canvas.grid.getSnappedPosition(center.x, center.y, 2);
-        this.document.updateSource({x: snapped.x, y: snapped.y});
-        this.refresh();
-        moveTime = now;
-      };
-
-      // Shared cancelation code
-      handlers.cancel = async event => {
-        this.layer._onDragLeftCancel(event);
-        canvas.stage.off("mousemove", handlers.mm);
-        canvas.stage.off("mousedown", handlers.lc);
-        canvas.app.view.oncontextmenu = null;
-        canvas.app.view.onwheel = null;
-        initialLayer.activate();
-        await this.actorSheet?.maximize();
-      };
-
-      // Cancel the workflow (right-click)
-      handlers.rc = async event => {
-        await handlers.cancel(event);
-        reject();
-      };
-
-      // Confirm the workflow (left-click)
-      handlers.lc = async event => {
-        await handlers.cancel(event);
-        const destination = canvas.grid.getSnappedPosition(this.document.x, this.document.y, 2);
-        this.document.updateSource(destination);
-        resolve(canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [this.document.toObject()]));
-      };
-
-      // Rotate the template by 3 degree increments (mouse-wheel)
-      handlers.mw = event => {
-        if ( event.ctrlKey ) event.preventDefault(); // Avoid zooming the browser window
-        event.stopPropagation();
-        let delta = canvas.grid.type > CONST.GRID_TYPES.SQUARE ? 30 : 15;
-        let snap = event.shiftKey ? delta : 5;
-        const update = {direction: this.document.direction + (snap * Math.sign(event.deltaY))};
-        this.document.updateSource(update);
-        this.refresh();
+      this.#initialLayer = initialLayer;
+      this.#events = {
+        cancel: this._onCancelPlacement.bind(this),
+        confirm: this._onConfirmPlacement.bind(this),
+        move: this._onMovePlacement.bind(this),
+        resolve,
+        reject,
+        rotate: this._onRotatePlacement.bind(this)
       };
 
       // Activate listeners
-      canvas.stage.on("mousemove", handlers.mm);
-      canvas.stage.on("mousedown", handlers.lc);
-      canvas.app.view.oncontextmenu = handlers.rc;
-      canvas.app.view.onwheel = handlers.mw;
+      canvas.stage.on("mousemove", this.#events.move);
+      canvas.stage.on("mousedown", this.#events.confirm);
+      canvas.app.view.oncontextmenu = this.#events.cancel;
+      canvas.app.view.onwheel = this.#events.rotate;
     });
   }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Shared code for when template placement ends by being confirmed or canceled.
+   * @param {Event} event  Triggering event that ended the placement.
+   */
+  async _finishPlacement(event) {
+    this.layer._onDragLeftCancel(event);
+    canvas.stage.off("mousemove", this.#events.move);
+    canvas.stage.off("mousedown", this.#events.confirm);
+    canvas.app.view.oncontextmenu = null;
+    canvas.app.view.onwheel = null;
+    this.#initialLayer.activate();
+    await this.actorSheet?.maximize();
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Move the template preview when the mouse moves.
+   * @param {Event} event  Triggering mouse event.
+   */
+  _onMovePlacement(event) {
+    event.stopPropagation();
+    let now = Date.now(); // Apply a 20ms throttle
+    if ( now - this.#moveTime <= 20 ) return;
+    const center = event.data.getLocalPosition(this.layer);
+    const snapped = canvas.grid.getSnappedPosition(center.x, center.y, 2);
+    this.document.updateSource({x: snapped.x, y: snapped.y});
+    this.refresh();
+    this.#moveTime = now;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Rotate the template preview by 3˚ increments when the mouse wheel is rotated.
+   * @param {Event} event  Triggering mouse event.
+   */
+  _onRotatePlacement(event) {
+    if ( event.ctrlKey ) event.preventDefault(); // Avoid zooming the browser window
+    event.stopPropagation();
+    let delta = canvas.grid.type > CONST.GRID_TYPES.SQUARE ? 30 : 15;
+    let snap = event.shiftKey ? delta : 5;
+    const update = {direction: this.document.direction + (snap * Math.sign(event.deltaY))};
+    this.document.updateSource(update);
+    this.refresh();
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Confirm placement when the left mouse button is clicked.
+   * @param {Event} event  Triggering mouse event.
+   */
+  async _onConfirmPlacement(event) {
+    await this._finishPlacement(event);
+    const destination = canvas.grid.getSnappedPosition(this.document.x, this.document.y, 2);
+    this.document.updateSource(destination);
+    this.#events.resolve(canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [this.document.toObject()]));
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Cancel placement when the right mouse button is clicked.
+   * @param {Event} event  Triggering mouse event.
+   */
+  async _onCancelPlacement(event) {
+    await this._finishPlacement(event);
+    this.#events.reject();
+  }
+
 }
