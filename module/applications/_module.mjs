7069fafdf8e60432ccc20906931f5823d09fa9ac
Kim Mantas <kim.mantas@gmail.com>
2022-08-19 20:14:13 +0100

    Initial styles for custom SRD compendium sheet.

--- module/applications/_module.mjs
+++ module/applications/_module.mjs
@@ -4,5 +4,5 @@ export * as item from "./item/_module.mjs";
 export {default as ProficiencySelector} from "./proficiency-selector.mjs";
 export {default as PropertyAttribution} from "./property-attribution.mjs";
 export {default as TraitSelector} from "./trait-selector.mjs";
-
 export {default as SelectItemsPrompt} from "./select-items-prompt.mjs";
+export {default as SRDCompendium} from "./srd-compendium.mjs";
