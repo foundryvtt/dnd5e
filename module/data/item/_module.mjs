a2d7ff3aebf7d0bf14c79d1ece068f784990f4ff
Jeff Hitchcock <mail@command-center.org>
2022-09-30 14:17:41 -0700

    [#1835] Create system data models for backgrounds & subclasses

--- module/data/item/_module.mjs
+++ module/data/item/_module.mjs
@@ -1,14 +1,18 @@
+import BackgroundData from "./background.mjs";
 import ClassData from "./class.mjs";
 import FeatData from "./feat.mjs";
 import LootData from "./loot.mjs";
 import SpellData from "./spell.mjs";
+import SubclassData from "./subclass.mjs";
 import WeaponData from "./weapon.mjs";
 
 export {
+  BackgroundData,
   ClassData,
   FeatData,
   LootData,
   SpellData,
+  SubclassData,
   WeaponData
 };
 export {default as ActionTemplate} from "./templates/action.mjs";
@@ -18,9 +22,11 @@ export {default as MountableTemplate} from "./templates/mountable.mjs";
 export {default as PhysicalItemTemplate} from "./templates/physical-item.mjs";
 
 export const config = {
+  background: BackgroundData,
   class: ClassData,
   feat: FeatData,
   loot: LootData,
   spell: SpellData,
+  subclass: SubclassData,
   weapon: WeaponData
 };
