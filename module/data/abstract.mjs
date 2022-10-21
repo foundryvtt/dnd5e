323146fd7d7defa3f1a0447e9772d313284bd5bc
Kim Mantas <kim.mantas@gmail.com>
2022-10-21 20:18:07 +0100

    Add some additional comments for reference purposes.

--- module/data/abstract.mjs
+++ module/data/abstract.mjs
@@ -3,6 +3,17 @@
  *
  * **Note**: This uses some advanced Javascript techniques that are not necessary for most data models.
  * Please refer to the [advancement data models]{@link BaseAdvancement} for an example of a more typical usage.
+ *
+ * In template.json, each Actor or Item type can incorporate several templates which are chunks of data that are
+ * common across all the types that use them. One way to represent them in the schema for a given Document type is to
+ * duplicate schema definitions for the templates and write them directly into the Data Model for the Document type.
+ * This works fine for small templates or systems that do not need many Document types but for more complex systems
+ * this boilerplate can become prohibitive.
+ *
+ * Here we have opted to instead create a separate Data Model for each template available. These define their own
+ * schemas which are then mixed-in to the final schema for the Document type's Data Model. A Document type Data Model
+ * can define its own schema unique to it, and then add templates in direct correspondence to those in template.json via
+ * SystemDataModel.mixin.
  */
 export default class SystemDataModel extends foundry.abstract.DataModel {
 
