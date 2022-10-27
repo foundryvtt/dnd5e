c807a8b38e0ccb34532f46fb419b335a6530bb2f
Jeff Hitchcock <mail@command-center.org>
2022-10-27 11:18:29 -0700

    [#1835] Create system data model for weapons (#1898)

---
+++ module/data/item/weapon.mjs
@@ -0,0 +1,58 @@
+import SystemDataModel from "../abstract.mjs";
+import { MappingField } from "../fields.mjs";
+import ActionTemplate from "./templates/action.mjs";
+import ActivatedEffectTemplate from "./templates/activated-effect.mjs";
+import ItemDescriptionTemplate from "./templates/item-description.mjs";
+import PhysicalItemTemplate from "./templates/physical-item.mjs";
+import MountableTemplate from "./templates/mountable.mjs";
+
+/**
+ * Data definition for Weapon items.
+ * @mixes ItemDescriptionTemplate
+ * @mixes PhysicalItemTemplate
+ * @mixes ActivatedEffectTemplate
+ * @mixes ActionTemplate
+ * @mixes MountableTemplate
+ *
+ * @property {string} weaponType   Weapon category as defined in `DND5E.weaponTypes`.
+ * @property {string} baseItem     Base weapon as defined in `DND5E.weaponIds` for determining proficiency.
+ * @property {object} properties   Mapping of various weapon property booleans.
+ * @property {boolean} proficient  Does the weapon's owner have proficiency?
+ */
+export default class WeaponData extends SystemDataModel.mixin(
+  ItemDescriptionTemplate, PhysicalItemTemplate, ActivatedEffectTemplate, ActionTemplate, MountableTemplate
+) {
+  static defineSchema() {
+    return this.mergeSchema(super.defineSchema(), {
+      weaponType: new foundry.data.fields.StringField({
+        required: true, initial: "simpleM", label: "DND5E.ItemWeaponType"
+      }),
+      baseItem: new foundry.data.fields.StringField({required: true, blank: true, label: "DND5E.ItemWeaponBase"}),
+      properties: new MappingField(new foundry.data.fields.BooleanField(), {
+        required: true, initialKeys: CONFIG.DND5E.weaponProperties, label: "DND5E.ItemWeaponProperties"
+      }),
+      proficient: new foundry.data.fields.BooleanField({required: true, initial: true, label: "DND5E.Proficient"})
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  static migrateData(source) {
+    this.migratePropertiesData(source);
+    return super.migrateData(source);
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Migrate the weapons's properties object to remove any old, non-boolean values.
+   * @param {object} source  The candidate source data from which the model will be constructed.
+   */
+  static migratePropertiesData(source) {
+    if ( !source.properties ) return;
+    for ( const [key, value] of Object.entries(source.properties) ) {
+      if ( typeof value !== "boolean" ) delete source.properties[key];
+    }
+  }
+}
