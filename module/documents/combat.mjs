9103f7542d110d94a8b05783c3ca278fbb735607
rtlow <rtlow@ku.edu>
2022-10-28 17:27:23 -0500

    Partial implementation of initiative

--- module/documents/combat.mjs
+++ module/documents/combat.mjs
@@ -11,26 +11,20 @@ export function _getInitiativeFormula() {
   const actor = this.actor;
   if ( !actor ) return "2d10";
   const init = actor.system.attributes.init;
+  const fin = actor.system.abilities.fin;
+  const sol = actor.system.abilities.sol;
   const rollData = actor.getRollData();
 
   // Construct initiative formula parts
-  let nd = 1;
+  let nd = 2;
   let mods = "";
   const parts = [
-    `${nd}d20${mods}`,
+    `${nd}d10${mods}`,
+    fin.value,
+    sol.value,
     init.mod,
-    (init.prof.term !== "0") ? init.prof.term : null,
     (init.bonus !== 0) ? init.bonus : null
   ];
 
-  // Ability Check Bonuses TODO: probably will need to revamp this
-  const dexCheckBonus = actor.system.abilities.dex?.bonuses?.check;
-  const globalCheckBonus = actor.system.bonuses?.abilities?.check;
-  if ( dexCheckBonus ) parts.push(Roll.replaceFormulaData(dexCheckBonus, rollData));
-  if ( globalCheckBonus ) parts.push(Roll.replaceFormulaData(globalCheckBonus, rollData));
-
-  // Optionally apply Dexterity tiebreaker
-  const tiebreaker = game.settings.get("shaper", "initiativeDexTiebreaker");
-  if ( tiebreaker ) parts.push((actor.system.abilities.dex?.value ?? 0) / 100);
   return parts.filter(p => p !== null).join(" + ");
 }
