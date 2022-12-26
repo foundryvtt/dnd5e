8c055f6ef043f7ccbef9ac9d7de2a56529aff17b
Kenneth B <kennethzhell@gmail.com>
2022-12-26 03:59:13 +0100

    Revert `fromUuidSync` change.

--- module/documents/active-effect.mjs
+++ module/documents/active-effect.mjs
@@ -56,13 +56,21 @@ export default class ActiveEffect5e extends ActiveEffect {
    */
   determineSuppression() {
     this.isSuppressed = false;
-    if ( this.disabled || (this.parent.documentName !== "Actor") || !this.origin ) return;
-    try {
-      // Determine if this is an effect from an item
-      const item = fromUuidSync(this.origin);
-      if ( item?.parent !== this.parent ) return;
-      this.isSuppressed = item.areEffectsSuppressed;
-    } catch(e) {}
+    if ( this.disabled || (this.parent.documentName !== "Actor") ) return;
+    const [parentType, parentId, documentType, documentId, syntheticItem, syntheticItemId] = this.origin?.split(".") ?? [];
+    // Case 1: This is a linked or sidebar actor
+    if ( parentType === "Actor" ) {
+      if ( (parentId !== this.parent.id) || (documentType !== "Item") ) return;
+    }
+    // Case 2: This is a synthetic actor on the scene
+    else if ( parentType === "Scene" ) {
+      if ( (documentId !== this.parent.token?.id) || (syntheticItem !== "Item") ) return;
+    }
+    // Case 3: This is not an actor
+    else return;
+    const item = this.parent.items.get(documentId) ?? this.parent.items.get(syntheticItemId);
+    if ( !item ) return;
+    this.isSuppressed = item.areEffectsSuppressed;
   }
 
   /* --------------------------------------------- */
