b726751cc5c99f09a920dbb2c033c0337e69301d
Jeff Hitchcock <mail@command-center.org>
2023-01-17 12:16:21 -0800

    [#2054] Fix crew & passengers sections not displaying on vehicle sheet

--- module/utils.mjs
+++ module/utils.mjs
@@ -145,12 +145,34 @@ export async function preloadHandlebarsTemplates() {
 /* -------------------------------------------- */
 
 /**
+ * A helper that fetch the appropriate item context from root and adds it to the first block parameter.
+ * @param {object} context  Current evaluation context.
+ * @param {object} options  Handlebars options.
+ * @returns {string}
+ */
+function itemContext(context, options) {
+  if ( arguments.length !== 2 ) throw new Error("#dnd5e-with requires exactly one argument");
+  if ( foundry.utils.getType(context) === "function" ) context = context.call(this);
+
+  const ctx = options.data.root.itemContext?.[context.id];
+  if ( !ctx ) {
+    const inverse = options.inverse(this);
+    if ( inverse ) return options.inverse(this);
+  }
+
+  return options.fn(context, { data: options.data, blockParams: [ctx] });
+}
+
+/* -------------------------------------------- */
+
+/**
  * Register custom Handlebars helpers used by 5e.
  */
 export function registerHandlebarsHelpers() {
   Handlebars.registerHelper({
     getProperty: foundry.utils.getProperty,
-    "dnd5e-linkForUuid": linkForUuid
+    "dnd5e-linkForUuid": linkForUuid,
+    "dnd5e-itemContext": itemContext
   });
 }
 
