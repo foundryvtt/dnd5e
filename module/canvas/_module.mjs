00d3c2d48f656acb1b7a7f62060ecf7ac6eb0d17
dev7355608 <dev7355608@gmail.com>
2023-04-15 19:35:14 +0200

    Replace See Invisibility detection mode

--- module/canvas/_module.mjs
+++ module/canvas/_module.mjs
@@ -1,3 +1,4 @@
 export {default as AbilityTemplate} from "./ability-template.mjs";
+export * from "./detection-mode.mjs";
 export {default as Token5e} from "./token.mjs";
 export {measureDistances} from "./grid.mjs";
