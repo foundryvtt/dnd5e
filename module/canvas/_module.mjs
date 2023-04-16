e6c5b502648cb8189a2a78550ee4af86a62c0e49
dev7355608 <dev7355608@gmail.com>
2023-04-16 19:09:13 +0200

    Refactor

--- module/canvas/_module.mjs
+++ module/canvas/_module.mjs
@@ -1,4 +1,4 @@
 export {default as AbilityTemplate} from "./ability-template.mjs";
-export * from "./detection-mode.mjs";
+export * as detectionModes from "./detection-modes/_module.mjs";
 export {default as Token5e} from "./token.mjs";
 export {measureDistances} from "./grid.mjs";
