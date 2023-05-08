2e8ef78c82076eacae1988b87432c220680afdad
Kenneth B <kennethzhell@gmail.com>
2023-05-08 11:23:24 +0200

    missing 'new'

--- module/documents/combat.mjs
+++ module/documents/combat.mjs
@@ -5,6 +5,6 @@
  * @returns {D20Roll}         The D20Roll instance which is used to determine initiative for the Combatant
  */
 export function getInitiativeRoll(formula="1d20") {
-  if ( !this.actor ) return CONFIG.Dice.D20Roll(formula, {});
+  if ( !this.actor ) return new CONFIG.Dice.D20Roll(formula, {});
   return this.actor.getInitiativeRoll();
 }
