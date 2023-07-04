55eb29497f85675228e6de84e8f7d6f74f17a5e7
Kim Mantas <kim.mantas@gmail.com>
2023-07-04 19:58:21 +0100

    [#2332] Fix vehicle actors not having HP as an available trackable bar.

--- dnd5e.mjs
+++ dnd5e.mjs
@@ -185,7 +185,7 @@ function _configureTrackableAttributes() {
       value: [...creature.value, "details.cr", "details.spellLevel", "details.xp.value"]
     },
     vehicle: {
-      bar: [...common.bar],
+      bar: [...common.bar, "attributes.hp"],
       value: [...common.value]
     },
     group: {
