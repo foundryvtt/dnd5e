4e81788c0f82f36a91a28965127bbecc7827fa58
Zhell <zhellqol@gmail.com>
2023-08-28 15:40:40 +0200

    [#2359]

--- module/documents/combat.mjs
+++ module/documents/combat.mjs
@@ -15,21 +15,31 @@ export function getInitiativeRoll(formula="1d20") {
  */
 export default class Combat5e extends Combat {
   /** @inheritdoc */
-  async _onStartTurn(combatant) {
-    await this.resetLegendaryActions(combatant.actor);
-    return super._onStartTurn(combatant);
+  async _onUpdate(data, options, userId) {
+    super._onUpdate(data, options, userId);
+    if ( options.direction === 1 ) {
+      this.resetLegendaryActions(this.combatant.actor, {whisper: this.combatant.hidden});
+    }
   }
 
   /**
    * Reset the legendary actions of an npc-type actor.
-   * @param {Actor5e} actor         The actor whose actions to reset.
-   * @returns {Promise<Actor5e>}    The actor being updated, if applicable.
+   * @param {Actor5e} actor                       The actor whose actions to reset.
+   * @param {object} [options={}]                 Options that modify the update and message.
+   * @param {boolean} [options.message=true]      Whether to display a chat message.
+   * @param {boolean} [options.whisper=false]     Whether to whisper the message to the owner.
+   * @returns {Promise<Actor5e>}                  The actor being updated, if applicable.
    */
-  async resetLegendaryActions(actor) {
-    if ( !actor || !this.active ) return;
+  async resetLegendaryActions(actor, {message=true, whisper=false}={}) {
+    if ( !actor || !this.active ) return null;
     const leg = actor.system.resources?.legact ?? {};
-    if ( actor.isOwner && !!leg.max && (leg.value < leg.max) ) {
-      ui.notifications.info(game.i18n.format("DND5E.LegendaryActionReset", {name: actor.name}));
+    const user = game.users.find(u => u.active && actor.testUserPermission(u, "OWNER"));
+    if ( (game.user === user) && !!leg.max && (leg.value < leg.max) ) {
+      if ( message ) ChatMessage.create({
+        content: game.i18n.format("DND5E.LegendaryActionReset", {name: actor.name}),
+        speaker: ChatMessage.getSpeaker({actor}),
+        whisper: whisper ? [user.id] : []
+      });
       return actor.update({"system.resources.legact.value": leg.max});
     }
   }
