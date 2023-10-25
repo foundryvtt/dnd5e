ca94fb996cc591c40459492afa7f99a0391c39f5
Kim Mantas <kim.mantas@gmail.com>
2023-10-25 18:47:23 +0100

    Use <label> tag rather than rollable style.

--- module/applications/actor/character-sheet.mjs
+++ module/applications/actor/character-sheet.mjs
@@ -231,8 +231,6 @@ export default class ActorSheet5eCharacter extends ActorSheet5e {
         return this.actor.rollDeathSave({event: event});
       case "rollInitiative":
         return this.actor.rollInitiativeDialog({event});
-      case "toggleInspiration":
-        return this.actor.update({"system.attributes.inspiration": !this.actor.system.attributes.inspiration});
     }
   }
 
