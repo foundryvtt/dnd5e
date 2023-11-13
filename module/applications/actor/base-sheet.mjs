532927208bc9e5958483f8d4ef69148116e1ff65
Zhell <50169243+krbz999@users.noreply.github.com>
2023-11-13 16:21:44 +0100

    [#2350] Require being a non-compendium actor to make any rolls on the sheet (#2577)
    Co-authored-by: Zhell <zhellqol@gmail.com>
    Co-authored-by: Kim Mantas <kim.mantas@gmail.com>

--- module/applications/actor/base-sheet.mjs
+++ module/applications/actor/base-sheet.mjs
@@ -663,8 +663,8 @@ export default class ActorSheet5e extends ActorSheetMixin(ActorSheet) {
       this._disableOverriddenFields(html);
     }
 
-    // Owner Only Listeners
-    if ( this.actor.isOwner ) {
+    // Owner Only Listeners, for non-compendium actors.
+    if ( this.actor.isOwner && !this.actor.compendium ) {
 
       // Ability Checks
       html.find(".ability-name").click(this._onRollAbilityTest.bind(this));
@@ -678,15 +678,15 @@ export default class ActorSheet5e extends ActorSheetMixin(ActorSheet) {
       // Item Rolling
       html.find(".rollable .item-image").click(event => this._onItemUse(event));
       html.find(".item .item-recharge").click(event => this._onItemRecharge(event));
-
-      // Item Context Menu
-      new ContextMenu(html, ".item-list .item", [], {onOpen: this._onItemContext.bind(this)});
     }
 
     // Otherwise, remove rollable classes
     else {
       html.find(".rollable").each((i, el) => el.classList.remove("rollable"));
     }
+
+    // Item Context Menu
+    new ContextMenu(html, ".item-list .item", [], {onOpen: this._onItemContext.bind(this)});
 
     // Handle default listeners last so system listeners are triggered first
     super.activateListeners(html);
