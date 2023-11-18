dda179adfc62e59cc73a5e64218a1d3af7c37a9c
Zhell <zhellqol@gmail.com>
2023-11-18 16:39:09 +0100

    Scale spells if they consume slot as resource

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -850,6 +850,7 @@ export default class Item5e extends SystemDocumentMixin(Item) {
     // Handle upcasting
     if ( item.type === "spell" ) {
       let level = null;
+      if ( config.resourceAmount in as.spells ) config.slotLevel = config.resourceAmount;
       if ( config.slotLevel ) {
         // A spell slot was consumed.
         level = Number.isInteger(config.slotLevel) ? config.slotLevel
@@ -1087,13 +1088,17 @@ export default class Item5e extends SystemDocumentMixin(Item) {
       return false;
     }
 
+    const as = this.actor.system;
     // Identify the consumed resource and its current quantity
     let resource = null;
     let amount = usageConfig.resourceAmount ? usageConfig.resourceAmount : (consume.amount || 1);
+    if ( amount in as.spells ) amount = consume.amount || 1;
     let quantity = 0;
     switch ( consume.type ) {
       case "attribute":
-        resource = foundry.utils.getProperty(this.actor.system, consume.target);
+        const amt = usageConfig.resourceAmount;
+        const target = (amt in as.spells) ? `spells.${amt}.value` : consume.target;
+        resource = foundry.utils.getProperty(as, target);
         quantity = resource || 0;
         break;
       case "ammo":
@@ -1134,7 +1139,9 @@ export default class Item5e extends SystemDocumentMixin(Item) {
     // Define updates to provided data objects
     switch ( consume.type ) {
       case "attribute":
-        actorUpdates[`system.${consume.target}`] = remaining;
+        const amt = usageConfig.resourceAmount;
+        const target = (amt in as.spells) ? `spells.${amt}.value` : consume.target;
+        actorUpdates[`system.${target}`] = remaining;
         break;
       case "ammo":
       case "material":
