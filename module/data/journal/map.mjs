3447ad3dc224c87982fbd874efefd6f30d88c1ec
Jeff Hitchcock <mail@command-center.org>
2023-11-22 11:33:38 -0800

    Rename flag to mapMarkerStyle

--- module/data/journal/map.mjs
+++ module/data/journal/map.mjs
@@ -35,7 +35,7 @@ export default class MapLocationJournalPageData extends foundry.abstract.DataMod
     if ( !this.code ) return;
     const style = foundry.utils.mergeObject(
       CONFIG.DND5E.mapLocationMarker.default,
-      CONFIG.DND5E.mapLocationMarker[this.parent.getFlag("dnd5e", "style")] ?? {},
+      CONFIG.DND5E.mapLocationMarker[this.parent.getFlag("dnd5e", "mapMarkerStyle")] ?? {},
       {inplace: false}
     );
     return new MapLocationControlIcon({code: this.code, ...options, ...style});
