10a1e2eb3c5fe9c3c3ef576de394e357b6b619c1
Jeff Hitchcock <mail@command-center.org>
2023-11-24 13:47:50 -0800

    Rework SRDCompendium into generic TableOfContentsCompendium

--- module/applications/journal/_module.mjs
+++ module/applications/journal/_module.mjs
@@ -2,4 +2,4 @@ export {default as JournalClassPageSheet} from "./class-page-sheet.mjs";
 export {default as JournalEditor} from "./journal-editor.mjs";
 export {default as JournalMapLocationPageSheet} from "./map-page-sheet.mjs";
 export {default as JournalSheet5e} from "./journal-sheet.mjs";
-export {default as SRDCompendium} from "./srd-compendium.mjs";
+export {default as TableOfContentsCompendium} from "./table-of-contents.mjs";
