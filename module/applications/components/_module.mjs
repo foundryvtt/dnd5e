cc5a3d118c56c38f5c1911961bde8600929aa5ef
Jeff Hitchcock <mail@command-center.org>
2023-12-23 13:16:39 -0800

    Create custom element for filigree box to allow color customization

--- module/applications/components/_module.mjs
+++ module/applications/components/_module.mjs
@@ -1,10 +1,12 @@
 import EffectsElement from "./effects.mjs";
+import FiligreeBoxElement from "./filigree-box.mjs";
 import InventoryElement from "./inventory.mjs";
 import ProficiencyCycleElement from "./proficiency-cycle.mjs";
 import SlideToggleElement from "./slide-toggle.mjs";
 
 window.customElements.define("dnd5e-effects", EffectsElement);
 window.customElements.define("dnd5e-inventory", InventoryElement);
+window.customElements.define("filigree-box", FiligreeBoxElement);
 window.customElements.define("proficiency-cycle", ProficiencyCycleElement);
 window.customElements.define("slide-toggle", SlideToggleElement);
 
