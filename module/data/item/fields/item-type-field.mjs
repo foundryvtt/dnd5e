e9c1f74d4a3f48cd1d622742eb3b21265010bdd0
Jeff Hitchcock <mail@command-center.org>
2023-12-28 09:12:27 -0800

    [#2168] Adjust type field options, add subtype back to loot items

--- module/data/item/fields/item-type-field.mjs
+++ module/data/item/fields/item-type-field.mjs
@@ -1,29 +1,27 @@
 /**
  * A field for storing Item type data.
  *
- * @param {object} [defaults={}]                   Options to configure this field's behavior.
- * @param {string} [defaults.value]                An initial value for the Item's type.
- * @param {string} [defaults.subtype]              An initial value for the Item's subtype.
- * @param {string} [defaults.baseItem]             An initial value for the Item's baseItem.
- * @param {DataFieldOptions} [schemaOptions]       Options forwarded to the SchemaField.
- * @param {boolean} [schemaOptions.subtype=true]   Include the subtype field.
- * @param {boolean} [schemaOptions.baseItem=true]  Include the baseItem field.
+ * @param {object} [options={}]                   Options to configure this field's behavior.
+ * @param {string} [options.value]                An initial value for the Item's type.
+ * @param {string|boolean} [options.subtype]      An initial value for the Item's subtype, or false to exclude it.
+ * @param {string|boolean} [options.baseItem]     An initial value for the Item's baseItem, or false to exclude it.
+ * @param {DataFieldOptions} [schemaOptions={}]   Options forwarded to the SchemaField.
  */
 export default class ItemTypeField extends foundry.data.fields.SchemaField {
-  constructor(defaults={}, { subtype=true, baseItem=true, ...schemaOptions}={}) {
+  constructor(options={}, schemaOptions={}) {
     const fields = {
       value: new foundry.data.fields.StringField({
-        required: true, blank: true, initial: defaults.value ?? "", label: "DND5E.Type"
+        required: true, blank: true, initial: options.value ?? "", label: "DND5E.Type"
       }),
       subtype: new foundry.data.fields.StringField({
-        required: true, blank: true, initial: defaults.subtype ?? "", label: "DND5E.Subtype"
+        required: true, blank: true, initial: options.subtype ?? "", label: "DND5E.Subtype"
       }),
       baseItem: new foundry.data.fields.StringField({
-        required: true, blank: true, initial: defaults.baseItem ?? "", label: "DND5E.BaseItem"
+        required: true, blank: true, initial: options.baseItem ?? "", label: "DND5E.BaseItem"
       })
     };
-    if ( !subtype ) delete fields.subtype;
-    if ( !baseItem ) delete fields.baseItem;
+    if ( options.subtype === false ) delete fields.subtype;
+    if ( options.baseItem === false ) delete fields.baseItem;
     super(fields, schemaOptions);
   }
 }
