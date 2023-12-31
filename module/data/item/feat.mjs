3ab05d182615758eb9462cfde97c19a4127bb311
Zhell <50169243+krbz999@users.noreply.github.com>
2023-12-31 10:48:33 +0100

    Update module/data/item/feat.mjs
    Co-authored-by: Jeff Hitchcock <github444@command-center.org>

--- module/data/item/feat.mjs
+++ module/data/item/feat.mjs
@@ -12,7 +12,7 @@ import ItemTypeField from "./fields/item-type-field.mjs";
  * @mixes ActivatedEffectTemplate
  * @mixes ActionTemplate
  *
- * @property {Set<string>} properties                    General properties of a feature item.
+ * @property {Set<string>} properties               General properties of a feature item.
  * @property {string} requirements                  Actor details required to use this feature.
  * @property {object} recharge                      Details on how a feature can roll for recharges.
  * @property {number} recharge.value                Minimum number needed to roll on a d6 to recharge this feature.
