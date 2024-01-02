5639601403eedc2870734609dc089aca88605ef4
Zhell <zhellqol@gmail.com>
2024-01-02 13:56:21 +0100

    Format

--- module/applications/item/item-sheet.mjs
+++ module/applications/item/item-sheet.mjs
@@ -139,10 +139,9 @@ export default class ItemSheet5e extends ItemSheet {
       const isAmmo = item.system.type?.value === "ammo";
       const valids = CONFIG.DND5E.validProperties[item.type];
       context.properties = Object.entries(CONFIG.DND5E.itemProperties).reduce((obj, [k, v]) => {
-        if ( valids.has(k) || (isAmmo && v.isPhysical)) obj[k] = {
-          label: v.label,
-          selected: item.system.properties.has(k)
-        };
+        if ( valids.has(k) || (isAmmo && v.isPhysical) ) {
+          obj[k] = { label: v.label, selected: item.system.properties.has(k) };
+        }
         return obj;
       }, {});
       if ( item.type !== "spell" ) context.properties = sortObjectEntries(context.properties, "label");
