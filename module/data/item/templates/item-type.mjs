5493b444b8ab93319fb2a2fb1d55abb025dbe2ea
Zhell <zhellqol@gmail.com>
2024-01-28 22:36:38 +0100

    Fix type migration

--- module/data/item/templates/item-type.mjs
+++ module/data/item/templates/item-type.mjs
@@ -28,9 +28,8 @@ export default class ItemTypeTemplate extends SystemDataModel {
    * @param {object} source  The candidate source data from which the model will be constructed.
    */
   static #migrateType(source) {
-    if ( foundry.utils.getType(source.type) === "Object" ) return;
     const oldType = source.consumableType ?? source.armor?.type ?? source.toolType ?? source.weaponType;
-    if ( (oldType !== null) && (oldType !== undefined) ) foundry.utils.setProperty(source, "type.value", oldType);
+    if ( oldType ) foundry.utils.setProperty(source, "type.value", oldType);
     if ( "baseItem" in source ) foundry.utils.setProperty(source, "type.baseItem", source.baseItem);
   }
 }
