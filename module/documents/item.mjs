db1a831f53756298a8269f57455d4f15741f8852
Jeff Hitchcock <mail@command-center.org>
2024-01-31 15:50:07 -0800

    [#2767] Fix error thrown when canceling template placement from chat card

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -1936,8 +1936,11 @@ export default class Item5e extends SystemDocumentMixin(Item) {
           try {
             await dnd5e.canvas.AbilityTemplate.fromItem(item, {"flags.dnd5e.spellLevel": spellLevel})?.drawPreview();
           } catch(err) {
-            err.message = game.i18n.localize("DND5E.PlaceTemplateError");
-            throw err;
+            Hooks.onError("Item5e#_onChatCardAction", err, {
+              msg: game.i18n.localize("DND5E.PlaceTemplateError"),
+              log: "error",
+              notify: "error"
+            });
           }
           break;
         case "abilityCheck":
