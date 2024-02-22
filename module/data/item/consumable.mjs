ebb16824e5b5890eb538bb17f638c2b1424d2c5e
Jeff Hitchcock <mail@command-center.org>
2024-02-22 10:11:56 -0800

    [#3091] Fix duplicated feature type label in header if no sub-type is selected

--- module/data/item/consumable.mjs
+++ module/data/item/consumable.mjs
@@ -71,7 +71,7 @@ export default class ConsumableData extends ItemDataModel.mixin(
     if ( !this.type.value ) return;
     const config = CONFIG.DND5E.consumableTypes[this.type.value];
     if ( config ) {
-      this.type.label = this.type.subtype ? config.subtypes[this.type.subtype] : config.label;
+      this.type.label = config.subtypes?.[this.type.subtype] ?? config.label;
     } else {
       this.type.label = game.i18n.localize(CONFIG.Item.typeLabels.consumable);
     }
