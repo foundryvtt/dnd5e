5a98b7cc1d041d198bb60e5c5986309a6e25465e
Jeff Hitchcock <mail@command-center.org>
2024-03-06 13:44:05 -0800

    [#2864] Disable sheet-specific theming

--- module/applications/actor/character-sheet-2.mjs
+++ module/applications/actor/character-sheet-2.mjs
@@ -164,7 +164,8 @@ export default class ActorSheet5eCharacter2 extends ActorSheet5eCharacter {
     });
 
     // Set theme
-    setTheme(html[0], this.actor.getFlag("dnd5e", "theme"));
+    // TODO: Re-enable this when we support V12 only
+    // setTheme(html[0], this.actor.getFlag("dnd5e", "theme"));
 
     return html;
   }
