1c5b5899de801ae95a21a9784350676d3f95f87b
Jeff Hitchcock <mail@command-center.org>
2024-03-14 11:28:45 -0700

    [#3250] Aggregate damage types before passing to `applyDamage`
    Adds a new dice helper function `aggregateDamageRolls` which splits
    and re-combines damage rolls based on the roll's type, term flavor,
    and optionally any roll properties. This function is then used by
    `ChatMessage5e#applyChatCardDamage` before rolls are totaled to
    ensure that damage types are properly split.
    
    The function starts by using a `chunkTerms` helper function which
    goes through the terms in a roll grouping into chunks based on the
    operators. Addition and subtraction cause new chunks to be created
    while multiplication and division are included in the same chunk.
    This is so that a damage type defined in flavor of a term can spread
    to other terms that it is multiplied or divided with. So if you have
    a formula `1d6 + 5 * 1d4[fire]` that has the overall type of piercing,
    the `1d6` will remaining piercing while the `5 * 1d4` will all be
    treated as fire.
    
    Once the chunking is complete then chunks from all different rolls
    are combined into lists of terms grouped by type and properties
    (so long as the `respectProperties` option is set, otherwise they
    will only be grouped by type). These sets are then converted into
    new `DamageRoll`s with options set appropriately.

---
+++ module/dice/aggregate-damage-rolls.mjs
@@ -0,0 +1,77 @@
+/**
+ * Parse the provided rolls, splitting parts based on damage types & properties, taking flavor into account.
+ * @param {DamageRoll[]} rolls                   Evaluated damage rolls to aggregate.
+ * @param {object} [options={}]
+ * @param {boolean} [options.respectProperties]  Should damage properties also affect grouping?
+ * @returns {DamageRoll[]}
+ */
+export default function aggregateDamageRolls(rolls, { respectProperties }={}) {
+  const makeHash = (type, properties=[]) => [type, ...(respectProperties ? Array.from(properties).sort() : [])].join();
+
+  // Split rolls into new sets of terms based on damage type & properties
+  const types = new Map();
+  for ( const roll of rolls ) {
+    if ( !roll._evaluated ) throw new Error("Only evaluated rolls can be aggregated.");
+    for ( const chunk of chunkTerms(roll.terms, roll.options.type) ) {
+      const key = makeHash(chunk.type, roll.options.properties);
+      if ( !types.has(key) ) types.set(key, { type: chunk.type, properties: new Set(), terms: [] });
+      const data = types.get(key);
+      data.terms.push(new OperatorTerm({ operator: chunk.negative ? "-" : "+" }), ...chunk.terms);
+      if ( roll.options.properties ) data.properties = data.properties.union(new Set(roll.options.properties));
+    }
+  }
+
+  // Create new damage rolls based on the aggregated terms
+  const newRolls = [];
+  for ( const type of types.values() ) {
+    const roll = new CONFIG.Dice.DamageRoll();
+    roll.terms = type.terms;
+    roll._total = roll._evaluateTotal();
+    roll._evaluated = true;
+    roll.options = { type: type.type, properties: Array.from(type.properties) };
+    roll.resetFormula();
+    newRolls.push(roll);
+  }
+
+  return newRolls;
+}
+
+/* -------------------------------------------- */
+
+/**
+ * Split terms into groups based on operators. Addition & subtraction will split groups while multiplication and
+ * division will keep groups together. These groups also contain information on contained types written in flavor
+ * and whether they are negative.
+ * @param {RollTerm[]} terms  Terms to chunk.
+ * @param {string} type       Type specified in the roll as a whole which will be ignored if used as flavor.
+ * @returns {{ terms: RollTerm[], negative: boolean, type: string }[]}
+ */
+function chunkTerms(terms, type) {
+  const pushChunk = () => {
+    currentChunk.type ??= type;
+    chunks.push(currentChunk);
+    currentChunk = null;
+  };
+  const isValidType = t => ((t in CONFIG.DND5E.damageTypes) || (t in CONFIG.DND5E.healingTypes)) && (t !== type);
+  const chunks = [];
+  let currentChunk;
+  let negative = false;
+
+  for ( const term of terms ) {
+    // Plus or minus operators split chunks
+    if ( (term instanceof OperatorTerm) && ["+", "-"].includes(term.operator) ) {
+      if ( currentChunk ) pushChunk();
+      negative = term.operator === "-";
+      continue;
+    }
+
+    // All other terms get added to the current chunk
+    currentChunk ??= { terms: [], negative, type: null };
+    currentChunk.terms.push(term);
+    const flavor = term.flavor?.toLowerCase().trim();
+    if ( isValidType(flavor) ) currentChunk.type ??= flavor;
+  }
+
+  if ( currentChunk ) pushChunk();
+  return chunks;
+}
