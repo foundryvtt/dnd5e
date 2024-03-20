87cbc8ba19704da4abb4b31273b3edb7c12bc2b8
Kim Mantas <kim.mantas@gmail.com>
2024-03-20 14:23:42 +0000

    [#3028] Add concentration configuration.

--- module/applications/actor/base-sheet.mjs
+++ module/applications/actor/base-sheet.mjs
@@ -4,6 +4,7 @@ import EffectsElement from "../components/effects.mjs";
 
 import ActorAbilityConfig from "./ability-config.mjs";
 import ActorArmorConfig from "./armor-config.mjs";
+import ActorConcentrationConfig from "./concentration-config.mjs";
 import ActorHitDiceConfig from "./hit-dice-config.mjs";
 import ActorHitPointsConfig from "./hit-points-config.mjs";
 import ActorInitiativeConfig from "./initiative-config.mjs";
@@ -750,7 +751,8 @@ export default class ActorSheet5e extends ActorSheetMixin(ActorSheet) {
         break;
       case "ability":
         const ability = event.currentTarget.closest("[data-ability]").dataset.ability;
-        app = new ActorAbilityConfig(this.actor, null, ability);
+        if ( ability === "concentration" ) app = new ActorConcentrationConfig(this.actor);
+        else app = new ActorAbilityConfig(this.actor, null, ability);
         break;
       case "skill":
         const skill = event.currentTarget.closest("[data-key]").dataset.key;
