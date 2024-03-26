97ecbdedaf13c8ea5cc2e34cd235ed6f072da8b0
Zhell <zhellqol@gmail.com>
2024-03-26 16:00:30 +0100

    Add drop and count

--- module/utils.mjs
+++ module/utils.mjs
@@ -50,7 +50,8 @@ export function isValidDieModifier(mod) {
     explode: /xo?([0-9]+)?([<>=]+)?([0-9]+)?/i,
     minimum: /(?:min)([0-9]+)/i,
     maximum: /(?:max)([0-9]+)/i,
-    keep: /k([hl])?([0-9]+)?/i
+    dropKeep: /[dk]([hl])?([0-9]+)?/i,
+    count: /(?:c[sf])([<>=]+)?([0-9]+)?/i
   };
   return Object.values(regex).some(rgx => rgx.test(mod));
 }
