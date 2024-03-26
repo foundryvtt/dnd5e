f6adee3a720b3b0e34b551aa493f3b79137dd5a9
Zhell <50169243+krbz999@users.noreply.github.com>
2024-03-26 16:17:40 +0100

    Add kh and kl as valid dice modifiers in Dice Scale Values (#3329)
    Co-authored-by: Zhell <zhellqol@gmail.com>

--- module/utils.mjs
+++ module/utils.mjs
@@ -49,7 +49,9 @@ export function isValidDieModifier(mod) {
     reroll: /rr?([0-9]+)?([<>=]+)?([0-9]+)?/i,
     explode: /xo?([0-9]+)?([<>=]+)?([0-9]+)?/i,
     minimum: /(?:min)([0-9]+)/i,
-    maximum: /(?:max)([0-9]+)/i
+    maximum: /(?:max)([0-9]+)/i,
+    dropKeep: /[dk]([hl])?([0-9]+)?/i,
+    count: /(?:c[sf])([<>=]+)?([0-9]+)?/i
   };
   return Object.values(regex).some(rgx => rgx.test(mod));
 }
