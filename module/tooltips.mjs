2f4190000a73877ece38e53f0d0d864ad0485900
Jeff Hitchcock <mail@command-center.org>
2024-04-23 10:56:54 -0700

    [#3071] Fix rendering issues with UP & DOWN item tooltips

--- module/tooltips.mjs
+++ module/tooltips.mjs
@@ -176,21 +176,7 @@ export default class Tooltips5e {
    * @protected
    */
   _positionItemTooltip(direction=TooltipManager.TOOLTIP_DIRECTIONS.LEFT) {
-    const tooltip = this.tooltip;
-    const { clientWidth, clientHeight } = document.documentElement;
-    const tooltipBox = tooltip.getBoundingClientRect();
-    const targetBox = game.tooltip.element.getBoundingClientRect();
-    const maxTop = clientHeight - tooltipBox.height;
-    const top = Math.min(maxTop, targetBox.bottom - ((targetBox.height + tooltipBox.height) / 2));
-    const left = targetBox.left - tooltipBox.width - game.tooltip.constructor.TOOLTIP_MARGIN_PX;
-    const right = targetBox.right + game.tooltip.constructor.TOOLTIP_MARGIN_PX;
-    const { RIGHT, LEFT } = TooltipManager.TOOLTIP_DIRECTIONS;
-    if ( (direction === LEFT) && (left < 0) ) direction = RIGHT;
-    else if ( (direction === RIGHT) && (right + targetBox.width > clientWidth) ) direction = LEFT;
-    tooltip.style.top = `${Math.max(0, top)}px`;
-    tooltip.style.right = "";
-    if ( direction === RIGHT ) tooltip.style.left = `${Math.min(right, clientWidth - tooltipBox.width)}px`;
-    else tooltip.style.left = `${Math.max(0, left)}px`;
+    game.tooltip._setAnchor(direction);
 
     // Set overflowing styles for item tooltips.
     if ( tooltip.classList.contains("item-tooltip") ) {
