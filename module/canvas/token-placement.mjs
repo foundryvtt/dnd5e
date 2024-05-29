bd2a299e28b0e45cc0e35245835b387092671c22
Hoppyhob <Hoppyhob@gmail.com>
2024-05-29 16:55:01 -0500

    [#3659] Added clear call for preview
    Added clear call for current placement preview if placement was false.
    Closes #3659

--- module/canvas/token-placement.mjs
+++ module/canvas/token-placement.mjs
@@ -111,6 +111,7 @@ export default class TokenPlacement {
         obj.eventMode = "none";
         const placement = await this.#requestPlacement();
         if ( placement ) placements.push(placement);
+        else this.#previews[this.#currentPlacement]?.object.clear();
       }
       return placements;
     } finally {
