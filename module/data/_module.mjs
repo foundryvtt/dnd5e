db402a20fb358e832e6ee025cb28c22a55b57429
Jeff Hitchcock <mail@command-center.org>
2024-06-25 14:08:19 -0700

    [#461, #1964] Build base data model for activities
    Adds a data model representing a base activity with all of the
    information that will be shared between all activities.

--- module/data/_module.mjs
+++ module/data/_module.mjs
@@ -1,6 +1,7 @@
 export {default as SystemDataModel, ActorDataModel, ItemDataModel, SparseDataModel} from "./abstract.mjs";
 export * as fields from "./fields.mjs";
 
+export * as activity from "./activity/_module.mjs";
 export * as actor from "./actor/_module.mjs";
 export * as advancement from "./advancement/_module.mjs";
 export * as collection from "./collection/_module.mjs";
---
+++ module/data/activity/_module.mjs
@@ -0,0 +1,1 @@
+export {default as BaseActivity} from "./base-activity.mjs";
