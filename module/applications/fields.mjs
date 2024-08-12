90e4946086cf126cbd83008a08ed5acf43fea07e
Kim Mantas <kim.mantas@gmail.com>
2024-08-12 13:57:32 +0100

    Item Sheet V2: Non-Activated Items (#3967)

--- module/applications/fields.mjs
+++ module/applications/fields.mjs
@@ -1,4 +1,44 @@
 /**
+ * Create a checkbox input for a BooleanField.
+ * @param {BooleanField} field               The field.
+ * @param {FormInputConfig<boolean>} config  The input configuration.
+ * @returns {HTMLElement}
+ */
+export function createCheckboxInput(field, config) {
+  const input = document.createElement("dnd5e-checkbox");
+  input.name = config.name;
+  if ( config.value ) input.checked = true;
+  foundry.applications.fields.setInputAttributes(input, config);
+  if ( "ariaLabel" in config ) input.ariaLabel = config.ariaLabel;
+  return input;
+}
+
+/* -------------------------------------------- */
+
+/**
+ * Create a grid of checkboxes.
+ * @param {DataField} field         The field.
+ * @param {FormInputCOnfig} config  The input configuration.
+ * @returns {HTMLCollection}
+ */
+export function createMultiCheckboxInput(field, config) {
+  const template = document.createElement("template");
+  for ( const option of config.options || [] ) {
+    const { label, value, selected } = option;
+    const element = document.createElement("label");
+    element.classList.add("checkbox");
+    element.innerHTML = `
+      <dnd5e-checkbox name="${config.name}.${value}" ${selected ? "checked" : ""}></dnd5e-checkbox>
+      <span>${label}</span>
+    `;
+    template.content.append(element);
+  }
+  return template.content.children;
+}
+
+/* -------------------------------------------- */
+
+/**
  * Create a text input for a StringField.
  * @param {StringField} field               The field.
  * @param {FormInputConfig<string>} config  The input configuration.
