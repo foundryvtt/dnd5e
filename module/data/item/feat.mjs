fd2b1fc90e5b25b991ee60c1c64112c8e9562d02
Kim Mantas <kim.mantas@gmail.com>
2024-08-12 14:12:58 +0100

    Item Sheets V2: Activated Items

--- module/data/item/feat.mjs
+++ module/data/item/feat.mjs
@@ -148,6 +148,7 @@ export default class FeatData extends ItemDataModel.mixin(
       { label: this.requirements, value: this._source.requirements, field: this.schema.getField("requirements"),
         placeholder: "DND5E.Requirements" }
     ];
+    context.parts = ["dnd5e.details-feat", "dnd5e.details-uses"];
   }
 
   /* -------------------------------------------- */
