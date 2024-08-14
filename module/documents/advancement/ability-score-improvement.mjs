jklagenberg/foundryvtt-dnd5e

[#2623] Separate ability score locking from fixed improvements
Previously if you granted an ability score a fixed improvement it
also locked that score so it cannot be improved further. This
moves the list of locked ability scores into a separate list so
scores without fixed improvements can be locked, and scores with
fixed improvements can still be improved further.

This is a slightly breaking change because fixed improvements that
were previously locked will no longer be so.

module/documents/advancement/ability-score-improvement.mjs
@@ -20,8 +20,8 @@ export default class AbilityScoreImprovementAdvancement extends Advancement {
       },
       order: 20,
       icon: "systems/dnd5e/icons/svg/ability-score-improvement.svg",
-      title: game.i18n.localize("DND5E.AdvancementAbilityScoreImprovementTitle"),
-      hint: game.i18n.localize("DND5E.AdvancementAbilityScoreImprovementHint"),
+      title: game.i18n.localize("DND5E.Advancement.AbilityScoreImprovement.Title"),
+      hint: game.i18n.localize("DND5E.Advancement.AbilityScoreImprovement.Hint"),
       apps: {
         config: AbilityScoreImprovementConfig,
         flow: AbilityScoreImprovementFlow
@@ -106,7 +106,7 @@ export default class AbilityScoreImprovementAdvancement extends Advancement {
         return `<span class="tag">${name} <strong>${formatter.format(value)}</strong></span>`;
       });
       if ( this.configuration.points ) entries.push(`<span class="tag">${
-        game.i18n.localize("DND5E.AdvancementAbilityScoreImprovementPoints")}: <strong>${
+        game.i18n.localize("DND5E.Advancement.AbilityScoreImprovement.FIELDS.points.label")}: <strong>${
         this.configuration.points}</strong></span>`
       );
       return entries.filterJoin("\n");

foundryvtt/dnd5e

module/documents/advancement/ability-score-improvement.mjs
@@ -21,8 +21,8 @@ export default class AbilityScoreImprovementAdvancement extends Advancement {
       order: 20,
       icon: "icons/magic/symbols/star-solid-gold.webp",
       typeIcon: "systems/dnd5e/icons/svg/ability-score-improvement.svg",
-      title: game.i18n.localize("DND5E.AdvancementAbilityScoreImprovementTitle"),
-      hint: game.i18n.localize("DND5E.AdvancementAbilityScoreImprovementHint"),
+      title: game.i18n.localize("DND5E.ADVANCEMENT.AbilityScoreImprovement.Title"),
+      hint: game.i18n.localize("DND5E.ADVANCEMENT.AbilityScoreImprovement.Hint"),
       apps: {
         config: AbilityScoreImprovementConfig,
         flow: AbilityScoreImprovementFlow
@@ -107,7 +107,7 @@ export default class AbilityScoreImprovementAdvancement extends Advancement {
         return `<span class="tag">${name} <strong>${formatter.format(value)}</strong></span>`;
       });
       if ( this.configuration.points ) entries.push(`<span class="tag">${
-        game.i18n.localize("DND5E.AdvancementAbilityScoreImprovementPoints")}: <strong>${
+        game.i18n.localize("DND5E.ADVANCEMENT.AbilityScoreImprovement.FIELDS.points.label")}: <strong>${
         this.configuration.points}</strong></span>`
       );
       return entries.filterJoin("\n");