128f9f1bb744b39a1a0bea8934ca1f63702578bf
Kim Mantas <kim.mantas@gmail.com>
2024-08-26 18:28:11 +0100

    Restore hint text to label-top form groups.

--- module/applications/api/application.mjs
+++ module/applications/api/application.mjs
@@ -26,12 +26,24 @@ export default class Application5e extends HandlebarsApplicationMixin(Applicatio
   /** @inheritDoc */
   _onRender(context, options) {
     super._onRender(context, options);
+
+    // Allow multi-select tags to be removed when the whole tag is clicked.
     this.element.querySelectorAll("multi-select").forEach(select => {
       if ( select.disabled ) return;
       select.querySelectorAll(".tag").forEach(tag => {
         tag.classList.add("remove");
         tag.querySelector(":scope > span")?.classList.add("remove");
       });
+    });
+
+    // Add special styling for label-top hints.
+    this.element.querySelectorAll(".label-top > p.hint").forEach(hint => {
+      const label = hint.parentElement.querySelector(":scope > label");
+      if ( !label ) return;
+      hint.ariaLabel = hint.innerText;
+      hint.dataset.tooltip = hint.innerHTML;
+      hint.innerHTML = "";
+      label.insertAdjacentElement("beforeend", hint);
     });
   }
 }
