6b3952001f7c7f7ba1e8ce4f399713d74ce28473
Jeff Hitchcock <mail@command-center.org>
2024-08-27 09:23:14 -0700

    [#4051] Export new shared data fields

--- module/data/shared/_module.mjs
+++ module/data/shared/_module.mjs
@@ -1,8 +1,12 @@
+export {default as ActivationField} from "./activation-field.mjs";
 export {default as CreatureTypeField} from "./creature-type-field.mjs";
 export {default as CurrencyTemplate} from "./currency.mjs";
 export {default as DamageField, DamageData} from "./damage-field.mjs";
+export {default as DurationField} from "./duration-field.mjs";
 export {default as MovementField} from "./movement-field.mjs";
+export {default as RangeField} from "./range-field.mjs";
+export {default as RollConfigField} from "./roll-config-field.mjs";
 export {default as SensesField} from "./senses-field.mjs";
 export {default as SourceField} from "./source-field.mjs";
-export {default as RollConfigField} from "./roll-config-field.mjs";
+export {default as TargetField} from "./target-field.mjs";
 export {default as UsesField} from "./uses-field.mjs";
