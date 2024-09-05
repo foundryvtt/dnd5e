8a2c33282e475948e665c93c623bfeea90b657c6
Jeff Hitchcock <mail@command-center.org>
2024-09-05 15:27:07 -0700

    [#4056] Fix bug awarding from Encounters with derived XP

--- module/applications/award.mjs
+++ module/applications/award.mjs
@@ -214,7 +214,8 @@ export default class Award extends DialogMixin(FormApplication) {
     destinations = destinations.filter(d => ["character", "group"].includes(d.type));
     if ( !amount || !destinations.length ) return;
 
-    let originUpdate = origin ? (origin.system.details.xp.value ?? 0) : Infinity;
+    const xp = origin?.system.details.xp;
+    let originUpdate = origin ? (xp.value ?? xp.derived ?? 0) : Infinity;
     if ( each ) amount = amount * destinations.length;
     const perDestination = Math.floor(Math.min(amount, originUpdate) / destinations.length);
     originUpdate -= amount;
