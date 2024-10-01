8bbe8f46a48a26564ef4969617155894c02f62d9
Jeff Hitchcock <mail@command-center.org>
2024-10-01 10:06:05 -0700

    [#4310] Add standard-form class to DocumentSheet5e

--- module/applications/api/document-sheet.mjs
+++ module/applications/api/document-sheet.mjs
@@ -5,4 +5,9 @@ const { DocumentSheetV2 } = foundry.applications.api;
 /**
  * Base document sheet from which all document-based application should be based.
  */
-export default class DocumentSheet5e extends V2Mixin(DocumentSheetV2) {}
+export default class DocumentSheet5e extends V2Mixin(DocumentSheetV2) {
+  /** @override */
+  static DEFAULT_OPTIONS = {
+    classes: ["standard-form"]
+  };
+}
