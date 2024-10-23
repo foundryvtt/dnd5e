0dc0af8de4c1a39783b2197fc37e7ec7c71307fb
Jeff Hitchcock <mail@command-center.org>
2024-10-23 11:29:36 -0700

    [#4310] Move bonuses to armor list, add dexterity breakdown

--- module/applications/actor/config/armor-class-config.mjs
+++ module/applications/actor/config/armor-class-config.mjs
@@ -40,7 +40,7 @@ export default class ActorArmorConfig extends BaseConfigSheet {
     context = await super._preparePartContext(partId, context, options);
     context.data = this.document.system.attributes.ac;
     context.fields = this.document.system.schema.fields.attributes.fields.ac.fields;
-    context.source = this.document.system.toObject().attributes.ac;
+    context.source = this.document.system._source.attributes.ac;
 
     context.calculationOptions = Object.entries(CONFIG.DND5E.armorClasses).reduce((arr, [value, config]) => {
       if ( value === "custom" ) arr.push({ rule: true });
@@ -49,34 +49,42 @@ export default class ActorArmorConfig extends BaseConfigSheet {
       return arr;
     }, []);
 
-    // Display active effects targeting bonus
-    context.effects = {
-      bonuses: this.document._prepareActiveEffectAttributions("system.attributes.ac.bonus")
-        .filter(e => e.mode === CONST.ACTIVE_EFFECT_MODES.ADD)
-    };
-
-    context.equipped = {};
-    for ( const key of ["armor", "shield"] ) {
-      const item = context.data[`equipped${key.capitalize()}`];
-      if ( !item ) continue;
-      context.equipped[key] = {
-        anchor: item.toAnchor().outerHTML,
-        img: item.img,
-        magicalBonus: formatNumber(
-          item.system.properties.has("mgc") ? item.system.armor.magicalBonus : 0, { signDisplay: "always" }
-        ),
-        name: item.name,
-        value: formatNumber(item.system.armor.base, { signDisplay: key === "shield" ? "always" : "auto" })
-      };
-    }
-    if ( foundry.utils.isEmpty(context.equipped) ) delete context.equipped;
-
     const config = CONFIG.DND5E.armorClasses[context.source.calc];
     context.formula = {
       disabled: context.source.calc !== "custom",
       showFlat: ["flat", "natural"].includes(context.source.calc),
-      value: context.source.calc === "custom" ? context.source.formula : (config?.formula ?? "")
+      value: (context.source.calc === "custom" ? context.source.formula : config?.formula) ?? ""
     };
+
+    if ( context.formula.value.includes("@attributes.ac.dex") ) context.dexterity = context.data.dex;
+
+    context.calculations = [];
+    if ( context.formula.value.includes("@attributes.ac.armor") ) {
+      for ( const key of ["armor", "shield"] ) {
+        const item = context.data[`equipped${key.capitalize()}`];
+        if ( !item ) continue;
+        context.calculations.push({
+          anchor: item.toAnchor().outerHTML,
+          img: item.img,
+          magicalBonus: item.system.properties.has("mgc")
+            ? formatNumber(item.system.armor.magicalBonus, { signDisplay: "always" }) : "—",
+          name: item.name,
+          value: formatNumber(item.system.armor.base, { signDisplay: key === "shield" ? "always" : "auto" })
+        });
+      }
+    }
+    if ( context.source.calc !== "flat" ) {
+      for ( const bonus of this.document._prepareActiveEffectAttributions("system.attributes.ac.bonus") ) {
+        if ( bonus.mode !== CONST.ACTIVE_EFFECT_MODES.ADD ) continue;
+        context.calculations.push({
+          anchor: bonus.document.toAnchor().outerHTML,
+          img: bonus.document.img,
+          magicalBonus: formatNumber(bonus.value, { signDisplay: "always" }),
+          name: bonus.label,
+          value: "—"
+        });
+      }
+    }
 
     return context;
   }
