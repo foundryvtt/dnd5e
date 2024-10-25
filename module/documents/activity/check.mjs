zarex97/zarex97-dnd5e_cust

[#1530, #2138] Re-implement `rollSkill` and `rollToolCheck`
Merges the functionality ot skill & tool checks into a single
method that takes advantage of the new rolling APIs. These new
methods make use of a `buildConfig` callback that is called by
`RollConfigurationDialog` during its `buildConfig` method. This
allows for more extensive modifications of the configuration when
the ability is changed.

Closes #1530

module/documents/activity/check.mjs
@@ -95,25 +95,22 @@ export default class CheckActivity extends ActivityMixin(CheckActivityData) {
     if ( !targets.length ) ui.notifications.warn("DND5E.ActionWarningNoToken", { localize: true });
     let { ability, dc, skill, tool } = target.dataset;
     dc = parseInt(dc);
-    const data = { event, targetValue: Number.isFinite(dc) ? dc : this.check.dc.value };
+    const rollData = { event, target: Number.isFinite(dc) ? dc : this.check.dc.value };
+    if ( ability in CONFIG.DND5E.abilities ) rollData.ability = ability;
 
     for ( const token of targets ) {
-      data.speaker = ChatMessage.getSpeaker({ scene: canvas.scene, token: token.document });
-      if ( skill ) {
-        await token.actor.rollSkill(skill, { ...data, ability });
-      } else if ( tool ) {
-        const checkData = { ...data, ability };
+      const messageData = { data: { speaker: ChatMessage.getSpeaker({ scene: canvas.scene, token: token.document }) } };
+      if ( skill ) await token.actor.rollSkill({ ...rollData, skill }, {}, messageData);
+      else if ( tool ) {
+        rollData.tool = tool;
         if ( (this.item.type === "tool") && !this.check.associated.size ) {
-          checkData.bonus = this.item.system.bonus;
-          checkData.prof = this.item.system.prof;
-          checkData.item = this.item;
+          rollData.bonus = this.item.system.bonus;
+          rollData.prof = this.item.system.prof;
+          rollData.item = this.item;
         }
-        await token.actor.rollToolCheck(tool, checkData);
-      } else {
-        await token.actor.rollAbilityTest(
-          { ability, target: data.targetValue }, {}, { data: { speaker: data.speaker } }
-        );
+        await token.actor.rollToolCheck(rollData, {}, messageData);
       }
+      else await token.actor.rollAbilityTest(rollData, {}, messageData);
     }
   }
 }

module/enrichers.mjs
@@ -792,7 +792,8 @@ async function rollAction(event) {
 
   const { type, ability, skill, tool, dc } = target.dataset;
   const options = { event };
-  if ( dc ) options.targetValue = dc;
+  if ( ability in CONFIG.DND5E.abilities ) options.ability = ability;
+  if ( dc ) options.target = dc;
 
   const action = event.target.closest("a")?.dataset.action ?? "roll";
 
@@ -815,22 +816,19 @@ async function rollAction(event) {
         const actor = token.actor;
         switch ( type ) {
           case "check":
-            await actor.rollAbilityTest({ ability, event: options.event, target: options.targetValue });
+            await actor.rollAbilityTest(options);
             break;
           case "concentration":
-            if ( ability in CONFIG.DND5E.abilities ) options.ability = ability;
             await actor.rollConcentration(options);
             break;
           case "save":
-            await actor.rollAbilitySave({ ability, event: options.event, target: options.targetValue });
+            await actor.rollAbilitySave(options);
             break;
           case "skill":
-            if ( ability ) options.ability = ability;
-            await actor.rollSkill(skill, options);
+            await actor.rollSkill({ skill, ...options });
             break;
           case "tool":
-            options.ability = ability;
-            await actor.rollToolCheck(tool, options);
+            await actor.rollToolCheck({ tool, ...options });
             break;
         }
       }

foundryvtt/dnd5e

module/documents/activity/check.mjs
@@ -95,26 +95,22 @@ export default class CheckActivity extends ActivityMixin(CheckActivityData) {
     if ( !targets.length ) ui.notifications.warn("DND5E.ActionWarningNoToken", { localize: true });
     let { ability, dc, skill, tool } = target.dataset;
     dc = parseInt(dc);
-    const data = { event, targetValue: Number.isFinite(dc) ? dc : this.check.dc.value };
+    const rollData = { event, target: Number.isFinite(dc) ? dc : this.check.dc.value };
+    if ( ability in CONFIG.DND5E.abilities ) rollData.ability = ability;
 
     for ( const token of targets ) {
-      data.speaker = ChatMessage.getSpeaker({ scene: canvas.scene, token: token.document });
-      if ( skill ) {
-        await token.actor.rollSkill(skill, { ...data, ability });
-      } else if ( tool ) {
-        const checkData = { ...data, ability };
+      const messageData = { data: { speaker: ChatMessage.getSpeaker({ scene: canvas.scene, token: token.document }) } };
+      if ( skill ) await token.actor.rollSkill({ ...rollData, skill }, {}, messageData);
+      else if ( tool ) {
+        rollData.tool = tool;
         if ( (this.item.type === "tool") && !this.check.associated.size ) {
-          checkData.bonus = this.item.system.bonus;
-          checkData.prof = this.item.system.prof;
-          checkData.item = this.item;
+          rollData.bonus = this.item.system.bonus;
+          rollData.prof = this.item.system.prof;
+          rollData.item = this.item;
         }
-        await token.actor.rollToolCheck(tool, checkData);
-      } else {
-        await token.actor.rollAbilityCheck({
-          ability,
-          target: data.targetValue
-        }, {}, { data: { speaker: data.speaker } });
+        await token.actor.rollToolCheck(rollData, {}, messageData);
       }
+      else await token.actor.rollAbilityCheck(rollData, {}, messageData);
     }
   }
 }

module/enrichers.mjs
@@ -799,7 +799,8 @@ async function rollAction(event) {
 
   const { type, ability, skill, tool, dc } = target.dataset;
   const options = { event };
-  if ( dc ) options.targetValue = dc;
+  if ( ability in CONFIG.DND5E.abilities ) options.ability = ability;
+  if ( dc ) options.target = dc;
 
   const action = event.target.closest("a")?.dataset.action ?? "roll";
 
@@ -822,22 +823,19 @@ async function rollAction(event) {
         const actor = token.actor;
         switch ( type ) {
           case "check":
-            await actor.rollAbilityCheck({ ability, event: options.event, target: options.targetValue });
+            await actor.rollAbilityCheck(options);
             break;
           case "concentration":
-            if ( ability in CONFIG.DND5E.abilities ) options.ability = ability;
             await actor.rollConcentration({ ...options, legacy: false });
             break;
           case "save":
-            await actor.rollSavingThrow({ ability, event: options.event, target: options.targetValue });
+            await actor.rollSavingThrow(options);
             break;
           case "skill":
-            if ( ability ) options.ability = ability;
-            await actor.rollSkill(skill, options);
+            await actor.rollSkill({ skill, ...options });
             break;
           case "tool":
-            options.ability = ability;
-            await actor.rollToolCheck(tool, options);
+            await actor.rollToolCheck({ tool, ...options });
             break;
         }
       }