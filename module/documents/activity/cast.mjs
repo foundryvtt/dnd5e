f0207177bc5d9dd777f276ad3c6cfe235f579564
Jeff Hitchcock <mail@command-center.org>
2024-10-30 13:49:27 -0700

    [#4061, #4613] Add Cast overrides, activity changes via enchanting
    Makes the DC & Attack overrides on the Cast activity functional.
    It does this through the Spell Changes enchantment which uses a
    new syntax to apply directly to all Attack & Save activities on
    the spell.
    
    The new syntax allows for modifying all activities of a certain
    type using the change key like `activity.attack.attack.bonus`,
    where `activity.attack` indicates this should change all attack
    activities and `attack.bonus` being the key path to the field to
    change. At the same time this also fixes some issues with applying
    enchantment changes directly to individual activities by ID.
    
    Closes #4613

--- module/documents/activity/cast.mjs
+++ module/documents/activity/cast.mjs
@@ -141,15 +141,31 @@ export default class CastActivity extends ActivityMixin(CastActivityData) {
   getSpellChanges() {
     const changes = [];
     const source = this.toObject();
+
+    // Override spell details
     for ( const type of ["activation", "duration", "range", "target"] ) {
       if ( !this[type].override ) continue;
       const data = source[type];
       delete data.override;
       changes.push({ key: `system.${type}`, mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE, value: JSON.stringify(data) });
     }
+
+    // Remove ignored properties
     for ( const property of this.spell.properties ) {
       changes.push({ key: "system.properties", mode: CONST.ACTIVE_EFFECT_MODES.ADD, value: `-${property}` });
     }
+
+    // Set challenge overrides
+    const challenge = this.spell.challenge;
+    if ( challenge.override && challenge.attack ) changes.push(
+      { key: "activity.attack.attack.bonus", mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE, value: challenge.attack },
+      { key: "activity.attack.attack.flat", mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE, value: true }
+    );
+    if ( challenge.override && challenge.save ) changes.push(
+      { key: "activity.save.save.dc.calculation", mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE, value: "" },
+      { key: "activity.save.save.dc.formula", mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE, value: challenge.save }
+    );
+
     return changes;
   }
 }
