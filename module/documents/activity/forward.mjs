d7d3992f8c0354eafbfd92754bd498a5c951a641
Jeff Hitchcock <mail@command-center.org>
2024-11-04 07:57:33 -0800

    [#4031] Fix Forward description

--- module/documents/activity/forward.mjs
+++ module/documents/activity/forward.mjs
@@ -3,7 +3,7 @@ import ForwardActivityData from "../../data/activity/forward-data.mjs";
 import ActivityMixin from "./mixin.mjs";
 
 /**
- * Activity for casting a spell from another item.
+ * Activity for triggering another activity with modified consumption.
  */
 export default class ForwardActivity extends ActivityMixin(ForwardActivityData) {
   /* -------------------------------------------- */
