0df7a151f57f4ed5c16ef663e157c139bd03521b
Jeff Hitchcock <mail@command-center.org>
2024-11-13 13:20:09 -0800

    [#4711] Fix usage recovery config for items with formula in max
    Closes #4711

--- module/data/item/templates/activities.mjs
+++ module/data/item/templates/activities.mjs
@@ -82,7 +82,7 @@ export default class ActivitiesTemplate extends SystemDataModel {
    * @type {boolean}
    */
   get hasLimitedUses() {
-    return !!this.uses.max;
+    return !!this._source.uses.max || !!this.uses.max;
   }
 
   /* -------------------------------------------- */
