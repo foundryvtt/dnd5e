bb472d1e0850ffac3149adda2adb83754e66d66b
Jeff Hitchcock <mail@command-center.org>
2024-11-21 13:42:54 -0800

    [#4708] Move HitPointsAdvancement localization

--- module/documents/advancement/hit-points.mjs
+++ module/documents/advancement/hit-points.mjs
@@ -16,8 +16,8 @@ export default class HitPointsAdvancement extends Advancement {
       order: 10,
       icon: "icons/magic/life/heart-pink.webp",
       typeIcon: "systems/dnd5e/icons/svg/hit-points.svg",
-      title: game.i18n.localize("DND5E.AdvancementHitPointsTitle"),
-      hint: game.i18n.localize("DND5E.AdvancementHitPointsHint"),
+      title: game.i18n.localize("DND5E.ADVANCEMENT.HitPoints.Title"),
+      hint: game.i18n.localize("DND5E.ADVANCEMENT.HitPoints.Hint"),
       multiLevel: true,
       apps: {
         config: HitPointsConfig,
