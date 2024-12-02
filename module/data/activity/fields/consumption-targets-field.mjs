9366bfc5dafa1d94bfbb76270f859c89c4abef2f
Kim Mantas <kim.mantas@gmail.com>
2024-12-02 15:01:12 +0000

    Code style & language pass

--- module/data/activity/fields/consumption-targets-field.mjs
+++ module/data/activity/fields/consumption-targets-field.mjs
@@ -416,8 +416,8 @@ export class ConsumptionTargetData extends foundry.abstract.DataModel {
   static consumptionLabelsHitDice(config, { consumed }={}) {
     const { cost, simplifiedCost, increaseKey, pluralRule } = this._resolveHintCost(config);
     let denomination;
-    if (this.target === "smallest") denomination = game.i18n.localize("DND5E.ConsumeHitDiceSmallest");
-    else if (this.target === "largest") denomination = game.i18n.localize("DND5E.ConsumeHitDiceLargest");
+    if ( this.target === "smallest" ) denomination = game.i18n.localize("DND5E.ConsumeHitDiceSmallest");
+    else if ( this.target === "largest" ) denomination = game.i18n.localize("DND5E.ConsumeHitDiceLargest");
     else denomination = this.target;
     const available = (["smallest", "largest"].includes(this.target)
       ? this.actor.system.attributes?.hd?.value : this.actor.system.attributes?.hd?.bySize?.[this.target]) ?? 0;
