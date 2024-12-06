c2a619cd32255ff072e527e3bb01908e628d3dd7
Zhell <kennethzhell@gmail.com>
2024-12-06 22:30:24 +0100

    Track advantage modes in new number field class

--- module/data/fields/_module.mjs
+++ module/data/fields/_module.mjs
@@ -1,6 +1,7 @@
 export * from "./activities-field.mjs";
-export {default as AdvancementField} from "./advancement-field.mjs";
 export {default as AdvancementDataField} from "./advancement-data-field.mjs";
+export {default as AdvancementField} from "./advancement-field.mjs";
+export {default as AdvantageModeField} from "./advantage-mode-field.mjs";
 export {default as FormulaField} from "./formula-field.mjs";
 export {default as IdentifierField} from "./identifier-field.mjs";
 export {default as LocalDocumentField} from "./local-document-field.mjs";
