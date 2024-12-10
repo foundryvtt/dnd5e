38808f6886b5b14262af1f677d1e11670c35e6aa
Zhell <kennethzhell@gmail.com>
2024-12-10 02:24:48 +0100

    [#4790] Death save bonus and config

---
+++ module/applications/actor/config/death-config.mjs
@@ -0,0 +1,57 @@
+import BaseConfigSheet from "../api/base-config-sheet.mjs";
+
+/**
+ * Configuration application for an actor's concentration checks.
+ */
+export default class DeathConfig extends BaseConfigSheet {
+  /** @override */
+  static DEFAULT_OPTIONS = {
+    ability: null,
+    position: {
+      width: 500
+    }
+  };
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  static PARTS = {
+    config: {
+      template: "systems/dnd5e/templates/actors/config/death-config.hbs"
+    }
+  };
+
+  /* -------------------------------------------- */
+  /*  Properties                                  */
+  /* -------------------------------------------- */
+
+  /** @override */
+  get title() {
+    return game.i18n.format("DND5E.ABILITY.Configure.Title", { ability: game.i18n.localize("DND5E.DeathSavingThrow") });
+  }
+
+  /* -------------------------------------------- */
+  /*  Rendering                                   */
+  /* -------------------------------------------- */
+
+  /** @inheritDoc */
+  async _preparePartContext(partId, context, options) {
+    context = await super._preparePartContext(partId, context, options);
+    const source = this.document.system._source;
+
+    context.data = source.attributes?.death ?? {};
+    context.fields = this.document.system.schema.getField("attributes.death").fields;
+    context.advantageModeOptions = [
+      { value: -1, label: game.i18n.localize("DND5E.Disadvantage") },
+      { value: 0, label: game.i18n.localize("DND5E.Normal") },
+      { value: 1, label: game.i18n.localize("DND5E.Advantage") }
+    ];
+
+    if ( this.document.system.bonuses?.abilities ) context.global = {
+      data: source.bonuses?.abilities ?? {},
+      fields: this.document.system.schema.getField("bonuses.abilities").fields
+    };
+
+    return context;
+  }
+}
