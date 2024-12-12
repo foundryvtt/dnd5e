d7d700e6d3c15ecfb7404457202f4840deb974b0
Edson Vicente Carli Junior <gandnegro@gmail.com>
2024-12-12 18:50:57 +0000

    remove undeclared variable
    remove undeclared variable

--- module/data/item/templates/activities.mjs
+++ module/data/item/templates/activities.mjs
@@ -189,19 +189,19 @@ export default class ActivitiesTemplate extends SystemDataModel {
     if ( source.uses?.per === "charges" ) {
       if ( source.uses.recovery ) {
         source.uses.recovery = [{ period: "lr", type: "formula", formula: source.uses.recovery }];
-      } else if (hasSourceUses) {
+      } else if (source.uses) {
         delete source.uses.recovery;
       }
     }
     // If period is not blank, set an appropriate recovery type
     else if (source?.uses?.per ) {
       if ( CONFIG.DND5E.limitedUsePeriods[source?.uses?.per]?.formula && source?.uses?.recovery ) {
-        if(source?.uses)
+        if(source.uses)
         {
           source.uses.recovery = [{ period: source?.uses?.per, type: "formula", formula: source?.uses?.recovery }];
         }
       }
-      else if(source?.uses)
+      else if(source.uses)
       {
         source.uses.recovery = [{ period: source?.uses?.per, type: "recoverAll" }];
       }
