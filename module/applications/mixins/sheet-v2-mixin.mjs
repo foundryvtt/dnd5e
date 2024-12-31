3b90e10d91955fb32a64476cdc998904dad4d7aa
Kim Mantas <kim.mantas@gmail.com>
2024-12-31 21:15:55 +0000

    Only adjust sheet mode on render if none had been set yet.

--- module/applications/mixins/sheet-v2-mixin.mjs
+++ module/applications/mixins/sheet-v2-mixin.mjs
@@ -37,10 +37,10 @@ export default function DocumentSheetV2Mixin(Base) {
 
     /**
      * The mode the sheet is currently in.
-     * @type {ActorSheetV2.MODES}
+     * @type {ActorSheetV2.MODES|null}
      * @protected
      */
-    _mode = this.constructor.MODES.PLAY;
+    _mode = null;
 
     /* -------------------------------------------- */
 
@@ -53,8 +53,8 @@ export default function DocumentSheetV2Mixin(Base) {
 
     /** @inheritDoc */
     async _render(force, { mode, ...options }={}) {
-      if ( mode && (mode !== this._mode) ) {
-        this._mode = mode;
+      if ( !this._mode ) {
+        this._mode = mode ?? this.constructor.MODES.PLAY;
         if ( this.rendered ) {
           const toggle = this.element[0].querySelector(".window-header .mode-slider");
           toggle.checked = this._mode === this.constructor.MODES.EDIT;
