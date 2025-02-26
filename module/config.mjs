c7155aa9550bb8df5d5de6d6283c4ae2d8004f0d
Jeff Hitchcock <mail@command-center.org>
2025-02-26 09:55:08 -0800

    [#4774] Add properties to classes with Sidekick property
    Adds properties to classes and give them support for the "Sidekick"
    property. Also sets the initial filter when browsing classes on a
    PC to exclude sidekicks (but not locked, in case a DM wants to use
    the character sheet for their sidekicks).

--- module/config.mjs
+++ module/config.mjs
@@ -2019,6 +2019,9 @@ DND5E.itemProperties = {
     reference: "Compendium.dnd5e.rules.JournalEntry.NizgRXLNUqtdlC1s.JournalEntryPage.FjWqT5iyJ89kohdA",
     isTag: true
   },
+  sidekick: {
+    label: "DND5E.Item.Property.Sidekick"
+  },
   sil: {
     label: "DND5E.ITEM.Property.Silvered",
     isPhysical: true
@@ -2064,6 +2067,9 @@ preLocalize("itemProperties", { keys: ["label", "abbreviation"], sort: true });
  * @enum {object}
  */
 DND5E.validProperties = {
+  class: new Set([
+    "sidekick"
+  ]),
   consumable: new Set([
     "mgc"
   ]),
