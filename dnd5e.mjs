cc28f8ef41a34728f2f0f921826b9f0bbe087e63
Jeff Hitchcock <mail@command-center.org>
2025-03-10 11:46:55 -0700

    [#5332] Migrate to core's `CompendiumArt` system
    Adjusts the system's art mapping to use core's system rather than
    the system's. Add handling to the system to ensure credit is
    added to actor descriptions when using the core's system.
    
    Deprecates the `ModuleArt` system while ensure it continues to work
    in tandem with core's system until the deprecation period ends.
    The removal version is set to `6.0` to give premium modules plenty
    of time to move over.
    
    Closes #5332

--- dnd5e.mjs
+++ dnd5e.mjs
@@ -512,6 +512,8 @@ Hooks.on("renderSettings", (app, html) => applications.settings.sidebar.renderSe
 /*  Other Hooks                                 */
 /* -------------------------------------------- */
 
+Hooks.on("applyCompendiumArt", (documentClass, ...args) => documentClass.applyCompendiumArt?.(...args));
+
 Hooks.on("renderChatPopout", documents.ChatMessage5e.onRenderChatPopout);
 Hooks.on("getChatLogEntryContext", documents.ChatMessage5e.addChatMessageContextOptions);
 
