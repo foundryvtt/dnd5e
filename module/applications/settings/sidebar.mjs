08888391ca3b073cd1e6b5cb42b573d25f2bf8e9
Kim Mantas <kim.mantas@gmail.com>
2025-03-20 17:27:36 +0000

    [#5391] Fix missing Game System label in v13.

--- module/applications/settings/sidebar.mjs
+++ module/applications/settings/sidebar.mjs
@@ -40,7 +40,7 @@ export function renderSettings(html) {
   const section = document.createElement("section");
   section.classList.add("dnd5e2", "sidebar-info");
   section.innerHTML = `
-    <h4 class="divider">${game.i18n.localize("WORLD.GameSystem")}</h4>
+    <h4 class="divider">${game.i18n.localize("WORLD.FIELDS.system.label")}</h4>
     <div class="dnd5e2 system-badge">
       <img src="systems/dnd5e/ui/official/dnd-badge-32.webp" data-tooltip="${dnd5e.title}" alt="${dnd5e.title}">
       <span class="system-info">${dnd5e.version}</span>
