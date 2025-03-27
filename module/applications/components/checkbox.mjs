0c04da7af4421126da2d2d3a28e9213a070341e6
Jeff Hitchcock <mail@command-center.org>
2025-03-27 16:04:49 -0700

    Swap to using cursor variables
    Swaps out everything except for uses of `cursor: help` and
    `cursor: not-allowed` which aren't supported as custom types.

--- module/applications/components/checkbox.mjs
+++ module/applications/components/checkbox.mjs
@@ -33,14 +33,14 @@ export default class CheckboxElement extends AdoptedStyleSheetMixin(
   /** @override */
   static CSS = `
     :host {
-      cursor: pointer;
+      cursor: var(--cursor-pointer);
       display: inline-block;
       width: var(--checkbox-size, 18px);
       height: var(--checkbox-size, 18px);
       aspect-ratio: 1;
     }
 
-    :host(:disabled) { cursor: default; }
+    :host(:disabled) { cursor: var(--cursor-default); }
 
     :host > div {
       width: 100%;
