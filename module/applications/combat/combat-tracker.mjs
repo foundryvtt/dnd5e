596d3acb565edd6f82e99e6f439c16e4ea888833
Max.Pat <86370342+MaxPat931@users.noreply.github.com>
2025-04-18 09:34:27 -0700

    Use Token name instead of Actor ProtoToken Name

--- module/applications/combat/combat-tracker.mjs
+++ module/applications/combat/combat-tracker.mjs
@@ -88,7 +88,7 @@ export default class CombatTracker5e extends foundry.applications.sidebar.tabs.C
         });
       }
 
-      const name = combatants[0].token?.baseActor.prototypeToken.name ?? combatants[0].name;
+      const name = combatants[0].token?.name ?? combatants[0].name;
       const img = children[0].querySelector("img");
       groupContainer.innerHTML = `
         <div class="group-header flexrow">
