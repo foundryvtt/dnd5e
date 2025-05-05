38091bbf0a1beed71b9a680d9ba44180a485eba0
vszalai <szalaivalter@proton.me>
2025-05-06 00:14:31 +0200

    comment fix

--- module/applications/actor/rest/base-rest-dialog.mjs
+++ module/applications/actor/rest/base-rest-dialog.mjs
@@ -64,7 +64,7 @@ export default class BaseRestDialog extends Dialog5e {
 
   /* -------------------------------------------- */
 
-    /* -------------------------------------------- */
+  /* -------------------------------------------- */
 
   /**
    * Should the user be prompted as to whether reset the actor's temporary max hit points?
