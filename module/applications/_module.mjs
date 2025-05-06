38ec5ce8557f8aa54ff69c046db8b7330ae445dc
Jeff Hitchcock <mail@command-center.org>
2025-05-06 16:16:59 -0700

    [#5633] Add standard calendar HUD
    Introduces a new calendar HUD UI element that displays at the top
    of the screen and is styled like the rest of the core UI. This
    element includes buttons for advancing and reversing time, opening
    the player's assigned character sheet, and opening the party group
    sheet. Between that it displays the current date and time along
    with a day visualizer to indicate the progress of the day.
    
    The calendar interface is set by a `CONFIG.DND5E.calendar` object
    that defines the application used to render the calendar, the
    current HUD instance if rendered, a list of formatters that can be
    used to display the date and time, and a pair of methods for
    tracking the progress through the day & night. This makes it easy
    to completely disable the native HUD (setting `application` to
    `null`) or to customize it to whatever calendar is being used.
    
    The settings offer two sets of calendar options, one visible only
    to GM users and the other available to all users. The GM settings
    allow for enabling the calendar for all users and for customizing
    the time change provided by each of the advance/reverse buttons.
    
    The preferences have a control to toggle the display of the HUD
    for each user and options to customize what formatter is used to
    display the date or time on each side of the widget.
    
     ## Todo
    - [ ] Show/hide sheet buttons based on sheet changes
    - [ ] Improve animation when advancing over large periods of time
    - [ ] Give GMs more control over what formatters players can use
    - [ ] Add UI for changing to specific date/time

--- module/applications/_module.mjs
+++ module/applications/_module.mjs
@@ -2,6 +2,7 @@ export * as activity from "./activity/_module.mjs";
 export * as actor from "./actor/_module.mjs";
 export * as advancement from "./advancement/_module.mjs";
 export * as api from "./api/_module.mjs";
+export * as calendar from "./calendar/_module.mjs";
 export * as combat from "./combat/_module.mjs";
 export * as components from "./components/_module.mjs";
 export * as dice from "./dice/_module.mjs";
