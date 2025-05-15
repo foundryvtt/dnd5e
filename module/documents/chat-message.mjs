382779f73e57f9c23fc7909111f8cd7e0b484a4c
roth-michael <72220390+roth-michael@users.noreply.github.com>
2025-05-15 11:19:06 -0400

    remove hardcoded healing types
    Co-authored-by: Kim Mantas <kim.mantas@gmail.com>

--- module/documents/chat-message.mjs
+++ module/documents/chat-message.mjs
@@ -825,7 +825,7 @@ export default class ChatMessage5e extends ChatMessage {
    */
   applyChatCardDamage(li, multiplier) {
     const damages = aggregateDamageRolls(this.rolls, { respectProperties: true }).map(roll => ({
-      value: roll.total * (["healing", "temphp"].includes(roll.options.type) ? -1 : 1),
+      value: roll.total * (roll.options.type in CONFIG.DND5E.healingTypes ? -1 : 1),
       type: roll.options.type,
       properties: new Set(roll.options.properties ?? [])
     }));
