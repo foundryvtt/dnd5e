224b38f00a23fb8d448807a82179227d0b8e981e
Kim Mantas <kim.mantas@gmail.com>
2025-05-15 16:32:37 +0100

    [#5667] Fix adding items to group sheets.

--- module/applications/components/inventory.mjs
+++ module/applications/components/inventory.mjs
@@ -453,6 +453,7 @@ export default class InventoryElement extends HTMLElement {
     if ( inventoryEvent.defaultPrevented ) return;
 
     if ( action === "currency" ) return this._onManageCurrency();
+    if ( action === "create" ) return this._onCreateItem(event); // TODO: Remove once legacy sheets are removed.
 
     const { itemId } = target.closest("[data-item-id]")?.dataset ?? {};
     const { activityId } = target.closest("[data-activity-id]")?.dataset ?? {};
@@ -547,6 +548,23 @@ export default class InventoryElement extends HTMLElement {
       }
       else item.update({ [input.dataset.name]: result });
     }
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Create an item on a legacy sheet.
+   * TODO: Remove once legacy sheets are removed.
+   * @param {Event} event
+   * @returns {Promise<Item5e>|void}
+   * @protected
+   */
+  _onCreateItem(event) {
+    const { type } = event?.target?.dataset ?? {};
+    if ( !type || !this.actor ) return;
+    return foundry.documents.Item.implementation.create({
+      type, name: game.i18n.format("DOCUMENT.New", { type: game.i18n.localize(CONFIG.Item.typeLabels[type]) })
+    }, { parent: this.actor });
   }
 
   /* -------------------------------------------- */
