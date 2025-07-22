7299b2055c4ed01c392e05bb57e660e1779ee7f1
Jeff Hitchcock <mail@command-center.org>
2025-07-22 14:07:00 -0700

    Add `standard` AE type to replace core's `base` type
    This new effect type replaces the base type provided by core that
    adds a `magical` toggle and moves rider statuses from flags into
    the system data.

--- module/documents/active-effect.mjs
+++ module/documents/active-effect.mjs
@@ -166,6 +166,11 @@ export default class ActiveEffect5e extends DependentDocumentMixin(ActiveEffect)
       foundry.utils.setProperty(data, "flags.dnd5e.persistSourceMigration", true);
     }
 
+    else if ( data.type === "base" ) {
+      data.type = "standard";
+      foundry.utils.setProperty(data, "flags.dnd5e.persistSourceMigration", true);
+    }
+
     return super._initializeSource(data, options);
   }
 
@@ -174,6 +179,7 @@ export default class ActiveEffect5e extends DependentDocumentMixin(ActiveEffect)
   /** @inheritDoc */
   static migrateData(source) {
     source = super.migrateData(source);
+
     for ( const change of source.changes ?? [] ) {
       if ( change.key === "flags.dnd5e.initiativeAdv" ) {
         change.key = "system.attributes.init.roll.mode";
@@ -181,6 +187,12 @@ export default class ActiveEffect5e extends DependentDocumentMixin(ActiveEffect)
         change.value = 1;
       }
     }
+
+    if ( source.flags?.dnd5e?.riders?.statuses && !source.system?.rider?.statuses ) {
+      foundry.utils.setProperty(source, "system.rider.statuses", source.flags.dnd5e.riders.statuses);
+      delete source.flags.dnd5e.riders.statuses;
+    }
+
     return source;
   }
 
@@ -444,11 +456,7 @@ export default class ActiveEffect5e extends DependentDocumentMixin(ActiveEffect)
    * @returns {Promise<ActiveEffect5e[]>}      Created rider effects.
    */
   async createRiderConditions() {
-    const riders = new Set();
-
-    for ( const status of this.getFlag("dnd5e", "riders.statuses") ?? [] ) {
-      riders.add(status);
-    }
+    const riders = new Set(this.system.riders?.statuses ?? []);
 
     for ( const status of this.statuses ) {
       const r = CONFIG.statusEffects.find(e => e.id === status)?.riders ?? [];
@@ -760,16 +768,25 @@ export default class ActiveEffect5e extends DependentDocumentMixin(ActiveEffect)
    * @param {ApplicationRenderContext} context The app's rendering context.
    */
   static onRenderActiveEffectConfig(app, html, context) {
-    const element = new foundry.data.fields.SetField(new foundry.data.fields.StringField(), {}).toFormGroup({
-      label: _loc("DND5E.CONDITIONS.RiderConditions.label"),
-      hint: _loc("DND5E.CONDITIONS.RiderConditions.hint")
-    }, {
-      name: "flags.dnd5e.riders.statuses",
-      value: app.document.getFlag("dnd5e", "riders.statuses") ?? [],
+    if ( app.document.system.onRenderActiveEffectConfig?.(app, html, context) === false ) return;
+    const fields = app.document.system.schema.fields;
+    const magicalField = fields.magical?.toFormGroup({}, {
+      value: app.document.system._source.magical,
+      disabled: !context.editable
+    });
+    const statusesField = fields.rider?.fields?.statuses?.toFormGroup({}, {
+      value: app.document.system._source.rider?.statuses ?? [],
       options: CONFIG.statusEffects.map(se => ({ value: se.id, label: se.name })),
       disabled: !context.editable
     });
-    html.querySelector("[data-tab=details] > .form-group:has([name=statuses])")?.after(element);
+    const detailsTab = html.querySelector("[data-application-part=details]");
+    const statuses = detailsTab.querySelector("& > .form-group:has([name=statuses])");
+    if ( statuses ) {
+      if ( magicalField ) statuses?.before(magicalField);
+      if ( statusesField ) statuses?.after(statusesField);
+    } else {
+      detailsTab.append(magicalField, statusesField);
+    }
 
     // Add tooltip with link to wiki for effects/enchantments
     const helpIconElement = document.createElement("i");
