119aa282e7caf76e0da74a31f4ca1a3765086eba
Themis <idonotwantspams@gmail.com>
2025-08-16 21:42:16 +0100

    removal of unneeded optional chaining

--- module/canvas/layers/tokens.mjs
+++ module/canvas/layers/tokens.mjs
@@ -21,9 +21,9 @@ export default class TokenLayer5e extends foundry.canvas.layers.TokenLayer {
       if ( token.document.disposition === t.document.disposition ) return false;
 
       // If creature has any statuses that should never block movement, don't block movement
-      if ( neverBlockStatuses.some(status => t.actor?.statuses.has(status)) ) return false;
+      if ( neverBlockStatuses.some(status => t.actor.statuses.has(status)) ) return false;
 
-      const occupiedSize = CONFIG.DND5E.actorSizes[t.actor?.system.traits?.size]?.numerical ?? 2;
+      const occupiedSize = CONFIG.DND5E.actorSizes[t.actor.system.traits.size]?.numerical ?? 2;
       // In modern rules, Tiny creatures can be moved through
       if ( modernRules && (occupiedSize === 0) ) return false;
 
