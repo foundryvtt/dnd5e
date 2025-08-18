852a5bdc6edda8d78f9f4fc1793dc560653c01b0
Jeff Hitchcock <mail@command-center.org>
2025-08-18 11:19:24 -0700

    Use migration to set initial magical value, change default to false
    Changes the default value of magical on effects to `false`, but use
    the creation options when creating on a magical item to set it for
    newly created effects.
    
    Adds a migration to change effects that existed before this version
    to have the magical effect if they are on spells, spell scrolls, or
    an item with the `magical` property.

--- module/applications/item/item-sheet.mjs
+++ module/applications/item/item-sheet.mjs
@@ -686,7 +686,11 @@ export default class ItemSheet5e extends PrimarySheetMixin(DocumentSheet5e) {
       return ActiveEffect.implementation.createDialog({
         name: this.document.name,
         img: this.document.img,
-        origin: this.document.uuid
+        origin: this.document.uuid,
+        system: {
+          magical: this.document.system.properties?.has("mgc")
+        },
+        type: "standard"
       }, { parent: this.document, renderSheet: true }, { sheet: this });
     }
   }
