fb24b6895dd60b96fa5d65906921e4b7289dcb7b
Jeff Hitchcock <mail@command-center.org>
2025-09-02 10:40:44 -0700

    Add `ActiveEffectDataModel`, abstract some logic from document
    Creates `ActiveEffectDataModel` that new can extend to gain some
    default functionality. This new model includes getters for the
    actor and item containing the effect, as well as `applicableType`
    which returns the document type to which the effect should apply.
    This allows some of the previous explicit checks for enchantments
    and rider effects to be handled by the specific types and supports
    module-provided AE types that can apply to actors, items, or
    neither.
    
    Also moves some of the enchantment-specific creation & deletion
    logic into the system data model instead.

---
+++ module/data/abstract/active-effect-data-model.mjs
@@ -0,0 +1,52 @@
+const { ActiveEffectTypeDataModel } = foundry.data;
+const { TypeDataModel } = foundry.abstract;
+
+/**
+ * Abstract base class to add some shared functionality to all of the system's custom active effect types.
+ * @abstract
+ */
+export default class ActiveEffectDataModel extends (ActiveEffectTypeDataModel ?? TypeDataModel) {
+  /* -------------------------------------------- */
+  /*  Properties                                  */
+  /* -------------------------------------------- */
+
+  /**
+   * Actor within which this effect is embedded.
+   * @type {Actor5e|void}
+   */
+  get actor() {
+    return this.item ? this.item.actor : this.parent.parent instanceof Actor ? this.parent.parent : undefined;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Document type to which this active effect should apply its changes.
+   * @type {string}
+   */
+  get applicableType() {
+    return "Actor";
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Item within which this effect is contained, if any.
+   * @type {Item5e|void}
+   */
+  get item() {
+    return this.parent.parent instanceof Item ? this.parent.parent : undefined;
+  }
+
+  /* -------------------------------------------- */
+  /*  Event Listeners and Handlers                */
+  /* -------------------------------------------- */
+
+  /**
+   * Add modifications to the core ActiveEffect config.
+   * @param {ActiveEffectConfig} app            The ActiveEffect config.
+   * @param {HTMLElement} html                  The ActiveEffect config element.
+   * @param {ApplicationRenderContext} context  The app's rendering context.
+   */
+  onRenderActiveEffectConfig(app, html, context) {}
+}
