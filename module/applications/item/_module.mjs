dbd14a2d9d54cf60d4061ec3b48631ba38b5849b
Jeff Hitchcock <mail@command-center.org>
2025-09-10 16:05:09 -0700

    Remove code deprecated in 5.2 (#6147)
    * Remove code deprecated in 5.2
    
    Leaves behind `ActorSheet5e` and `ActorSheetMixin` because those
    are still used by the vehicle sheet, but removed their exports.
    These can be removed fully once the vehicle sheet is converted to
    AppV2.
    
    Moved `DragDropApplicationMixin` from `mixins` to `api` to match
    the other V2 mixins. Since this wasn't previously exported, no
    deprecation warning is required. This mixin is now exported.
    
    Also saw some places where we were calling the V2 version of a
    hook without also calling the un-suffixed name, so fixed those.
    
    Finally removes the unused `CONFIG.DND5E.allowedActorFlags`.
    
    Closes #4782
    
    * Remove ancient `attunements` config entry

--- module/applications/item/_module.mjs
+++ module/applications/item/_module.mjs
@@ -4,8 +4,3 @@ export {default as ItemDirectory5e} from "./item-directory.mjs";
 export {default as ItemSheet5e} from "./item-sheet.mjs";
 
 export {default as StartingEquipmentConfig} from "./config/starting-equipment-config.mjs";
-
-// Deprecated
-export {default as ContainerSheet2} from "./deprecated/container-sheet-2.mjs";
-export {default as ItemSheet5e2} from "./deprecated/item-sheet-2.mjs";
-export {default as ItemSheetV2Mixin} from "./deprecated/sheet-v2-mixin.mjs";
