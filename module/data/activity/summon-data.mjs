d9e6ec81ac68a2aa345dd8069476dea5a6809163
Jeff Hitchcock <mail@command-center.org>
2025-09-17 13:03:54 -0700

    Fix calling new getter in SummonData#ability

--- module/data/activity/summon-data.mjs
+++ module/data/activity/summon-data.mjs
@@ -91,7 +91,7 @@ export default class SummonActivityData extends BaseActivityData {
 
   /** @inheritDoc */
   get ability() {
-    return this.match.ability || super.ability || this.item.abilityMod || this.actor?.system.spellcastingAbility;
+    return this.match.ability || super.ability || this.item.abilityMod || this.actor?.spellcastingAbility;
   }
 
   /* -------------------------------------------- */
