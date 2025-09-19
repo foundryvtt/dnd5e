e0842ffc714318c1533fd36ccf65512b728c2a24
Jeff Hitchcock <mail@command-center.org>
2025-09-19 11:32:57 -0700

    [#5031] Fix embedded stat blocks showing `undefined` creature type
    Closes #5031

--- module/data/actor/npc.mjs
+++ module/data/actor/npc.mjs
@@ -724,7 +724,7 @@ export default class NPCData extends CreatureTemplate {
         // Tag (e.g. `Gargantuan Dragon, Lawful Evil`)
         tag: game.i18n.format("DND5E.CreatureTag", {
           size: CONFIG.DND5E.actorSizes[this.traits.size]?.label ?? "",
-          type: Actor5e.formatCreatureType(this.details.type),
+          type: Actor5e.formatCreatureType(this.details.type) ?? "",
           alignment: this.details.alignment
         }).replace(/, $/, "")
       },
