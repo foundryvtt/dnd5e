f58d361e95ed5c34509aa9965e51e582b3e84115
Jeff Hitchcock <mail@command-center.org>
2025-10-13 13:50:48 -0700

    [#4610] Allow certain enrichers to be invoked from chat
    Allows for `/check`, `/save`, `/attack` and `/damage` commands to
    be used from chat using the same options as if they were used in
    the enricher format.
    
    For the `/check` and `/save` commands, a new `request` option can
    be included to create a roll request card directly.
    
    Closes #4610

--- dnd5e.mjs
+++ dnd5e.mjs
@@ -572,7 +572,10 @@ Hooks.on("renderChatLog", (app, html, data) => {
 });
 Hooks.on("renderChatPopout", (app, html, data) => documents.Item5e.chatListeners(html));
 
-Hooks.on("chatMessage", (app, message, data) => applications.Award.chatMessage(message));
+Hooks.on("chatMessage", (app, message, data) => {
+  if ( applications.Award.chatMessage(message) === false ) return false;
+  return enrichers.chatMessage(message);
+});
 Hooks.on("createChatMessage", dataModels.chatMessage.RequestMessageData.onCreateMessage);
 Hooks.on("updateChatMessage", dataModels.chatMessage.RequestMessageData.onUpdateResultMessage);
 
