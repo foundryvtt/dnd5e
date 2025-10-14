16a832357cbe6a53e453f7fc4a8e3a24aa04b193
Jeff Hitchcock <mail@command-center.org>
2025-10-14 10:36:51 -0700

    [#6247] Add speed reduction from armor's strength requirement
    Add speed reduction when wearing armor and not meeting its
    strength requirement. The reduction is defined in the config at
    `DND5E.armorSpeedReduction` in feet and converted to meters if
    necessary.
    
    Closes #6247

--- module/data/actor/templates/attributes.mjs
+++ module/data/actor/templates/attributes.mjs
@@ -428,6 +428,9 @@ export default class AttributesFields {
     const units = this.attributes.movement.units ??= defaultUnits("length");
     let reduction = dnd5e.settings.rulesVersion === "modern"
       ? (this.attributes.exhaustion ?? 0) * (CONFIG.DND5E.conditionTypes.exhaustion?.reduction?.speed ?? 0) : 0;
+    if ( (this.attributes.ac?.equippedArmor?.system.strength ?? 0) > (this.abilities?.str?.value ?? Infinity) ) {
+      reduction += CONFIG.DND5E.armorSpeedReduction;
+    }
     reduction = convertLength(reduction, CONFIG.DND5E.defaultUnits.length.imperial, units);
     const bonus = simplifyBonus(this.attributes.movement.bonus, rollData);
     this.attributes.movement.max = 0;
