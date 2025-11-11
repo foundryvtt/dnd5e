a006356cf487bbcf87accf46e59047641e1eee39
Tyler <3deathsaves@gmail.com>
2025-11-11 13:51:33 -0700

    Update item.mjs
    - Ensure we get the class stats only if the spells' source was a class or subclass, otherwise get defaults.

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -1264,7 +1264,13 @@ export default class Item5e extends SystemDocumentMixin(Item) {
 
     const values = {};
     if ( (spell instanceof Item5e) && spell.isOwned && (dnd5e.settings.rulesVersion === "modern") ) {
-      const spellcastingClass = spell.actor.spellcastingClasses?.[spell.system.spellSource?.identifier];
+      const sourceItem = spell.system.spellSource
+        ? spell.actor.identifiedItems.get(spell.system.spellSource)?.first()
+        : null;
+      let classIdentifier;
+      if ( sourceItem?.type === "class" ) classIdentifier = spell.system.spellSource;
+      else if ( sourceItem?.type === "subclass" ) classIdentifier = sourceItem.system.classIdentifier;
+      const spellcastingClass = spell.actor.spellcastingClasses?.[classIdentifier];
       if ( spellcastingClass ) {
         values.bonus = spellcastingClass.spellcasting.attack;
         values.dc = spellcastingClass.spellcasting.save;
