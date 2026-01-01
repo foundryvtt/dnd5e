3013d6cef1b899fe2cfd05f988e3b4e0e745aa58
Richard Simões <rsimoes@gmail.com>
2025-12-31 19:34:12 -0600

    Add `_state` parameter to `MappingField#_cleanType` in anticipation of V14

--- module/data/fields/mapping-field.mjs
+++ module/data/fields/mapping-field.mjs
@@ -53,10 +53,10 @@ export default class MappingField extends foundry.data.fields.ObjectField {
   /* -------------------------------------------- */
 
   /** @inheritDoc */
-  _cleanType(value, options) {
+  _cleanType(value, options, _state) {
     Object.entries(value).forEach(([k, v]) => {
       if ( k.startsWith("-=") ) return;
-      value[k] = this.model.clean(v, options);
+      value[k] = this.model.clean(v, options, _state);
     });
     return value;
   }
