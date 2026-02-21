6ec2278cc768233713a4664e274863b6d03ffd96
Artem Bogdanov <32058674+ArtemBogdanov@users.noreply.github.com>
2026-02-21 02:14:39 +0200

    Tooltips for skills and tools are not displayed. (#6749)
    * Fixed the bug where tooltips for skills and tools are not displayed on the character sheet
    
    Fixed adding links for skills and tools during the preparation of the rendering context for the details tab
    
    * Swap back to using for loop
    
    ---------
    
    Co-authored-by: Jeff Hitchcock <github444@command-center.org>

--- module/applications/actor/character-sheet.mjs
+++ module/applications/actor/character-sheet.mjs
@@ -361,7 +361,8 @@ export default class CharacterActorSheet extends BaseActorSheet {
     // Skills & Tools
     context.skills = this._prepareSkillsTools(context, "skills");
     context.tools = this._prepareSkillsTools(context, "tools");
-    for ( const [key, entry] of Object.entries(context.skills).concat(Object.entries(context.tools)) ) {
+    for ( const entry of context.skills.concat(context.tools) ) {
+      const key = entry.key;
       entry.class = this.constructor.PROFICIENCY_CLASSES[context.editable ? entry.baseValue : entry.value];
       if ( key in CONFIG.DND5E.skills ) entry.reference = CONFIG.DND5E.skills[key].reference;
       else if ( key in CONFIG.DND5E.tools ) entry.reference = Trait.getBaseItemUUID(CONFIG.DND5E.tools[key].id);
