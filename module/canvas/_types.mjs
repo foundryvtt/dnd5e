3b6fff902322465878dc97bf9bcc8e84a44ec53e
Jeff Hitchcock <mail@command-center.org>
2026-02-25 14:36:04 -0800

    [#6763] Implement `TemplatePlacement` to replace `AbilityTemplate`
    Adds a new `dnd5e.canvas.TemplatePlacement` API to handle placing
    shapes within a scene. This base API deals with basic shape data
    but the `fromActivity` factory method behaves similar to the old
    method on `AbilityTemplate` to create a full measured template
    region.
    
    The created regions are set up to work as similar as possible to
    the old measured templates, using `coverage` highlight mode and
    the user's color. When the area of effect count is larger than one,
    all of the shapes are combined into a single measured template.
    
    `TokenPlacement` has been refactored and much of its code has been
    shifted into a new `BasePlacement` class that is shared between the
    two canvas placement systems.
    
    Closes #6763

--- module/canvas/_types.mjs
+++ module/canvas/_types.mjs
@@ -1,21 +1,48 @@
 /**
- * Configuration information for a token placement operation.
- *
- * @typedef TokenPlacementConfiguration
+ * @typedef BasePlacementConfiguration
  * @property {TokenDocument} [origin]   Token that is the origin point of the placement.
- * @property {PrototypeToken[]} tokens  Prototype token information for rendering.
+ * @property {object[]} placements      Configuration data for individual placements.
  */
 
 /**
- * Data for token placement on the scene.
- *
- * @typedef TokenPlacementData
- * @property {PrototypeToken} prototypeToken
+ * @typedef BasePlacementData
  * @property {object} index
  * @property {number} index.total             Index of the placement across all placements.
- * @property {number} index.unique            Index of the placement across placements with the same original token.
+ * @property {number} index.unique            Index of the placement across placements with the same unique data.
  * @property {number} x
  * @property {number} y
  * @property {number} elevation
  * @property {number} rotation
+ */
+
+/* -------------------------------------------- */
+
+/**
+ * @typedef {BasePlacementConfiguration} TemplatePlacementConfiguration
+ * @property {number} color                                      Color to use when creating the template.
+ * @property {TemplatePlacementShapeConfiguration[]} placements  Configuration data for individual placements.
+ */
+
+/**
+ * @typedef TemplatePlacementShapeConfiguration
+ * @property {string} type      Shape type to use as the basis for the template.
+ * @property {number} size      Primary dimension of the template, converted into unit used for scene.
+ * @property {number} [width]   Width of the shape if relevant to shape type.
+ * @property {number} [height]  Height of the shape if relevant to shape type.
+ */
+
+/**
+ * @typedef {BaseShapeData} TemplatePlacementData
+ */
+
+/* -------------------------------------------- */
+
+/**
+ * @typedef {Omit<BasePlacementConfiguration, "placements">} TokenPlacementConfiguration
+ * @property {PrototypeToken[]} tokens  Prototype token information for rendering.
+ */
+
+/**
+ * @typedef {BasePlacementData} TokenPlacementData
+ * @property {PrototypeToken} prototypeToken
  */
