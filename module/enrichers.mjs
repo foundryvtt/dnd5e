36b5dd21f4049b8a45c5e8a085fe3650e2e2b97a
Jeff Hitchcock <mail@command-center.org>
2026-03-06 09:12:21 -0800

    [#4610] Export new parsing functions

--- module/enrichers.mjs
+++ module/enrichers.mjs
@@ -270,7 +270,7 @@ function handleAttackCommand(config) {
  * @param {EnrichmentOptions} [options={}]  Options provided to customize text enrichment.
  * @returns {object}
  */
-function parseAttackConfig(config, options={}) {
+export function parseAttackConfig(config, options={}) {
   const formulaParts = [];
   if ( config.formula ) formulaParts.push(config.formula);
   for ( const value of config.values ) {
@@ -686,7 +686,7 @@ function createCheckRequestButtons(dataset) {
  * @param {EnrichmentOptions} [options={}]  Options provided to customize text enrichment.
  * @returns {object}
  */
-function parseCheckConfig(config, options={}) {
+export function parseCheckConfig(config, options={}) {
   config.skill = config.skill?.replaceAll("/", "|").split("|") ?? [];
   config.tool = config.tool?.replaceAll("/", "|").split("|") ?? [];
   for ( let value of config.values ) {
@@ -849,7 +849,7 @@ function createSaveRequestButtons(dataset) {
  * @param {EnrichmentOptions} [options={}]  Options provided to customize text enrichment.
  * @returns {object}
  */
-function parseSaveConfig(config, options={}) {
+export function parseSaveConfig(config, options={}) {
   config.ability = config.ability?.replace("/", "|").split("|") ?? [];
   for ( let value of config.values ) {
     const slug = foundry.utils.getType(value) === "string" ? slugify(value) : value;
@@ -1108,7 +1108,7 @@ function handleDamageCommand(configs) {
  * @param {EnrichmentOptions} [options={}]  Options provided to customize text enrichment.
  * @returns {object}
  */
-function parseDamageConfig(configs, options={}) {
+export function parseDamageConfig(configs, options={}) {
   const config = { type: "damage", formulas: [], damageTypes: [], rollType: configs._isHealing ? "healing" : "damage" };
   const lookup = CONFIG.DND5E.enrichmentLookup;
   for ( const c of configs ) {
