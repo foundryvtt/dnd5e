2d5f74d1d16c0b9fd8a479ee1623f6111d2e0d87
Jeff Hitchcock <mail@command-center.org>
2026-03-19 10:04:50 -0700

    [#6763] Minimize all open windows during placement
    Finds all applications that are not minimized, have a frame, and
    are not detached and minimizes them until placement is completed.
    This behavior can be disabled using the `minimizeWindows` option
    in the placement configuration.
    
    Also adds a `restoreLayer` option to the configuration to control
    whether the original canvas layer is made active after placement.
    Both of these options are enabled by default.
    
    Closes #6763

--- module/canvas/_module.mjs
+++ module/canvas/_module.mjs
@@ -1,3 +1,4 @@
+export {default as BasePlacement} from "./api/base-placement.mjs";
 export * as detectionModes from "./detection-modes/_module.mjs";
 export * as layers from "./layers/_module.mjs";
 export {default as MapLocationControlIcon} from "./map-location-control-icon.mjs";
