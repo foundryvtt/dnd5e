d9ff1421359c7ce4be531a50528ab6e392a2dc1e
Tyler <3deathsaves@gmail.com>
2026-06-07 22:50:22 -0600

    Hide the Attune context option on concealed items
    Match the attunement icon so players can't attune unidentified items via the menu

--- module/applications/components/inventory.mjs
+++ module/applications/components/inventory.mjs
@@ -325,6 +325,7 @@ export default class InventoryElement extends (foundry.applications.elements.Ado
   _getContextOptions(item, element) {
     const compendiumLocked = game.packs.get(item.pack)?.locked;
     const inFavorites = !!element.closest(".favorites");
+    const concealDetails = !game.user.isGM && (item.system.identified === false);
 
     // Standard options.
     const options = [{
@@ -391,7 +392,7 @@ export default class InventoryElement extends (foundry.applications.elements.Ado
       label: `DND5E.ContextMenuAction${item.system.attuned ? "Unattune" : "Attune"}`,
       icon: '<i class="fa-solid fa-sun fa-fw"></i>',
       group: "state",
-      visible: () => item.system.attunement && item.isOwner && !compendiumLocked,
+      visible: () => !concealDetails && item.system.attunement && item.isOwner && !compendiumLocked,
       onClick: (event, target) => this._onAction(target, "attune", { event })
     }, {
       label: `DND5E.ContextMenuAction${item.system.equipped ? "Unequip" : "Equip"}`,
