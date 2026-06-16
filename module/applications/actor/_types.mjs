981007d7eaaf4e5fa6d8e21ac64f3d5387605468
Jeff Hitchcock <mail@command-center.org>
2026-06-16 16:09:49 -0700

    [#5129] Add dialog for sending requests to certain players (#5749)
    * [#5129] Add dialog for sending requests to certain players
    
    Adds a new `PartyRequestDialog` that can be used to prompt for
    which characters/players should receive a request. This dialog
    can be invoked from a specific group, in which case it will list
    the actors in that group, or it can use the primary party/assigned
    characters.
    
    The dialog presents the user with a list of actors that can be
    checked or unchecked as well as a list of owners for each actor.
    This list can be pre-filtered with the provided condition.
    
    When the dialog is submitted a new `request` chat message will be
    created with the selected actors and users as well as any other
    provided data.
    
    * [#5129] Swap to `_loc`
    
    * Move types into separate file, fix created request message data

--- module/applications/actor/_types.mjs
+++ module/applications/actor/_types.mjs
@@ -1,3 +1,20 @@
 /**
  * @typedef {"crew"|"draft"|"passengers"} CrewArea5e
  */
+
+/* -------------------------------------------- */
+
+/**
+ * @type PartyRequestDialogOptions
+ * @property {object}                     request
+ * @property {PartyRequestCondition|null} request.condition  Callback used to determine if an actor should be included.
+ * @property {Actor5e|null}               request.group      Group actor to fetch the actor list from, otherwise uses
+ *                                                           the primary party if one is set or falls back to the
+ *                                                           assigned characters.
+ */
+
+/**
+ * @callback PartyRequestCondition
+ * @param {Actor5e} actor  An actor that might be able to receive the request.
+ * @returns {boolean}      Should the actor be shown in the dialog?
+ */
