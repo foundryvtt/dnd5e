7ee434eea36fb460b24358d82597fc6ba4b0aa79
observer1236 <3195956924@qq.com>
2026-06-22 12:16:11 +0800

    Update spell.mjs

--- module/data/item/spell.mjs
+++ module/data/item/spell.mjs
@@ -116,7 +116,23 @@ export default class SpellData extends ItemDataModel.mixin(ActivitiesTemplate, I
           collapseGroup: group => group !== CONFIG.DND5E.spellListTypes.class
         }
       }],
-      ["properties", this.compendiumBrowserPropertiesFilter("spell")]
+      ["properties", this.compendiumBrowserPropertiesFilter("spell")],
+      ["activationType", {
+        label: "DND5E.SpellActivationType", 
+        type: "set",
+        config: {
+          choices: () => {
+            const types = CONFIG.DND5E.abilityActivationTypes;
+            const keep = ["action", "bonus", "reaction", "minute", "hour", "day"];
+            return Object.fromEntries(
+              Object.entries(types)
+              .filter(([key]) => keep.includes(key))
+              .map(([key, label]) => [key, game.i18n.localize(label)])
+            );
+          },
+          keyPath: "system.activation.type"
+        }
+      }]
     ]);
   }
 
