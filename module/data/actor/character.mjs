e0ed4b126771f09c0b86521ca6c75741b244c0b3
Zhell <50169243+krbz999@users.noreply.github.com>
2026-07-02 23:13:49 +0200

    Add piety score (#5828)

--- module/data/actor/character.mjs
+++ module/data/actor/character.mjs
@@ -77,7 +77,12 @@ export default class CharacterData extends CreatureTemplate {
             save: new FormulaField({ required: true, label: "DND5E.DeathSaveBonus" })
           })
         }, { label: "DND5E.DeathSave" }),
-        inspiration: new BooleanField({ required: true, label: "DND5E.Inspiration" })
+        inspiration: new BooleanField({ required: true, label: "DND5E.Inspiration" }),
+        piety: new SchemaField({
+          value: new NumberField({
+            min: 1, initial: null, nullable: true, integer: true, label: "DND5E.PIETY.FIELDS.value.label", placeholder: "0"
+          }),
+        })
       }, { label: "DND5E.Attributes" }),
       bastion: new SchemaField({
         name: new StringField({ required: true }),
