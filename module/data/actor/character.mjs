ce9c4f19ff75f5bc4c2b323b27b8b61de6c65dc0
Zhell <kennethzhell@gmail.com>
2026-07-02 23:05:50 +0200

    Remove redundant label, add placeholder

--- module/data/actor/character.mjs
+++ module/data/actor/character.mjs
@@ -80,7 +80,7 @@ export default class CharacterData extends CreatureTemplate {
         inspiration: new BooleanField({ required: true, label: "DND5E.Inspiration" }),
         piety: new SchemaField({
           value: new NumberField({
-            min: 1, initial: null, nullable: true, integer: true, label: "DND5E.PIETY.FIELDS.value.label"
+            min: 1, initial: null, nullable: true, integer: true, label: "DND5E.PIETY.FIELDS.value.label", placeholder: "0"
           }),
         })
       }, { label: "DND5E.Attributes" }),
