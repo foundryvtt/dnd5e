840915b9c749891a87723d3547fc6e5b6ecd020e
thatlonelybugbear <7237090+thatlonelybugbear@users.noreply.github.com>
2026-07-20 23:53:43 +0100

    style(attack): sort dialog option defaults

--- module/applications/dice/attack-configuration-dialog.mjs
+++ module/applications/dice/attack-configuration-dialog.mjs
@@ -11,8 +11,8 @@ import D20RollConfigurationDialog from "./d20-configuration-dialog.mjs";
 export default class AttackRollConfigurationDialog extends D20RollConfigurationDialog {
   /** @override */
   static DEFAULT_OPTIONS = {
-    ammunitionOptions: [],
     abilityOptions: [],
+    ammunitionOptions: [],
     attackModeOptions: [],
     masteryOptions: []
   };
