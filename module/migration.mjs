333af36a3b967244734aa83dca086aedc26a422c
Kim Mantas <kim.mantas@gmail.com>
2026-07-21 20:19:01 +0100

    Comment-out problematic migrations.

--- module/migration.mjs
+++ module/migration.mjs
@@ -823,6 +823,7 @@ export function migrateMessageData(messageData) {
     updateData["flags.dnd5e.bastion"] = _del;
   }
 
+  /* TODO: Re-instate these migrations when foundryvtt/foundryvtt#14229 is resolved.
   else if ( (flags?.dnd5e?.roll?.type === "generic") && (messageData.type === "base") ) {
     updateData.type = "generic";
     updateData.system = _replace({});
@@ -839,7 +840,7 @@ export function migrateMessageData(messageData) {
     updateData.type = "hitPoints";
     updateData.system = _replace({});
     updateData["flags.dnd5e.roll"] = _del;
-  }
+  }*/
 
   return updateData;
 }
