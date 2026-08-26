414b410c93a0ddc373c9f015843807459fb54bbc
Jeff Hitchcock <mail@command-center.org>
2026-08-26 14:27:10 -0700

    [#6357] Add riders directly to enchantment active effects
    Adds the ability to specify rider effects and items on enchantments
    (both using UUIDs) and rider activities (using IDs, but only if the
    enchantment is embedded in an item).
    
    Adjusts the API for how rider statuses as well as enchantment
    riders are added using a new `system.collectRiders` method to give
    AE types full control over riders.
    
    Closes #6357

--- module/data/active-effect/_types.mjs
+++ module/data/active-effect/_types.mjs
@@ -8,6 +8,10 @@
 
 /**
  * @typedef EnchantmentActiveEffectSystemData
- * @property {EffectChangeData[]} changes  Changes to apply to the item.
- * @property {boolean} magical             Does this effect originate from a magical source?
+ * @property {EffectChangeData[]} changes    Changes to apply to the item.
+ * @property {boolean} magical               Does this effect originate from a magical source?
+ * @property {object} rider
+ * @property {Set<string>} rider.activities  Additional activities to add when enchantment is applied.
+ * @property {Set<string>} rider.effects     Additional effects to add when enchantment is applied.
+ * @property {Set<string>} rider.items       Additional items to add when enchantment is applied to item on actor.
  */
